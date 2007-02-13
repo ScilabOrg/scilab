1469c2ee976fb0cec6daf74f920a11bcd28462d2
Alan Layec <alan.layec@inria.fr>
2007-02-13 13:10:12 +0000

    Fix

--- scilab/routines/scicos/vvtosci.f
+++ scilab/routines/scicos/vvtosci.f
@@ -1,19 +1,18 @@
       subroutine vvtosci(x,nx)
 c     fortran var2vec  to scilab
 c     x is supposed to be a fortran image of var2vec result
-c 
+c
 c     Copyright INRIA
       double precision x(*)
-c 
+c
+      iadr(l) = l + l - 1
+c
       integer  l
-c      integer  inull(4)
-      double precision dnull(4)
-c 
+      integer  il
+c
       external dcopy, error
       include "../stack.h"
-c      data inull/1,1,0,0/
-      data dnull/1.00d0,1.00d0,0.00d0,0.00d0/
-c 
+c
       if (top .ge. bot) then
         call error(18)
       else
@@ -26,8 +25,11 @@ c
           call error(17)
           return
         endif
-c          call dcopy(2,inull,1,stk(l),1)
-          call dcopy(2,dnull,1,stk(l),1)
+          il = iadr(lstk(top))
+          istk(il) = 1
+          istk(il+1) = 0
+          istk(il+2) = 0
+          istk(il+3) = 0
           lstk(top+1) = l + 4
        else
         err = l + nx - lstk(bot)
@@ -39,4 +41,4 @@ c          call dcopy(2,inull,1,stk(l),1)
         lstk(top+1) = l + nx
        endif
       endif
-      end 
+      end
