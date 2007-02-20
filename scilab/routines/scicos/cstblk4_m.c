2301c676bedf5c2b1ae6aee74332f0da1c3eb708
Fady NASSIF <fady.nassif@inria.fr>
2007-02-20 14:20:45 +0000

    avoiding warnings for dollar4_m avoiding a bug in cstblk4_m

--- scilab/routines/scicos/cstblk4_m.c
+++ scilab/routines/scicos/cstblk4_m.c
@@ -7,9 +7,9 @@ void cstblk4_m(scicos_block *block,int flag)
   /* Copyright INRIA
 
   Scicos block simulator
-  output a vector of constants out(i)=rpar(i)
-  rpar(1:nrpar) : given constants */
-  int nopar,mo,no,s;
+  output a vector of constants out(i)=opar(i)
+  opar(1:nopar) : given constants */
+  int nopar,mo,no,so;
   nopar = GetNopar(block);
   void *y;
   void *opar;
@@ -17,6 +17,6 @@ void cstblk4_m(scicos_block *block,int flag)
   opar=GetOparPtrs(block,1);
   mo=GetOparSize(block,1,1);
   no=GetOparSize(block,1,2);
-  s=GetSizeOfOut(block,1);
-  memcpy(y,opar,mo*no*s);
+  so=GetSizeOfOpar(block,1);
+  memcpy(y,opar,mo*no*so);
 }
