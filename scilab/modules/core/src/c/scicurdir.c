f5dc7cd894b8d1dfa86b26566176df45a3dea4de
Antoine ELIAS <antoine.elias@scilab.org>
2009-03-12 12:32:05 +0100

    fix scicurdir.c linux

--- scilab/modules/core/src/c/scicurdir.c
+++ scilab/modules/core/src/c/scicurdir.c
@@ -2,23 +2,23 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) INRIA
  * Copyright (C) ENPC
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
 #include <string.h>
-#ifdef _MSC_VER 
+#ifdef _MSC_VER
 	#include <windows.h>
 	#include <direct.h>
 	#include <errno.h>
-#else 
+#else
 	#include <unistd.h>
 	#define GETCWD(x,y) getcwd(x,y)
-#endif 
+#endif
 #include "sciprint.h"
 #include "scicurdir.h"
 #include "MALLOC.h"
@@ -31,18 +31,18 @@ static char cur_dir[PATH_MAX];
 int scichdir(char *path,int *err)
 {
 	*err=0;
-	if (path == (char*) NULL) 
+	if (path == (char*) NULL)
 	{
 		*cur_dir = '\0';
 		return (0);
 	}
 
 #ifndef _MSC_VER
-	if (chdir(path) == -1) 
+	if (chdir(path) == -1)
 	{
-		if ( getWarningMode() ) sciprint(_("Can't go to directory %s.\n"), path); 
+		if ( getWarningMode() ) sciprint(_("Can't go to directory %s.\n"), path);
 		*err=1;
-	} 
+	}
 #else
 	/**/
 	{
@@ -53,13 +53,13 @@ int scichdir(char *path,int *err)
 			switch (errno)
 			{
 			case ENOENT:
-				if ( getWarningMode() ) sciprint(_("Can't go to directory %s.\n"), path); 
+				if ( getWarningMode() ) sciprint(_("Can't go to directory %s.\n"), path);
 				break;
 			case EINVAL:
-				if ( getWarningMode() ) sciprint(_("Invalid buffer.\n")); 
+				if ( getWarningMode() ) sciprint(_("Invalid buffer.\n"));
 				break;
 			default:
-				if ( getWarningMode() ) sciprint(_("Unknown error.\n")); 
+				if ( getWarningMode() ) sciprint(_("Unknown error.\n"));
 			}
 			*err = 1;
 		}
@@ -74,7 +74,7 @@ int scigetcwd(char **path,int *lpath,int *err)
 #ifndef _MSC_VER
 	if (GETCWD(cur_dir, PATH_MAX) == (char*) 0)
 #else
-	wchar_t wcdir[PATH_MAX]; 
+	wchar_t wcdir[PATH_MAX];
 	if ( _wgetcwd(wcdir, PATH_MAX) == (wchar_t*) 0 )
 #endif
 	{
@@ -84,8 +84,9 @@ int scigetcwd(char **path,int *lpath,int *err)
 		*lpath = 0;
 		*err = 1;
 	}
-    else 
+  else
 	{
+#ifdef _MSC_VER
 		char *tmpcdir = wide_string_to_UTF8(wcdir);
 		if (tmpcdir)
 		{
@@ -102,7 +103,12 @@ int scigetcwd(char **path,int *lpath,int *err)
 			*lpath=0;
 			*err = 1;
 		}
+#else
+	*path = cur_dir;
+	*lpath =(int)strlen(cur_dir);
+	*err = 0;
+#endif
 	}
-    return 0;
+	return 0;
 }
 /*--------------------------------------------------------------------------*/
