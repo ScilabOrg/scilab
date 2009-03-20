ab5b320c01a28eba63d7f1e1a3aca4965bcebe55
Allan CORNET <allan.cornet@scilab.org>
2009-03-20 08:48:53 +0100

    fix part

--- scilab/modules/string/src/c/partfunction.c
+++ scilab/modules/string/src/c/partfunction.c
@@ -22,55 +22,35 @@
 /*--------------------------------------------------------------------------*/
 char **partfunction(char** stringInput,int m,int n,int *vectInput,int row)
 {
-	wchar_t **wcparts = NULL;
 	char **parts = NULL;
 	int mn = m * n;
 
-	wcparts = (wchar_t**)MALLOC(sizeof(wchar_t*)*(mn));
+	int i = 0;
 
-	if (wcparts)
+	parts = (char**)MALLOC(sizeof(char*)*(mn));
+
+	for (i = 0;i < mn; i++)
 	{
-		int i = 0;
+		int j = 0;
+		wchar_t *wcInput = to_wide_string(stringInput[i]);
+		int lengthstringInput = (int)wcslen(wcInput);
+		wchar_t *wcOutput = (wchar_t*)MALLOC(sizeof(wchar_t)*((row)+1));
 
-		parts = (char**)MALLOC(sizeof(char*)*(mn));
-
-		for (i = 0;i < mn; i++)
+		for (j = 0;j < row; j++)
 		{
-			int j = 0;
-			wchar_t *wcInput = to_wide_string(stringInput[i]);
-			int lengthstringInput = (int)wcslen(wcInput);
-
-			wcparts[i] = (wchar_t*)MALLOC(sizeof(wchar_t)*((row)+1));
-			if (wcparts[i] == NULL)
+			if ( vectInput[j] > lengthstringInput )
 			{
-				for (i = 0;i < mn; i++)
-				{
-					if (wcparts[i])
-					{
-						FREE(wcparts[i]);
-						wcparts[i] = NULL;
-					}
-				}
-				FREE(wcparts);
-				wcparts = NULL;
-				return NULL;
+				wcOutput[j] = L' ';
 			}
-
-			for (j = 0;j < row; j++)
+			else
 			{
-				if ( vectInput[j] > lengthstringInput )
-				{
-					wcparts[i][j] = L' ';
-				}
-				else
-				{
-					wcparts[i][j] = wcInput[vectInput[j]-1];
-				}
+				wcOutput[j] = wcInput[vectInput[j]-1];
 			}
-			wcparts[i][j] ='\0';
-
-			parts[i] = wide_string_to_UTF8(wcparts[i]);
 		}
+		wcOutput[j] ='\0';
+		parts[i] = wide_string_to_UTF8(wcOutput);
+		if (wcOutput) {FREE(wcOutput); wcOutput = NULL;}
+		if (wcInput) {FREE(wcInput); wcInput = NULL;}
 	}
 	return parts;
 }
