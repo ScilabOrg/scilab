6e5c61ee20b0cefd84c4d62c167db2d62e9f417c
Allan SIMON <allan.simon@scilab.org>
2010-04-30 16:35:26 +0200

    now editvar is called with the variable name in string,\nshould be able to edit values and get the result evaluated by scilab and returned back
    Change-Id: I0450a4208690096c99e966a93b0504d47b0478ca

---
+++ scilab/modules/ui_data/src/java/org/scilab/modules/ui_data/variableeditor/celleditor/VariableEditorCellEditor.java
@@ -0,0 +1,117 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2010 - DIGITEO - Allan SIMON
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+package org.scilab.modules.ui_data.variableeditor.celleditor;
+
+import static org.scilab.modules.graph.utils.ScilabInterpreterManagement.asynchronousScilabExec;
+import static org.scilab.modules.graph.utils.ScilabInterpreterManagement.buildCall;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.text.ParseException;
+
+import javax.swing.AbstractAction;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JFormattedTextField;
+import javax.swing.JTable;
+import javax.swing.KeyStroke;
+
+import org.scilab.modules.graph.utils.ScilabInterpreterManagement.InterpreterException;
+import org.scilab.modules.ui_data.variableeditor.ScilabVariableEditor;
+
+/**
+ * class VariableEditorCellEditor
+ * Use when editing cells, call Scilab interpreter.
+ */
+public class VariableEditorCellEditor extends DefaultCellEditor {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3866054279955696250L;
+
+	private int row;
+	private int col;
+	private JFormattedTextField textField;
+
+	/**
+	 * Constructor
+	 */
+	public VariableEditorCellEditor() {
+		super(new JFormattedTextField());
+		// TODO Auto-generated constructor stub
+
+
+		textField = (JFormattedTextField) getComponent();
+		textField.setFocusLostBehavior(JFormattedTextField.PERSIST);
+		textField.getInputMap().put(KeyStroke .getKeyStroke(
+				KeyEvent.VK_ENTER, 0),
+		"check");
+		textField.getInputMap().put(KeyStroke .getKeyStroke(
+				KeyEvent.VK_TAB, 0),
+		"check");
+		textField.getActionMap().put("check", new CellContentCheck());
+
+	}
+	/**
+	 * ellContentCheck
+	 * Called when user try to validate current cell content with TAB or ENTER
+	 */
+	private class CellContentCheck extends AbstractAction {
+
+		public void actionPerformed(ActionEvent e) {
+
+			final StringBuilder command = new StringBuilder();
+			String variableName = ScilabVariableEditor.getVariableEditor().getVariablename();
+			String data = String.valueOf(textField.getText());
+
+			String cellInVariable = variableName + "(" + row + "," + col + ")";
+			String cmdInExecStr = cellInVariable + " = " + data;
+			command.append("temp = " + buildCall("execstr", cmdInExecStr, "errcatch"));
+			command.append("updateEditvarValue(\"" + variableName + "\"," + row + "," + col + "," + cellInVariable + ",temp);");
+
+			final ActionListener action = new ActionListener() {
+
+				public void actionPerformed(ActionEvent e) {
+
+				}
+			};
+
+			try {
+				asynchronousScilabExec(action, command.toString());
+			} catch (InterpreterException e1) {
+				// TODO Auto-generated catch block
+				System.err.println("An error in the interpreter has been catched: "+e1.getLocalizedMessage()); 
+			}
+
+			try {
+				textField.commitEdit();
+				textField.postActionEvent();
+			} catch (ParseException e1) {
+				e1.printStackTrace();
+			}
+
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Component getTableCellEditorComponent(JTable table, Object value,
+			boolean isSelected, int row, int col) {
+		this.row = row + 1; // +1 because scilab index start at 1
+		this.col = row + 1;
+		return super.getTableCellEditorComponent(table, value, isSelected, row, col);
+	}
+
+}
