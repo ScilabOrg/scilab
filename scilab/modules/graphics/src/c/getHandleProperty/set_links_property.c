21907ae4c3b4cacc4ed218ed28cfd4338bec0403
Pierre LANDO <pierre.lando@scilab.org>
2010-05-03 16:36:32 +0200

    Fix bug 7075: When the "links" property was set for a handle of type "legend", the property type was not checked.
    Change-Id: I1fd87a80e065b1dadc1d1b254c3cc046e53ee10a

--- scilab/modules/graphics/src/c/getHandleProperty/set_links_property.c
+++ scilab/modules/graphics/src/c/getHandleProperty/set_links_property.c
@@ -13,9 +13,8 @@
  */
 
 /*------------------------------------------------------------------------*/
-/* file: set_title_property.c                                             */
-/* desc : function to modify in Scilab the title field of                 */
-/*        a handle                                                        */
+/* file: set_links_property.c                                             */
+/* desc : function to modify in Scilab the related polyline to a legend.  */
 /*------------------------------------------------------------------------*/
 
 #include "setHandleProperty.h"
@@ -36,10 +35,29 @@ int set_links_property( sciPointObj * pobj, size_t stackPointer, int valueType, 
     Scierror(999, _("'%s' property does not exist for this handle.\n"),"links");
     return SET_PROPERTY_ERROR ;
   }
+
+  if ( !isParameterHandle( valueType ) )
+  {
+    Scierror(999, _("Wrong type for '%s' property: Graphic handle array expected.\n"), "links");
+    return SET_PROPERTY_ERROR ;
+  }
+
   nblegends=pLEGEND_FEATURE(pobj)->nblegends;
   if (nbRow*nbCol != nblegends) {
     Scierror(999, _("Wrong size for '%s' property: %d elements expected.\n"), "links", nblegends);
     return SET_PROPERTY_ERROR ;
+  }
+
+  for (i=0; i<nblegends; i++) {
+    sciPointObj* polylineObject = sciGetPointerFromHandle( getHandleFromStack( stackPointer+i ) );
+    if (polylineObject->entitytype != SCI_POLYLINE) {
+      Scierror(999, _("%s: Input argument #%d must be a '%s' handle.\n"), "links", i, "polyline");
+      return SET_PROPERTY_ERROR ;
+    }
+    if (sciGetParentSubwin(polylineObject) != sciGetParentSubwin(pobj)) {
+      Scierror(999, _("%s: Input argument and the legend must have the same parent axes.\n"), "links", i);
+      return SET_PROPERTY_ERROR ;
+    }
   }
 
   for (i=0; i<nblegends; i++) {
