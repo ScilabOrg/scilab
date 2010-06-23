d03dd13ae59dd477bd3fa4241dfcd1c3305364b2
Bruno JOFRET <bruno.jofret@scilab.org>
2010-06-23 10:21:47 +0200

    typeof as Scilab Gateway
    Change-Id: I428f4c9cb87c79da3579e30ee6c412e6dc7c123d

---
+++ scilab/modules/types/sci_gateway/cpp/sci_typeof.cpp
@@ -0,0 +1,35 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2010-2010 - DIGITEO - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+#include "types.hxx"
+#include "function.hxx"
+#include "string.hxx"
+
+extern "C" {
+    #include "Scierror.h"
+    #include "localization.h"
+}
+
+using namespace types;
+
+Function::ReturnValue sci_typeof(typed_list &in, int _piRetCount, typed_list &out)
+{
+    if(in.size() != 1)
+    {
+        Scierror(999, _("%s: Wrong number of input argument(s): %d expected.\n"), "typeof", 1);
+        return Function::Error;
+    }
+
+    out.push_back(new String(in[0]->getTypeStr().c_str()));
+
+    return Function::OK;
+}
