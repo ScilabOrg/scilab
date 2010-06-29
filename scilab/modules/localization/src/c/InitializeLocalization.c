b19714643563f9d1bee25427033ab73baa9ca370
Yann Collette <yann.collette@scilab.org>
2010-06-29 14:08:53 +0200

    bug 7045 fixed - it was not possible to set the language via LANG
    Change-Id: I1d91f26384d1359d5bef00756c99cf0c9c5c2848

--- scilab/modules/localization/src/c/InitializeLocalization.c
+++ scilab/modules/localization/src/c/InitializeLocalization.c
@@ -1,6 +1,7 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2008 - INRIA - Sylvestre LEDRU
+ * Copyright (C) 2010 - DIGITEO - Yann COLLETTE
  * 
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -46,71 +47,76 @@ BOOL InitializeLocalization(void)
 {
 #ifdef HAVE_LIBINTL_H
 
-	char *SCIpath = getSCIpath();
-	char *pathLocales = NULL, *previousPathLocales = NULL;
-	char *ret = NULL;
+        char *SCIpath = getSCIpath();
+        char *pathLocales = NULL, *previousPathLocales = NULL;
+        char *ret = NULL;
 
-	/* set directory containing message catalogs */
-	pathLocales=(char *)MALLOC(sizeof(char)*(strlen(SCIpath)+strlen(PATHLOCALIZATIONFILE)+1));
+        /* clean some env variables to cleanly take into accounts LANG */
 
-	strcpy(pathLocales, SCIpath);
-	strcat(pathLocales, PATHLOCALIZATIONFILE);
+        unsetenv ("LANGUAGE");
+        unsetenv ("LC_MESSAGES");
 
-	if (bindtextdomain(NAMELOCALIZATIONDOMAIN,pathLocales)==NULL || !isdir(pathLocales))
-	{ 
-		/* source tree and classic build */
-		previousPathLocales = strdup(pathLocales);
-		if (pathLocales) {FREE(pathLocales); pathLocales = NULL;}
+        /* set directory containing message catalogs */
+        pathLocales=(char *)MALLOC(sizeof(char)*(strlen(SCIpath)+strlen(PATHLOCALIZATIONFILE)+1));
 
-		pathLocales=(char *)MALLOC(sizeof(char)*(strlen(SCIpath)+strlen("/..")+strlen(PATHLOCALIZATIONFILE)+1));
-		strcpy(pathLocales, SCIpath);
-		strcat(pathLocales, "/..");
-		strcat(pathLocales, PATHLOCALIZATIONFILE);
-		if (bindtextdomain(NAMELOCALIZATIONDOMAIN,pathLocales)==NULL || !isdir(pathLocales))
-		{ 
-			/* when it is installed on the system for example /usr/share/locale/ */
-			fprintf(stderr, "Warning: Localization issue: Error while binding the domain from %s or %s: Switch to the default language (English).\n", pathLocales, previousPathLocales);
-			if (previousPathLocales) {FREE(previousPathLocales); previousPathLocales = NULL;}
-			if (pathLocales) {FREE(pathLocales); pathLocales = NULL;}
-			if (SCIpath) {FREE(SCIpath); SCIpath = NULL;}
-			return FALSE;
-		}
-		if (previousPathLocales) {FREE(previousPathLocales); previousPathLocales = NULL;}
-		if (pathLocales) {FREE(pathLocales); pathLocales = NULL;}
-		if (SCIpath) {FREE(SCIpath); SCIpath = NULL;}
-		
-	}
+        strcpy(pathLocales, SCIpath);
+        strcat(pathLocales, PATHLOCALIZATIONFILE);
 
-	/* set domain for future gettext() calls */
-	ret=textdomain(NAMELOCALIZATIONDOMAIN);
-	if (ret==NULL)
-	{
-		fprintf(stderr, "Localization: Error while declaring the text domain %s\n", NAMELOCALIZATIONDOMAIN);
-		return FALSE;
-	}
-	bind_textdomain_codeset (NAMELOCALIZATIONDOMAIN,"UTF-8"); /*such that gettext and dgettext return UTF8 string*/
+        if (bindtextdomain(NAMELOCALIZATIONDOMAIN,pathLocales)==NULL || !isdir(pathLocales))
+        { 
+                /* source tree and classic build */
+                previousPathLocales = strdup(pathLocales);
+                if (pathLocales) {FREE(pathLocales); pathLocales = NULL;}
+
+                pathLocales=(char *)MALLOC(sizeof(char)*(strlen(SCIpath)+strlen("/..")+strlen(PATHLOCALIZATIONFILE)+1));
+                strcpy(pathLocales, SCIpath);
+                strcat(pathLocales, "/..");
+                strcat(pathLocales, PATHLOCALIZATIONFILE);
+                if (bindtextdomain(NAMELOCALIZATIONDOMAIN,pathLocales)==NULL || !isdir(pathLocales))
+                { 
+                        /* when it is installed on the system for example /usr/share/locale/ */
+                        fprintf(stderr, "Warning: Localization issue: Error while binding the domain from %s or %s: Switch to the default language (English).\n", pathLocales, previousPathLocales);
+                        if (previousPathLocales) {FREE(previousPathLocales); previousPathLocales = NULL;}
+                        if (pathLocales) {FREE(pathLocales); pathLocales = NULL;}
+                        if (SCIpath) {FREE(SCIpath); SCIpath = NULL;}
+                        return FALSE;
+                }
+                if (previousPathLocales) {FREE(previousPathLocales); previousPathLocales = NULL;}
+                if (pathLocales) {FREE(pathLocales); pathLocales = NULL;}
+                if (SCIpath) {FREE(SCIpath); SCIpath = NULL;}
+                
+        }
+
+        /* set domain for future gettext() calls */
+        ret=textdomain(NAMELOCALIZATIONDOMAIN);
+        if (ret==NULL)
+        {
+                fprintf(stderr, "Localization: Error while declaring the text domain %s\n", NAMELOCALIZATIONDOMAIN);
+                return FALSE;
+        }
+        bind_textdomain_codeset (NAMELOCALIZATIONDOMAIN,"UTF-8"); /*such that gettext and dgettext return UTF8 string*/
 #ifndef _MSC_VER
-	/* Here, the "" means that we will try to use the language of the system
-	 * first. If it doesn't work, we switch back to default (English) */
-	setlanguage("");
+        /* Here, the "" means that we will try to use the language of the system
+         * first. If it doesn't work, we switch back to default (English) */
+        setlanguage("");
 #else
-	/* We look if registry value LANGUAGE exists */
-	/* If not exists the "" means that we will try to use the language of the system.*/
-	{
-		char *loadLanguage = getLanguagePreferences();
-		setlanguage(loadLanguage);
-		if (loadLanguage) {FREE(loadLanguage); loadLanguage = NULL;}
-	}
+        /* We look if registry value LANGUAGE exists */
+        /* If not exists the "" means that we will try to use the language of the system.*/
+        {
+                char *loadLanguage = getLanguagePreferences();
+                setlanguage(loadLanguage);
+                if (loadLanguage) {FREE(loadLanguage); loadLanguage = NULL;}
+        }
 #endif
 
-	if (previousPathLocales) FREE(previousPathLocales);
-	if (pathLocales) FREE(pathLocales);
-	if (SCIpath) FREE(SCIpath);
+        if (previousPathLocales) FREE(previousPathLocales);
+        if (pathLocales) FREE(pathLocales);
+        if (SCIpath) FREE(SCIpath);
 
-	return TRUE;
+        return TRUE;
 #else
-	fprintf(stderr, "Localization: setlocale didn't exist on the computer used to compile Scilab ! This is abnormal ! No localization will be working for this distribution of Scilab.\n");
-	return FALSE;
+        fprintf(stderr, "Localization: setlocale didn't exist on the computer used to compile Scilab ! This is abnormal ! No localization will be working for this distribution of Scilab.\n");
+        return FALSE;
 #endif
 }
 /*--------------------------------------------------------------------------*/ 
