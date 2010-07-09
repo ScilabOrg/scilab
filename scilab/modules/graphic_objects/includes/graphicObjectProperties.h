f576e35c211aba607518ac96e0cba37d458e89fd
Bruno JOFRET <bruno.jofret@scilab.org>
2010-07-09 19:02:39 +0200

    Enable defines generation both Java and C to keep name consistency.
    Correct some Checkstyle
    
    Use newly defined values __GO_*__
    
    Change-Id: Ifb1a342410b027dfd6ca22ddce065c12f14f0a3e

---
+++ scilab/modules/graphic_objects/includes/graphicObjectProperties.h
@@ -0,0 +1,43 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2010-2010 - DIGITEO - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/*
+ * -=- This is a generated file, please do not edit by hand -=-
+ */
+
+#ifndef  __GRAPHIC_OBJECT_PROPERTIES_H__
+#define __GRAPHIC_OBJECT_PROPERTIES_H__
+
+#define __GO_AXES__                "axes" 
+#define __GO_AXIS__                "axis" 
+#define __GO_CHAMP__               "champ" 
+#define __GO_COMPOUND__            "compound" 
+#define __GO_FAC3D__               "fac3d" 
+#define __GO_FEC__                 "fec" 
+#define __GO_FIGURE__              "figure" 
+#define __GO_GRAYPLOT__            "grayplot" 
+#define __GO_LABEL__               "label" 
+#define __GO_LEGEND__              "legend" 
+#define __GO_MATPLOT__             "matplot" 
+#define __GO_PLOT3D__              "plot3d" 
+#define __GO_POLYLINE__            "polyline" 
+#define __GO_RECTANGLE__           "rectangle" 
+#define __GO_SEGS__                "segs" 
+#define __GO_TEXT__                "text" 
+
+#define __GO_CHILDREN__            "Children" 
+#define __GO_PARENT__              "Parent" 
+#define __GO_USER_DATA__            "UserData" 
+#define __GO_USER_DATA_SIZE__      "UserDataSize" 
+#define __GO_VISIBLE__             "Visible" 
+
+#endif /* !__GRAPHIC_OBJECT_PROPERTIES_H__ */
