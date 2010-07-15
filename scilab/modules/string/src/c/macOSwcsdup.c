67da110335bf370f55e79ce3febd732f9b1ad5ea
Bruno JOFRET <bruno.jofret@scilab.org>
2010-07-15 17:14:37 +0200

    - Add OS specific wcsdup function declared in os_wcsdup.h
    - Add MacOSX dedicated implementation
    
    Change-Id: I69cff31f5783473c0198771d284f96f62bf13d42

---
+++ scilab/modules/string/src/c/macOSwcsdup.c
@@ -0,0 +1,29 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2010-2010 - DIGITEO - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/*
+** This file is only compiled under MacOSX
+** because wcsdup does not exists.
+*/
+
+#include <stdlib.h>
+#include <wchar.h>
+
+#include "os_wcsdup.h"
+
+wchar_t *macOSXwcsdup(wchar_t *_pwcsSource)
+{
+    wchar_t * pwcsDest = (wchar_t *) malloc(sizeof(wchar_t) * (wcslen(_pwcsSource) + 1));
+    wcscpy(_pwcsSource, pwcsDest);
+
+    return pwcsDest;
+}
