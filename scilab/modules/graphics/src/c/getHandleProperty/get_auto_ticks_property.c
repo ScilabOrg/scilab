76d3c4e1b35e8bf88f0f8622b38b5958d33e7a6a
Manuel Juliachs <manuel.juliachs@scilab.org>
2010-07-28 10:43:09 +0200

    Axes object: per axis properties get/set C functions.
    Change-Id: If39b50d367e7a46b2e7ea6a27a2a2c5713393c18

--- scilab/modules/graphics/src/c/getHandleProperty/get_auto_ticks_property.c
+++ scilab/modules/graphics/src/c/getHandleProperty/get_auto_ticks_property.c
@@ -3,6 +3,7 @@
  * Copyright (C) 2004-2006 - INRIA - Fabrice Leray
  * Copyright (C) 2006 - INRIA - Allan Cornet
  * Copyright (C) 2006 - INRIA - Jean-Baptiste Silvy
+ * Copyright (C) 2010 - DIGITEO - Manuel Juliachs
  * 
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -25,48 +26,70 @@
 #include "localization.h"
 #include "MALLOC.h"
 
+#include "getGraphicObjectProperty.h"
+#include "graphicObjectProperties.h"
+
 /*------------------------------------------------------------------------*/
 int get_auto_ticks_property( sciPointObj * pobj )
 {
+  char * auto_ticks[3]  = { NULL, NULL, NULL };
+  char* axesAutoTicksPropertiesNames[3] = {__GO_X_AXIS_AUTO_TICKS__, __GO_Y_AXIS_AUTO_TICKS__, __GO_Z_AXIS_AUTO_TICKS__};
+  int* autoTicks;
 
-  char * auto_ticks[3]  = { NULL, NULL, NULL } ;
   int i ;
-  int status = -1 ;
+  int status = -1;
 
+#if 0
   if ( sciGetEntityType (pobj) != SCI_SUBWIN )
   {
-    Scierror(999, _("'%s' property does not exist for this handle.\n"),"auto_ticks") ;
-    return -1 ;
+    Scierror(999, _("'%s' property does not exist for this handle.\n"),"auto_ticks");
+    return -1;
   }
+#endif
 
   for ( i = 0 ; i < 3 ; i++ )
   {
-    auto_ticks[i] = MALLOC( 4 * sizeof(char) ) ;
+    auto_ticks[i] = MALLOC( 4 * sizeof(char) );
     if ( auto_ticks[i] == NULL )
     {
       int j ;
       for ( j = 0 ; j < i ; j++ )
       {
         FREE( auto_ticks[j] ) ;
-				Scierror(999, _("%s: No more memory.\n"),"get_auto_ticks_property");
+        Scierror(999, _("%s: No more memory.\n"),"get_auto_ticks_property");
         return -1 ;
       }
     }
-    if ( pSUBWIN_FEATURE (pobj)->axes.auto_ticks[i] )
+
+    autoTicks = (int*) getGraphicObjectProperty(pobj->UID, axesAutoTicksPropertiesNames[i], jni_bool);
+
+    if (autoTicks == NULL)
     {
-      strcpy( auto_ticks[i], "on" ) ;
+      int j;
+      for ( j = 0 ; j < i ; j++ )
+      {
+        FREE(auto_ticks[j]);
+      }
+
+      Scierror(999, _("'%s' property does not exist for this handle.\n"),"auto_ticks");
+      return -1;
+    }
+
+    if (*autoTicks)
+    {
+      strcpy( auto_ticks[i], "on" );
     }
     else
     {
-      strcpy( auto_ticks[i], "off" ) ;
+      strcpy( auto_ticks[i], "off" );
     }
   }
 
-  status = sciReturnRowStringVector( auto_ticks, 3 ) ;
+  status = sciReturnRowStringVector( auto_ticks, 3 );
 
   for ( i = 0 ; i < 3 ; i++ )
   {
-    FREE( auto_ticks[i] ) ;
+    FREE( auto_ticks[i] );
   }
 
   return status ;
