7706e387f2ea7b9d50419759eac160482b18efeb
Jerzy ZAGORSKI <xendoo@gmail.com>
2010-07-28 12:39:48 +0200

    Some polish changes
    Change-Id: Ie9cc7e922ba3e7724af8ac8d4d0ae22dffcd3bc4

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/simulink/BlockSpecificElement.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/simulink/BlockSpecificElement.java
@@ -20,40 +20,18 @@ import edu.tum.cs.commons.collections.UnmodifiableIterator;
 import edu.tum.cs.simulink.model.SimulinkBlock;
 
 public class BlockSpecificElement {
-	PatternElement patternElement = new PatternElement();
+
 	private static final Log LOG = LogFactory.getLog(BlockSpecificElement.class);
 	
-	public BasicBlock decode(SimulinkBlock from ,BasicBlock into) {
+	public static void print(SimulinkBlock from) {
 		
-		validate();
-		/**
-		 * mutable field used to share decoded BasicBlock with submethods
-		 */
-		BasicBlock base = into;
 		if (LOG.isTraceEnabled()) {
 			LOG.trace("From " + from.getName() + ":");
 			UnmodifiableIterator<String> paramNameIter = from.getParameterNames().iterator();
 			while(paramNameIter.hasNext()){
 				String paramName = paramNameIter.next();
-				LOG.trace(paramName + ": " + from.getParameter(paramName) + " || " + patternElement.decode(from.getName(), paramName.toString(), from.getParameter(paramName)));
+				LOG.trace(paramName + ": " + from.getParameter(paramName));
 			}
 		}
-		
-		/*
-		 * fill the data
-		 */
-		fillSpecificParameters(base);
-		//findCorrespondingParameter(string simulinkParameter);
-		
-		return into;
-	}
-
-	private void fillSpecificParameters(BasicBlock base) {
-		
-	}
-
-	private void validate() {
-		// TODO Auto-generated method stub
-		
 	}
 }
