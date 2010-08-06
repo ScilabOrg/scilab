8aa27020887521e2ba04671aa1e8dac3dcdd795c
Clément DAVID <clement.david@scilab.org>
2010-08-06 17:38:01 +0200

    Xcos: checkstyle and comment
    Change-Id: I05b7ec822dbdd32942fb51650775b5d6afa4083c

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/io/scicos/BlockElement.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/io/scicos/BlockElement.java
@@ -386,7 +386,11 @@ public class BlockElement extends AbstractElement<BasicBlock> {
 	 */
 	private void setupPortSize(BasicBlock from) {
 		// Getting children size per type.
-		int in, out, ein, eout;
+		int in;
+		int out;
+		int ein;
+		int eout;
+		
 		in = BasicBlockInfo.getAllTypedPorts(from, false, InputPort.class).size();
 		out = BasicBlockInfo.getAllTypedPorts(from, false, OutputPort.class).size();
 		ein = BasicBlockInfo.getAllTypedPorts(from, false, ControlPort.class).size();
