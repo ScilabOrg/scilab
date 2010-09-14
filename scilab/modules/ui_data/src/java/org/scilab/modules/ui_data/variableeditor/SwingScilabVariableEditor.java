692a18260d9d80f73e0f13d8fe480c80e2f1cd96
Calixte DENIZET <calixte@contrib.scilab.org>
2010-09-14 15:37:20 +0200

    Refactoring of EditVar: work in progress (near the end)
    Change-Id: Ie309a881b644bca248fa2ae94e0a2c2c5326215b

--- scilab/modules/ui_data/src/java/org/scilab/modules/ui_data/variableeditor/SwingScilabVariableEditor.java
+++ scilab/modules/ui_data/src/java/org/scilab/modules/ui_data/variableeditor/SwingScilabVariableEditor.java
@@ -1,29 +1,48 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2010 - DIGITEO - Allan SIMON
- * 
+ * Copyright (C) 2010 - Calixte DENIZET
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
 
 package org.scilab.modules.ui_data.variableeditor;
 
-
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
+import javax.swing.AbstractAction;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.KeyStroke;
 import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.table.TableModel;
 
 import org.scilab.modules.gui.bridge.tab.SwingScilabTab;
+import org.scilab.modules.gui.bridge.window.SwingScilabWindow;
 import org.scilab.modules.gui.menubar.MenuBar;
+import org.scilab.modules.gui.pushbutton.PushButton;
 import org.scilab.modules.gui.tab.SimpleTab;
 import org.scilab.modules.gui.tab.Tab;
 import org.scilab.modules.gui.textbox.TextBox;
+import org.scilab.modules.gui.toolbar.ScilabToolBar;
 import org.scilab.modules.gui.toolbar.ToolBar;
 import org.scilab.modules.gui.utils.UIElementMapper;
 import org.scilab.modules.gui.window.Window;
@@ -31,113 +50,232 @@ import org.scilab.modules.localization.Messages;
 import org.scilab.modules.ui_data.datatable.SwingEditvarTableModel;
 import org.scilab.modules.ui_data.rowheader.RowHeader;
 import org.scilab.modules.ui_data.rowheader.RowHeaderModel;
-import org.scilab.modules.ui_data.variableeditor.celleditor.CellEditorFactory;
-import org.scilab.modules.ui_data.variableeditor.listeners.ExpandListener;
+import org.scilab.modules.ui_data.utils.UiDataMessages;
+import org.scilab.modules.ui_data.variableeditor.action.RefreshAction;
 import org.scilab.modules.ui_data.variableeditor.renderers.RendererFactory;
-
+import org.scilab.modules.ui_data.variableeditor.celleditor.CellEditorFactory;
+import org.scilab.modules.ui_data.variableeditor.celleditor.ScilabGenericCellEditor;
 
 /**
  * Swing implementation of Scilab Variable Editor
  * uses JTable
+ * @author Allan SIMON
+ * @author Calixte DENIZET
  */
 public class SwingScilabVariableEditor extends SwingScilabTab implements Tab, SimpleVariableEditor {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private SwingEditvarTableModel<Object> dataModel;
-	private JTable table;
-	private JScrollPane scrollPane;
+    /**
+     * Prefix used in the tabs titles.
+     */
+    public static final String PREFIX = "Var - ";
 
-	/**
-	 * Create a JTable with data Model.
-	 * @param data : The JTable data.
-	 */
-	public SwingScilabVariableEditor(Object[][] data) {
-		super(Messages.gettext("Variable Browser"));
-		
-		setData(data);
-	}
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void addInfoBar(TextBox infoBarToAdd) {
-		setInfoBar(infoBarToAdd);
-	}
+    private SwingEditvarTableModel dataModel;
+    private JTabbedPane tabPane;
+    private JScrollPane scrollPane;
+    private PushButton refreshButton;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void addMenuBar(MenuBar menuBarToAdd) {
-		setMenuBar(menuBarToAdd);
-	}
+    /**
+     * Create a JTable with data Model.
+     * @param data : The JTable data.
+     */
+    public SwingScilabVariableEditor(String name, String type, Object[][] data) {
+        super(Messages.gettext("Variable Editor"));
+        refreshButton = RefreshAction.createButton(this, UiDataMessages.REFRESH, "view-refresh.png");
+        tabPane = new JTabbedPane();
+        tabPane.addChangeListener(new ChangeListener() {
+                public void stateChanged(ChangeEvent e) {
+                    String name = tabPane.getTitleAt(tabPane.getSelectedIndex()).substring(PREFIX.length());
+                    String tooltip = "";
+                    if (name != null) {
+                        tooltip = UiDataMessages.REFRESH + "'" + name + "'";
+                        String type = ((SwingEditvarTableModel) getCurrentModel()).getType();
+                        String title = Messages.gettext("Variable Editor") + " - " + name + "  (" + type + ")";
+                        setName(title);
+                        SwingScilabWindow window = (SwingScilabWindow) SwingUtilities.getAncestorOfClass(SwingScilabWindow.class, tabPane);
+                        if (window != null) {
+                            window.setTitle(title);
+                        }
+                    }
+                    refreshButton.setToolTipText(tooltip);
+                }
+            });
+        setContentPane(tabPane);
+        setData(name, type, data);
+        ToolBar toolBar = ScilabToolBar.createToolBar();
+        toolBar.add(refreshButton);
+        addToolBar(toolBar);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void addToolBar(ToolBar toolBarToAdd) {
-		setToolBar(toolBarToAdd);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public void addInfoBar(TextBox infoBarToAdd) {
+        setInfoBar(infoBarToAdd);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
+    /**
+     * {@inheritDoc}
+     */
+    public void addMenuBar(MenuBar menuBarToAdd) {
+        setMenuBar(menuBarToAdd);
+    }
 
-	public void setData(Object[][] data) {
-		dataModel = new SwingEditvarTableModel<Object>(data);
+    /**
+     * {@inheritDoc}
+     */
+    public void addToolBar(ToolBar toolBarToAdd) {
+        setToolBar(toolBarToAdd);
+    }
 
-		table = new JTable(dataModel);
-		table.setDefaultEditor(Object.class, CellEditorFactory.createCellEditor(data));
-		table.setFillsViewportHeight(true);
-		table.setAutoResizeMode(CENTER);
-		table.setRowHeight(25);
-		table.setDefaultRenderer(Object.class, RendererFactory.createRenderer(data));
+    /**
+     * @return the TabbedPane used in this window
+     */
+    public JTabbedPane getTabPane() {
+        return tabPane;
+    }
 
-		//table.getColumnModel().setColumnMargin(2);
+    /**
+     * @return the current table
+     */
+    public JTable getCurrentTable() {
+        return (JTable) ((JScrollPane) tabPane.getSelectedComponent()).getViewport().getComponent(0);
+    }
 
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		table.setCellSelectionEnabled(true);		
+    /**
+     * @return the current table
+     */
+    public TableModel getCurrentModel() {
+        return getCurrentTable().getModel();
+    }
 
-		RowHeaderModel rowHeaderModel = new RowHeaderModel(dataModel);
-		RowHeader rowHeader = new RowHeader(rowHeaderModel, table);
+    /**
+     * {@inheritDoc}
+     */
+    public void setData(String name, String type, Object[][] data) {
+        JTable table = new JTable();
+        scrollPane = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+        table.setAutoResizeMode(CENTER);
+        table.setRowHeight(25);
 
+        updateData(table, type, data);
 
-		scrollPane = new JScrollPane(table);
-		scrollPane.setRowHeaderView(rowHeader);
-		scrollPane.getHorizontalScrollBar().addAdjustmentListener(new ExpandListener());
+        table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        table.setCellSelectionEnabled(true);
 
-		table.setBackground(Color.WHITE);
-		setContentPane(scrollPane);
-	}
+        table.setBackground(Color.WHITE);
+        tabPane.add(PREFIX + name, scrollPane);
+        tabPane.setSelectedComponent(scrollPane);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void setValueAt(Object value, int row, int col) {
-		// Update renderer in case we changed type after openning value
-	    // ex : a = 10; editvar("a"); change value to "hello" 
-	    table.setDefaultRenderer(Object.class, RendererFactory.createRenderer(value));
-	    
-	    dataModel.setValueAt(value, row, col);
-		RowHeaderModel rowHeaderModel = new RowHeaderModel(dataModel);
-		RowHeader rowHeader = new RowHeader(rowHeaderModel, table);
-		scrollPane.setRowHeaderView(rowHeader);
-	}
+    /**
+     * @param table the table to update
+     * @param type the type of the matrix
+     * @param data the matrix datas
+     */
+    public void updateData(JTable table, String type, Object[][] data) {
+        ScilabGenericCellEditor cellEditor = (ScilabGenericCellEditor) CellEditorFactory.createCellEditor(type);
+        if (table.getModel() instanceof SwingEditvarTableModel) {
+            cellEditor.setExpressions(((SwingEditvarTableModel) table.getModel()).getCellEditor().getExpressions());
+        }
+        dataModel = new SwingEditvarTableModel(type, data, cellEditor);
+        table.setModel(dataModel);
+        RowHeaderModel rowHeaderModel = new RowHeaderModel(dataModel);
+        RowHeader rowHeader = new RowHeader(rowHeaderModel, table);
+        scrollPane.setRowHeaderView(rowHeader);
+        table.setDefaultEditor(Object.class, cellEditor);
+        table.setDefaultRenderer(Object.class, RendererFactory.createRenderer(type));
+        addCopyPaste(table);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public SimpleTab getAsSimpleTab() {
-		return this;
-	}
+    /**
+     * @param c the component in the tabbedpane
+     * @param type the type of the matrix
+     * @param data the matrix datas
+     */
+    public void updateData(Component c, String type, Object[][] data) {
+        tabPane.setSelectedComponent(c);
+        JTable table = (JTable) ((JScrollPane) c).getViewport().getComponent(0);
+        updateData(table, type, data);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public Window getParentWindow() {
-		return (Window) UIElementMapper.getCorrespondingUIElement(getParentWindowId());
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public SimpleTab getAsSimpleTab() {
+        return this;
+    }
 
+    /**
+     * {@inheritDoc}
+     */
+    public Window getParentWindow() {
+        return (Window) UIElementMapper.getCorrespondingUIElement(getParentWindowId());
+    }
+
+    /**
+     * @param table where to add copy/paste action
+     */
+    private void addCopyPaste(final JTable table) {
+        KeyStroke copy = KeyStroke.getKeyStroke("ctrl C");
+        KeyStroke paste = KeyStroke.getKeyStroke("ctrl V");
+        AbstractAction copyAction = new AbstractAction() {
+                public void actionPerformed(ActionEvent e) {
+                    int[] cols = table.getSelectedColumns();
+                    int[] rows = table.getSelectedRows();
+                    table.setColumnSelectionInterval(cols[0], cols[cols.length - 1]);
+                    table.setRowSelectionInterval(rows[0], rows[rows.length - 1]);
+                    StringBuffer buf = new StringBuffer();
+                    for (int i = rows[0]; i <= rows[rows.length - 1]; i++) {
+                        for (int j = cols[0]; j <= cols[cols.length - 1]; j++) {
+                            String exp = ((SwingEditvarTableModel) table.getModel()).getCellEditor().getExpression(i, j);
+                            if (exp != null) {
+                                buf.append("=" + exp);
+                            } else {
+                                buf.append(((SwingEditvarTableModel) table.getModel()).getScilabValueAt(i, j));
+                            }
+                            if (j < cols[cols.length - 1]) {
+                                buf.append("\t");
+                            }
+                        }
+                        buf.append("\n");
+                    }
+                    StringSelection sel  = new StringSelection(buf.toString());
+                    Toolkit.getDefaultToolkit().getSystemClipboard().setContents(sel, sel);
+                }
+            };
+
+        AbstractAction pasteAction = new AbstractAction() {
+                public void actionPerformed(ActionEvent e) {
+                    int col = table.getSelectedColumn();
+                    int row = table.getSelectedRow();
+                    table.setColumnSelectionInterval(col, col);
+                    table.setRowSelectionInterval(row, row);
+                    String str = "";
+                    try {
+                        str = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getContents(this).getTransferData(DataFlavor.stringFlavor);
+                    } catch (UnsupportedFlavorException ex1) {
+                        System.err.println(ex1);
+                    } catch (IOException ex2) {
+                        System.err.println(ex2);
+                    }
+                    StringTokenizer rElems = new StringTokenizer(str, "\n");
+                    for (int i = 0; rElems.hasMoreTokens(); i++) {
+                        StringTokenizer cElems = new StringTokenizer(rElems.nextToken(), "\t");
+                        for (int j = 0; cElems.hasMoreTokens(); j++) {
+                            String value = (String) cElems.nextToken();
+                            ((SwingEditvarTableModel) table.getModel()).setValueAtAndUpdate(false, value, row + i, col + j);
+                        }
+                    }
+                    ((SwingEditvarTableModel) table.getModel()).updateMatrix();
+                }
+            };
+
+        table.getActionMap().put("Copy", copyAction);
+        table.getInputMap().put(copy, "Copy");
+        table.getActionMap().put("Paste", pasteAction);
+        table.getInputMap().put(paste, "Paste");
+    }
 }
