d4e15be5938b27d6647287259587c9fb2cf59439
Clément DAVID <clement.david@scilab.org>
2010-09-16 10:55:52 +0200

    Xcos: use the ordering data instead of the port index on alignement
    Bug: #7868 (#8042)
    Change-Id: If0de82b66d4c58f8945917bacb382ffd448df929

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/utils/BlockPositioning.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/utils/BlockPositioning.java
@@ -73,8 +73,14 @@ public final class BlockPositioning {
 			final mxGeometry portGeom = port.getGeometry();
 			
 			double nonVariantPosition = -portGeom.getWidth();
+			final int order;
+			if (port.getOrdering() <= portsSize) {
+				order = port.getOrdering() - 1;
+			} else {
+				order = i;
+			}
 			double alignedPosition = calculateAlignedPosition(gridSize,
-					segLength, i);
+					segLength, order);
 			
 			portGeom.setX(nonVariantPosition);
 			portGeom.setY(alignedPosition);
@@ -168,8 +174,14 @@ public final class BlockPositioning {
 			final mxGeometry portGeom = port.getGeometry();
 			
 			double nonVariantPosition = -portGeom.getHeight();
+			final int order;
+			if (port.getOrdering() <= portsSize) {
+				order = port.getOrdering() - 1;
+			} else {
+				order = i;
+			}
 			double alignedPosition = calculateAlignedPosition(gridSize,
-					segLength, i);
+					segLength, order);
 			
 			portGeom.setX(alignedPosition);
 			portGeom.setY(nonVariantPosition);
@@ -204,8 +216,14 @@ public final class BlockPositioning {
 			final mxGeometry portGeom = port.getGeometry();
 			
 			double nonVariantPosition = blockGeom.getWidth();
+			final int order;
+			if (port.getOrdering() <= portsSize) {
+				order = port.getOrdering() - 1;
+			} else {
+				order = i;
+			}
 			double alignedPosition = calculateAlignedPosition(gridSize,
-					segLength, i);
+					segLength, order);
 			
 			portGeom.setX(nonVariantPosition);
 			portGeom.setY(alignedPosition);
@@ -240,8 +258,14 @@ public final class BlockPositioning {
 			final mxGeometry portGeom = port.getGeometry();
 			
 			double nonVariantPosition = blockGeom.getHeight();
+			final int order;
+			if (port.getOrdering() <= portsSize) {
+				order = port.getOrdering() - 1;
+			} else {
+				order = i;
+			}
 			double alignedPosition = calculateAlignedPosition(gridSize,
-					segLength, i);
+					segLength, order);
 			
 			portGeom.setX(alignedPosition);
 			portGeom.setY(nonVariantPosition);
@@ -313,8 +337,6 @@ public final class BlockPositioning {
 		Orientation rotated = rotateOrientation(iter, mirrored, flipped);
 		
 		updatePortsPosition(block, rotated, angle, working);
-		
-		
 	}
 
 	/**
@@ -367,19 +389,27 @@ public final class BlockPositioning {
 		rotated = Orientation.values()[(rotated.ordinal() + rotationIndex)
 		           					% nbOfOrientations];
 
+		/* Select ports with the right angle */
+		final ArrayList<BasicPort> orientedPorts = new ArrayList<BasicPort>(working.size());
+		for (BasicPort basicPort : working) {
+			if (basicPort.getOrientation() == rotated) {
+				orientedPorts.add(basicPort);
+			}
+		}
+		
 		/* Call the associated function */
 		switch (rotated) {
 		case NORTH:
-			updateNorthPortsPosition(block, working);
+			updateNorthPortsPosition(block, orientedPorts);
 			break;
 		case SOUTH:
-			updateSouthPortsPosition(block, working);
+			updateSouthPortsPosition(block, orientedPorts);
 			break;
 		case EAST:
-			updateEastPortsPosition(block, working);
+			updateEastPortsPosition(block, orientedPorts);
 			break;
 		case WEST:
-			updateWestPortsPosition(block, working);
+			updateWestPortsPosition(block, orientedPorts);
 			break;
 
 		default:
