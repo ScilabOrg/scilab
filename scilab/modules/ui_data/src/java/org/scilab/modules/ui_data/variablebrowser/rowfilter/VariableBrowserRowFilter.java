cfe6fece6663008a44effd900f2a1fcdef356f5f
Calixte DENIZET <calixte@contrib.scilab.org>
2010-09-16 20:40:20 +0200

    Variable Browser: Fix package name due to 86c529c121cd2c6cb6d87bdedbf5c2daf35eec61
    Change-Id: Ib1620e739b5d2b8bf8de69f6f24ad61153094ba9

--- scilab/modules/ui_data/src/java/org/scilab/modules/ui_data/variablebrowser/rowfilter/VariableBrowserRowFilter.java
+++ scilab/modules/ui_data/src/java/org/scilab/modules/ui_data/variablebrowser/rowfilter/VariableBrowserRowFilter.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 
 import javax.swing.RowFilter;
 
-import org.scilab.modules.types.ScilabTypeEnum;
+import org.scilab.modules.types.scilabTypes.ScilabTypeEnum;
 
 public class VariableBrowserRowFilter extends RowFilter<Object, Object>{
 
