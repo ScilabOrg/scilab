c2247c6556fb9f6b7a010fd24de4ea8f2de44215
Pierre LANDO <pierre.lando@scilab.org>
2010-09-24 10:23:46 +0200

    TEST commit, for trace only. Nothing to see here.
    Change-Id: Ibe66f1bf9fc7f9179952ebbc0506f27b8a942f0f

---
+++ scilab/modules/graphic_objects/src/java/org/scilab/modules/graphic_objects/JoGLView/FigureBridge.java
@@ -0,0 +1,90 @@
+package org.scilab.modules.graphic_objects.JoGLView;
+
+import org.scilab.forge.scirenderer.impl.opengl.renderer.simple.SimpleGLRenderer;
+import org.scilab.modules.graphic_objects.JoGLView.nativegl.NativeGL;
+import org.scilab.modules.graphic_objects.graphicController.GraphicController;
+import org.scilab.modules.graphic_objects.graphicObject.GraphicObjectProperties;
+import org.scilab.modules.graphic_objects.graphicView.GraphicView;
+
+import javax.media.opengl.GLCanvas;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Pierre Lando
+ */
+public class FigureBridge implements GraphicView {
+    private final String id;
+    private final JFrame frame = new JFrame();
+    private final GLCanvas canvas = new GLCanvas();
+
+
+    private FigureBridge(String id) throws Exception {
+        Object type = GraphicController.getController().getProperty(id, GraphicObjectProperties.__GO_TYPE__);
+        if (type instanceof String && type.equals("Figure")) {
+            // TODO : use static Figure.type instead of "Figure".
+
+            this.id = id;
+
+            SimpleGLRenderer renderer = new SimpleGLRenderer(canvas);
+            renderer.setSceneTree(new SciSceneTree(id));
+
+            JPanel jContentPane = new JPanel(new BorderLayout());
+            jContentPane.add(canvas, BorderLayout.CENTER);
+
+            GraphicController.getController().register(this);
+
+            updateName();
+
+            frame.add(jContentPane);
+            frame.setSize(900, 750);
+            frame.setVisible(true);
+        } else {
+            throw new Exception("this id is not a figure");
+        }
+    }
+
+    /**
+     * This method update the figure name showed on the frame.
+     * For compatibility, it replace the first occurrence of "%d" by the figure id.  
+     */
+    private void updateName() {
+        String name = (String) GraphicController.getController().getProperty(id, GraphicObjectProperties.__GO_NAME__);
+        Integer figureId = (Integer) GraphicController.getController().getProperty(id, GraphicObjectProperties.__GO_ID__);
+        if (name != null) {
+            if (figureId == null) {
+                figureId = -1;
+            }
+            name = name.replaceFirst("%d", figureId.toString());
+        }
+
+        frame.setTitle(name);
+    }
+
+    public void updateObject(String id, String property) {
+        if (id != null && id.equals(this.id)) {
+            updateName();
+
+            if (property != null && property.equals(GraphicObjectProperties.__GO_DATA_MODEL__)) {
+                NativeGL.updateObject(id);
+            }
+
+            canvas.display();
+        }
+    }
+
+    @Override
+    public void createObject(String id) {}
+
+    @Override
+    public void deleteObject(String id) {}
+
+    public static FigureBridge createFigure(String id) {
+        try {
+            return new FigureBridge(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ scilab/modules/graphic_objects/src/java/org/scilab/modules/graphic_objects/JoGLView/JoGLView.java
@@ -0,0 +1,56 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2010 - DIGITEO - Pierre Lando
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+package org.scilab.modules.graphic_objects.JoGLView;
+
+import org.scilab.forge.scirenderer.impl.opengl.core.drawersmanager.IGLDrawersManager;
+import org.scilab.forge.scirenderer.impl.opengl.core.scenedrawer.GLSceneDrawer;
+import org.scilab.modules.graphic_objects.graphicController.GraphicController;
+import org.scilab.modules.graphic_objects.graphicObject.GraphicObject;
+import org.scilab.modules.graphic_objects.graphicView.GraphicView;
+import org.scilab.modules.jvm.LoadClassPath;
+
+public class JoGLView implements GraphicView {
+    private static JoGLView singleton = null;
+    
+    private JoGLView() {
+        IGLDrawersManager drawerManager = GLSceneDrawer.getSingleton().getDrawersManager();
+        //drawerManager.registerDrawer(new FigureGLDrawer());
+    }
+
+    public static JoGLView getJoGLView() {
+        if (singleton == null) {
+            System.out.println("-- LoadClassPath.loadOnUse('graphics')");
+            LoadClassPath.loadOnUse("graphics");
+            singleton = new JoGLView();
+        }
+        return singleton;
+    }
+
+    public void updateObject(String id, String property) {
+        
+    }
+
+    public void createObject(String id) {
+        Object controller = GraphicController.getController().getObjectFromId(id);
+        if (controller != null && controller instanceof GraphicObject) {
+            String type = ((GraphicObject) controller).getType();
+            if (type != null && type.equals("Figure")) {
+                FigureBridge.createFigure(id);
+            }
+        }
+    }
+
+    public void deleteObject(String id) {
+        System.out.println("PEDRO : delete");
+    }
+}
