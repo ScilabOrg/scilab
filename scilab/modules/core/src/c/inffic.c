92e34d74242c7aa059ff397c8f96b903dba5e5bc
Allan CORNET <allan.cornet@scilab.org>
2010-09-27 12:00:37 +0200

    bug 8079
    Change-Id: I3661f9310e3174bd1bceec6e440210278d6bb211

--- scilab/modules/core/src/c/inffic.c
+++ scilab/modules/core/src/c/inffic.c
@@ -2,6 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) INRIA
  * Copyright (C) ENPC
+ * Copyright (C) DIGITEO - 2010 - Allan CORNET
  * 
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -17,50 +18,49 @@
 #define MAXDATA 5 
 
 
-
 static char *dataStrings[] = {
-  "$MANCHAPTERS",
-  /* start_up instructions file */
-  "exec('SCI/etc/scilab.start',-1);",
-  /* demos instructions file */
-  "scilab_demos();",	 
-  /* on crash save file */
-  "home/scilab.save",
-  /* exit instructions file */
-  "exec('SCI/etc/scilab.quit',-1);quit;"	 ,
-  /* on exit , we try to catch errors */
-  /* workaround to not have scilab "zombie" */
-  /* bug 3672 */
-  "exec('SCI/etc/scilab.quit','errcatch',-1);quit;"	 
+    "$MANCHAPTERS",
+    /* start_up instructions file */
+    "exec('SCI/etc/scilab.start',-1);",
+    /* demos instructions file */
+    "scilab_demos();",	 
+    /* on crash save file */
+    "home/scilab.save",
+    /* exit instructions file */
+    "clear;exec('SCI/etc/scilab.quit',-1);quit;"	 ,
+    /* on exit , we try to catch errors */
+    /* workaround to not have scilab "zombie" */
+    /* bug 3672 */
+    "clear;exec('SCI/etc/scilab.quit','errcatch',-1);quit;"	 
 };
 
 /******************************************
- * get string associated with iopt 
- ******************************************/
+* get string associated with iopt 
+******************************************/
 
 void C2F(inffic)(int *iopt, char *name, int *nc)
 {
-  *nc=(int) strlen(dataStrings[Max(Min(*iopt-1,MAXDATA),0)]);
-  strcpy(name,dataStrings[Max(Min(*iopt-1,MAXDATA),0)]);
+    *nc=(int) strlen(dataStrings[Max(Min(*iopt-1,MAXDATA),0)]);
+    strcpy(name,dataStrings[Max(Min(*iopt-1,MAXDATA),0)]);
 }
 
 /******************************************
- * just get string length 
- ******************************************/
+* just get string length 
+******************************************/
 
 void C2F(infficl)(int *iopt, int *nc)
 {
-  *nc=(int) strlen(dataStrings[Max(Min(*iopt-1,MAXDATA),0)]);
+    *nc=(int) strlen(dataStrings[Max(Min(*iopt-1,MAXDATA),0)]);
 }
 
 
 /******************************************
- *  get string .....
- ******************************************/
+*  get string .....
+******************************************/
 
 char *get_sci_data_strings(int n)
 {
-  return dataStrings[Max(Min(n,MAXDATA),0)];
+    return dataStrings[Max(Min(n,MAXDATA),0)];
 }
 
 
