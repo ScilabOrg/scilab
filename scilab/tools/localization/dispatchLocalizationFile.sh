3e25f652f5754887e37e03d753c9751df12d30c4
Calixte DENIZET <calixte@contrib.scilab.org>
2010-11-18 17:18:20 +0100

    Check for single apos or single double quote inside translated strings
    Change-Id: I8d331474544883e77865ad6fb6880ec8c1316494

--- scilab/tools/localization/dispatchLocalizationFile.sh
+++ scilab/tools/localization/dispatchLocalizationFile.sh
@@ -9,7 +9,7 @@
 # http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
 #
 # This script takes a list of localization files dumped by launchpad
-# here https://translations.launchpad.net/scilab/trunk/+export 
+# here https://translations.launchpad.net/scilab/trunk/+export
 # and dispatch them into Scilab source tree
 
 if test $# -ne 1; then
@@ -22,7 +22,7 @@ if test $# -ne 1; then
 fi
 
 if test -z "$SCI"; then
-        echo "Please define the variable SCI" 
+        echo "Please define the variable SCI"
         exit -2
 fi
 
@@ -37,7 +37,7 @@ fi
 FILES=$(find $LAUNCHPAD_DIRECTORY/ -mindepth 2 -iname '*.po' -type f)
 /bin/cp -fiu $FILES $LAUNCHPAD_DIRECTORY/
 
-for file in $LAUNCHPAD_DIRECTORY/*.po; do 
+for file in $LAUNCHPAD_DIRECTORY/*.po; do
     file=`echo $file|awk -F / '{print $NF}'` # get only the filename
     LOC=`echo $file|cut -d. -f1|awk -F - '{print $NF}'` # Get the locale (fr_FR, en_US ...)
 
@@ -72,6 +72,14 @@ for file in $LAUNCHPAD_DIRECTORY/*.po; do
         if test $? -ne 0; then
             echo "Error detected in the copy"
             exit 1;
+        fi
+
+        # Check if the file contains single apos or single double quote
+        G=`/bin/grep "\"[^'\"]*\(['\"][^'\"]\).*\"" $DIR/$MODULE.po`
+        if [ -n "$G" ]
+        then
+            echo "An error of ' or \" in file $DIR/$MODULE.po"
+            echo $G
         fi
     else
         echo "Ignore locale $LOC"
