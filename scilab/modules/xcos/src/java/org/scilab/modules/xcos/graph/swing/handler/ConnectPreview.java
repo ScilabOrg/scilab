bca41ad88a9eb7bd857ff6a2b47e2a4cdf633c37
Clément DAVID <clement.david@scilab.org>
2010-11-29 14:28:53 +0100

    Xcos: fix a split block creation from a link. 	Starting a split from a link was not possible anymore.
    Bug: 8556
    Change-Id: I7dc431a78586f34bafc1681275ff3dc05330753b

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/graph/swing/handler/ConnectPreview.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/graph/swing/handler/ConnectPreview.java
@@ -14,9 +14,16 @@ package org.scilab.modules.xcos.graph.swing.handler;
 
 import java.awt.event.MouseEvent;
 
+import org.scilab.modules.xcos.block.SplitBlock;
+import org.scilab.modules.xcos.graph.XcosDiagram;
 import org.scilab.modules.xcos.graph.swing.GraphComponent;
+import org.scilab.modules.xcos.link.BasicLink;
+import org.scilab.modules.xcos.port.BasicPort;
 
 import com.mxgraph.model.mxICell;
+import com.mxgraph.util.mxConstants;
+import com.mxgraph.util.mxEvent;
+import com.mxgraph.util.mxEventObject;
 import com.mxgraph.view.mxCellState;
 import com.mxgraph.view.mxGraph;
 
@@ -27,7 +34,7 @@ import com.mxgraph.view.mxGraph;
  * @see <a href="http://www.jgraph.org/bugzilla/show_bug.cgi?id=47">http://www.jgraph.org/bugzilla/show_bug.cgi?id=47</a>
  */
 public class ConnectPreview extends com.mxgraph.swing.handler.mxConnectPreview {
-
+	
 	/**
 	 * Default constructor
 	 * 
@@ -52,5 +59,73 @@ public class ConnectPreview extends com.mxgraph.swing.handler.mxConnectPreview {
 		((mxICell) startState.getCell()).insertEdge(cell, true);
 
 		return cell;
+	}
+	
+	/**
+	 * Starting a new connection by setting sourceState, startPoint and previewState.
+	 * 
+	 * @param e the mouse event
+	 * @param startState the cell state
+	 * @param style the style of the edge
+	 * @see com.mxgraph.swing.handler.mxConnectPreview#start(java.awt.event.MouseEvent, com.mxgraph.view.mxCellState, java.lang.String)
+	 */
+	@Override
+	public void start(MouseEvent e, mxCellState startState, String style) {
+		final XcosDiagram graph = (XcosDiagram) graphComponent.getGraph();
+		final ConnectionHandler handler = (ConnectionHandler) graphComponent.getConnectionHandler();
+		
+		// Split case
+		if (startState.getCell() instanceof BasicLink)  {
+			BasicLink lnk = (BasicLink) startState.getCell();
+			handler.setResetEnable(false);
+			SplitBlock split = graph.addSplitEdge(transformScreenPoint(e.getPoint().getX(), e.getPoint().getY()), lnk);
+			handler.setResetEnable(true);
+			super.start(e, graph.getView().getState(split), style);
+		} else {
+			super.start(e, startState, style);
+		}
+	}
+	
+	/**
+	 * Stop the current link creation
+	 * @param commit true if any modification should be performed 
+	 * @param e the mouse event
+	 */
+	@Override
+	public Object stop(boolean commit, MouseEvent e) {
+		if (!commit && sourceState != null && sourceState.getCell() instanceof SplitBlock) {
+			final XcosDiagram graph = (XcosDiagram) graphComponent.getGraph();
+			final SplitBlock block = (SplitBlock) sourceState.getCell();
+			
+			BasicPort src = null;
+			BasicPort trg = null;
+			if (block.getIn().getEdgeCount() > 0) {
+				src = (BasicPort) block.getIn().getEdgeAt(0).getTerminal(true);
+			}
+			if (block.getIn().getEdgeCount() > 0) {
+				trg = (BasicPort) block.getOut1().getEdgeAt(0).getTerminal(false);
+			}
+			
+			graph.getModel().beginUpdate();
+			try {
+				graph.removeCells(new Object[] {block}, true);
+				
+				if (src != null && trg != null) {
+					graph.connect(src, trg, null);
+				}
+			} finally {
+				graph.getModel().endUpdate();
+			}
+			
+			Object ret = super.stop(false, e);
+			
+			if (src != null && trg != null) {
+				return src.getEdgeAt(0); // the new link
+			} else {
+				return ret;
+			}
+		} else {
+			return super.stop(commit, e);
+		}
 	}
 }
