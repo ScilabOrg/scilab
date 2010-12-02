deee5e539d4b7efff5ce240f3d1161e9a6975c0d
Calixte DENIZET <calixte@contrib.scilab.org>
2010-12-02 23:19:25 +0100

    SciNotes: Refactorize the printing and add an Export Action (pdf,ps,eps,rtf)
    Change-Id: I20a38399cfe0f3512047a68fb0388eef8c6d6cb5

--- scilab/modules/scinotes/src/java/org/scilab/modules/scinotes/utils/ConfigSciNotesManager.java
+++ scilab/modules/scinotes/src/java/org/scilab/modules/scinotes/utils/ConfigSciNotesManager.java
@@ -16,6 +16,7 @@ package org.scilab.modules.scinotes.utils;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Toolkit;
+import java.awt.print.Paper;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -139,6 +140,12 @@ public final class ConfigSciNotesManager {
     private static final String FAVORITE_DIRS = "favoriteDirectories";
     private static final String DIRECTORY = "Directory";
 
+    private static final String PAPER = "PaperFormat";
+    private static final String MARGINLEFT = "MarginLeft";
+    private static final String MARGINRIGHT = "MarginRight";
+    private static final String MARGINTOP = "MarginTop";
+    private static final String MARGINBOTTOM = "MarginBottom";
+
     private static final String SCI = "SCI";
     private static final String SCINOTES_CONFIG_FILE = System.getenv(SCI) + "/modules/scinotes/etc/scinotesConfiguration.xml";
     private static final String SCINOTES_CONFIG_KEYS_FILE = System.getenv(SCI) + "/modules/scinotes/etc/keysConfiguration.xml";
@@ -235,7 +242,6 @@ public final class ConfigSciNotesManager {
 
     public static List<String> getAllStyleName() {
         List<String> stylesName = new ArrayList<String>();
-
         readDocument();
 
         Element root = document.getDocumentElement();
@@ -243,10 +249,7 @@ public final class ConfigSciNotesManager {
 
         for (int i = 0; i < styles.getLength(); ++i) {
             Element style = (Element) styles.item(i);
-
             stylesName.add(style.getAttribute(NAME));
-
-
         }
         return stylesName;
     }
@@ -256,7 +259,6 @@ public final class ConfigSciNotesManager {
      * @return the name of the font
      */
     public static String getFontName() {
-
         /*load file*/
         readDocument();
 
@@ -267,8 +269,8 @@ public final class ConfigSciNotesManager {
 
         NodeList fontNameElement = scinotesProfile.getElementsByTagName(FONT_NAME);
         Element fontName = (Element) fontNameElement.item(0);
-        return fontName.getAttribute(VALUE);
 
+        return fontName.getAttribute(VALUE);
     }
 
     /**
@@ -323,12 +325,80 @@ public final class ConfigSciNotesManager {
         NodeList allSizeElements = scinotesProfile.getElementsByTagName(HELPONTYPING + type);
         Element helpOnTyping = (Element) allSizeElements.item(0);
         if (helpOnTyping == null) {
-            Element help = document.createElement(HELPONTYPING + type);
+            helpOnTyping = document.createElement(HELPONTYPING + type);
             helpOnTyping.setAttribute(VALUE, new Boolean(activated).toString());
-            helpOnTyping.appendChild((Node) help);
+            scinotesProfile.appendChild((Node) helpOnTyping);
         } else {
             helpOnTyping.setAttribute(VALUE, new Boolean(activated).toString());
         }
+        writeDocument();
+    }
+
+    /**
+     * @return the paper format saved in previous session
+     */
+    public static Paper getPaperFormat() {
+        readDocument();
+
+        Element root = document.getDocumentElement();
+
+        NodeList profiles = root.getElementsByTagName(PROFILE);
+        Element scinotesProfile = (Element) profiles.item(0);
+
+        NodeList allSizeElements = scinotesProfile.getElementsByTagName(PAPER);
+        Element paper = (Element) allSizeElements.item(0);
+
+        if (paper == null) {
+            return new Paper();
+        }
+
+        Paper p = new Paper();
+        double width = Double.parseDouble(paper.getAttribute(WIDTH));
+        double height = Double.parseDouble(paper.getAttribute(HEIGHT));
+        double marginLeft = Double.parseDouble(paper.getAttribute(MARGINLEFT));
+        double marginRight = Double.parseDouble(paper.getAttribute(MARGINRIGHT));
+        double marginTop = Double.parseDouble(paper.getAttribute(MARGINTOP));
+        double marginBottom = Double.parseDouble(paper.getAttribute(MARGINBOTTOM));
+        p.setSize(width, height);
+        p.setImageableArea(marginLeft, marginTop, width - (marginLeft + marginRight), height - (marginTop + marginBottom));
+
+        return p;
+    }
+
+    /**
+     * Save the paper format
+     * @param p the Paper to save
+     */
+    public static void savePaperFormat(Paper p) {
+        readDocument();
+
+        Element root = document.getDocumentElement();
+
+        NodeList profiles = root.getElementsByTagName(PROFILE);
+        Element scinotesProfile = (Element) profiles.item(0);
+
+        NodeList allSizeElements = scinotesProfile.getElementsByTagName(PAPER);
+        Element paper = (Element) allSizeElements.item(0);
+
+        if (paper == null) {
+            paper = document.createElement(PAPER);
+            scinotesProfile.appendChild((Node) paper);
+        }
+
+        double width = p.getWidth();
+        double height = p.getHeight();
+        double marginLeft = p.getImageableX();
+        double marginRight = width - (marginLeft + p.getImageableWidth());
+        double marginTop = p.getImageableY();
+        double marginBottom = height - (marginTop + p.getImageableHeight());
+
+        paper.setAttribute(WIDTH, Double.toString(width));
+        paper.setAttribute(HEIGHT, Double.toString(height));
+        paper.setAttribute(MARGINLEFT, Double.toString(marginLeft));
+        paper.setAttribute(MARGINRIGHT, Double.toString(marginRight));
+        paper.setAttribute(MARGINTOP, Double.toString(marginTop));
+        paper.setAttribute(MARGINBOTTOM, Double.toString(marginBottom));
+
         writeDocument();
     }
 
@@ -364,9 +434,9 @@ public final class ConfigSciNotesManager {
         NodeList allSizeElements = scinotesProfile.getElementsByTagName(LINENUMBERING);
         Element lineNumbering = (Element) allSizeElements.item(0);
         if (lineNumbering == null) {
-            Element line = document.createElement(LINENUMBERING);
+            lineNumbering = document.createElement(LINENUMBERING);
             lineNumbering.setAttribute(VALUE, Integer.toString(state));
-            lineNumbering.appendChild((Node) line);
+            scinotesProfile.appendChild((Node) lineNumbering);
         } else {
             lineNumbering.setAttribute(VALUE, Integer.toString(state));
         }
@@ -405,9 +475,9 @@ public final class ConfigSciNotesManager {
         NodeList allSizeElements = scinotesProfile.getElementsByTagName(LINEHIGHLIGHTER);
         Element lineHighlighter = (Element) allSizeElements.item(0);
         if (lineHighlighter == null) {
-            Element line = document.createElement(LINEHIGHLIGHTER);
+            lineHighlighter = document.createElement(LINEHIGHLIGHTER);
             lineHighlighter.setAttribute(VALUE, Boolean.toString(state));
-            lineHighlighter.appendChild((Node) line);
+            scinotesProfile.appendChild((Node) lineHighlighter);
         } else {
             lineHighlighter.setAttribute(VALUE, Boolean.toString(state));
         }
@@ -970,9 +1040,7 @@ public final class ConfigSciNotesManager {
         Element scinotesAutoIndent = (Element) allSizeElements.item(0);
         if (scinotesAutoIndent == null) {
             Element autoIndent = document.createElement(AUTOINDENT);
-
             autoIndent.setAttribute(VALUE, new Boolean(activated).toString());
-
             scinotesProfile.appendChild((Node) autoIndent);
         } else {
             scinotesAutoIndent.setAttribute(VALUE, new Boolean(activated).toString());
@@ -1281,8 +1349,7 @@ public final class ConfigSciNotesManager {
 
             String rgb = Integer.toHexString(color.getRGB());
             styleForeground.setAttribute(VALUE, COLORPREFIX + rgb.substring(2, rgb.length()));
-
-
+            clean(styleForeground);
         }
         /* Save changes */
         writeDocument();
@@ -1317,6 +1384,7 @@ public final class ConfigSciNotesManager {
             }
 
             fontStyle.setAttribute(VALUE, Integer.toString(bold + italic));
+            clean(fontStyle);
         }
         /* Save changes */
         writeDocument();
@@ -1349,6 +1417,7 @@ public final class ConfigSciNotesManager {
 
             style.setAttribute(UNDERLINE, underline);
             style.setAttribute(STROKE, stroke);
+            clean(style);
         }
         /* Save changes */
         writeDocument();
