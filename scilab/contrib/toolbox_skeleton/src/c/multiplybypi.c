e99831795680c399bea869434447e6d8f92a2d47
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2011-01-17 09:21:01 +0100

    * New example added (function with copy instead of reference)
    Change-Id: Ia021f101baaaafdc91bbeab1d9a8a1c0c2e4cd02

---
+++ scilab/contrib/toolbox_skeleton/src/c/multiplybypi.c
@@ -0,0 +1,13 @@
+/* ==================================================================== */
+/* 2011 - Digiteo - Sylvestre LEDRU
+/* Template toolbox_skeleton */
+/* This file is released under the 3-clause BSD license. See COPYING-BSD. */
+/* ==================================================================== */
+#include <math.h>
+#include "multiplybypi.h"
+/* ==================================================================== */
+double multiplybypi(double a)
+{
+	return a*M_PI;
+}
+/* ==================================================================== */
