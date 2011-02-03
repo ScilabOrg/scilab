22fb83b591bafdb25c4f9c09b34f6fec695f294d
Bruno JOFRET <bruno.jofret@scilab.org>
2011-02-03 13:56:46 +0100

    Change MVC getGraphicObjectProperty prototype to avoid static variable referencing.
    Change-Id: I2586943d80f42c5b6fa821180ad2b659a21684b2

--- scilab/modules/graphics/src/c/getHandleProperty/get_color_range_property.c
+++ scilab/modules/graphics/src/c/getHandleProperty/get_color_range_property.c
@@ -2,11 +2,11 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2009 - Digiteo - Jean-Baptiste Silvy
  * Copyright (C) 2010 - DIGITEO - Manuel Juliachs
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -30,7 +30,8 @@
 /*------------------------------------------------------------------------*/
 int get_color_range_property( sciPointObj * pobj )
 {
-    int* range;
+    int iRange = 0;
+    int* piRange = &iRange;
 #if 0
     if (sciGetEntityType (pobj) != SCI_FEC)
     {
@@ -39,14 +40,14 @@ int get_color_range_property( sciPointObj * pobj )
     }
 #endif
 
-    range = (int*) getGraphicObjectProperty(pobj->UID, __GO_COLOR_RANGE__, jni_int_vector);
+    getGraphicObjectProperty(pobj->UID, __GO_COLOR_RANGE__, jni_int_vector, &piRange);
 
-    if (range == NULL)
+    if (piRange == NULL)
     {
         Scierror(999, _("'%s' property does not exist for this handle.\n"),"color_range");
         return -1;
     }
 
-    return sciReturnRowVectorFromInt( range, 2 );
+    return sciReturnRowVectorFromInt( iRange, 2 );
 }
 /*------------------------------------------------------------------------*/
