c74d2cefc93bdb47c5969061e2277375191f27d9
Jerzy ZAGORSKI <xendoo@gmail.com>
2011-03-06 22:44:49 +0100

    Added initial Pattern Binding error handling
    Change-Id: Id8984622c0eecd537decc91bb689b61d47753d69

---
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/simulink/PatternBindingException.java
@@ -0,0 +1,8 @@
+package org.scilab.modules.xcos.simulink;
+
+@SuppressWarnings("serial")
+public class PatternBindingException extends Exception {
+	public PatternBindingException(String message) {
+		super(message);
+	}
+}
