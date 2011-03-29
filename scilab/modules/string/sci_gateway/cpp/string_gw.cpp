3a5623cbef48f6b91d2110b83214c7c44b5f0120
cedric Delamarre <cedric.delamarre@scilab.org>
2011-03-29 14:20:32 +0200

    Added tokens strtok strspn strcspn isletter
    Change-Id: I35412e51d17e72eb7beb455fa0eb083149fa002d

--- scilab/modules/string/sci_gateway/cpp/string_gw.cpp
+++ scilab/modules/string/sci_gateway/cpp/string_gw.cpp
@@ -34,10 +34,18 @@ bool StringModule::Load()
     symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"convstr", &sci_convstr, MODULE_NAME));
     symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"strncpy", &sci_strncpy, MODULE_NAME));
     symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"str2code", &sci_str2code, MODULE_NAME));
+    symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"code2str", &sci_str2code, MODULE_NAME));
     symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"strrchr", &sci_strrchr, MODULE_NAME));
     symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"strchr", &sci_strchr, MODULE_NAME));
     symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"strstr", &sci_strstr, MODULE_NAME));
     symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"strrev", &sci_strrev, MODULE_NAME));
     symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"strtod", &sci_strtod, MODULE_NAME));
+        symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"tokens", &sci_tokens, MODULE_NAME));
+        symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"strcmp", &sci_strcmp, MODULE_NAME));
+        symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"isletter", &sci_isletter, MODULE_NAME));
+        symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"strcspn", &sci_strcspn, MODULE_NAME));
+        symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"strtok", &sci_strtok, MODULE_NAME));
+        symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"strspn", &sci_strspn, MODULE_NAME));
+                
     return true;
 }
