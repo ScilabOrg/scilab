6e35535e954c0905408da120495280ab3b436f07
cedric Delamarre <cedric.delamarre@scilab.org>
2011-04-13 09:31:15 +0200

    mfprintf, mfscanf, mscanf, msscanf added.
    Change-Id: If5587ae1dfd5aaeb5505bc43cb791dfeee8b1b32

--- scilab/modules/fileio/includes/fileio_gw.hxx
+++ scilab/modules/fileio/includes/fileio_gw.hxx
@@ -62,6 +62,9 @@ CPP_GATEWAY_PROTOTYPE(sci_mputstr);
 CPP_GATEWAY_PROTOTYPE(sci_mgetstr);
 CPP_GATEWAY_PROTOTYPE(sci_mseek);
 CPP_GATEWAY_PROTOTYPE(sci_mtell);
-
+CPP_GATEWAY_PROTOTYPE(sci_mfprintf);
+CPP_GATEWAY_PROTOTYPE(sci_mscanf);
+CPP_GATEWAY_PROTOTYPE(sci_mfscanf);
+CPP_GATEWAY_PROTOTYPE(sci_msscanf);
 
 #endif /* !__FILEIO_GW_HXX__ */
