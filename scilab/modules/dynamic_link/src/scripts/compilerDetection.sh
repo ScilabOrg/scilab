6d3af9e27cc28ecfb94a937f6da3caf18ed08d7a
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2011-05-11 16:43:08 +0200

    * bug 7887 fixed - Under some GNU/Linux distributions (like Ubuntu or Mageia),                    the dynamic link process was using the libstdc++ embedded                    into the Scilab distribution. Now, if the compiler is gcc,                    the dynamic link guesses the libstdc++ path and use it                    uppermost.
    Change-Id: I74b0563f5a71bce668f250b15f82882594bd1bb9

--- scilab/modules/dynamic_link/src/scripts/compilerDetection.sh
+++ scilab/modules/dynamic_link/src/scripts/compilerDetection.sh
@@ -3,12 +3,28 @@
 PROGNAME="$0"
 PATHTOCONFIGURE=`dirname "$PROGNAME"`
 cd $PATHTOCONFIGURE
+if test -x "$(which gcc 2>/dev/null)"; then
+    # Detect the actual path to the libstdc++ library. For the dynamic link
+    # build, we want to use the same lib as the compiler installed.
+    # CF bug #7887 for more information.
+    # Note that, for the Makefile, the setup is done in the Scilab macros
+    # ilib_compile
+    GCClibpath=$(gcc -print-search-dirs|grep ^install:|awk '{print $2}')/../../../
+    echo "GCClibpath $GCClibpath" >> /tmp/plop2KKKK
+
+    if test -z "$(grep $GCClibpath $LD_LIBRARY_PATH 2>/dev/null)"; then
+        LD_LIBRARY_PATH="$GCClibpath${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+        export LD_LIBRARY_PATH
+	echo "aze $LD_LIBRARY_PATH" >> /tmp/plop2KKKK
+    fi
+fi
+echo "bze $LD_LIBRARY_PATH" >> /tmp/plop2KKKK
 
 # Relaunch configure if files are missing
 if test ! -s Makefile.orig -o ! -s libtool; then 
-	echo "Detection of C/C++/Fortran Compilers"
-	./configure --disable-static --disable-dependency-tracking "$@"
-	mv Makefile Makefile.orig
+    echo "Detection of C/C++/Fortran Compilers"
+    ./configure --disable-static --disable-dependency-tracking "$@"
+    mv Makefile Makefile.orig
 else 
-	echo "Detection of compilers already done"
+    echo "Detection of compilers already done"
 fi
