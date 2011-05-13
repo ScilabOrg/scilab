a145e611e49e4397c7b3b6e256078f85c6cedd24
Calixte DENIZET <calixte@contrib.scilab.org>
2011-05-13 12:55:33 +0200

    Bug 9480 fixed: Highlighted words after a selection were not correct
    Change-Id: Ib94c3632e24edfc7e95d6fb5d2d26d8507709f79

--- scilab/modules/scinotes/src/java/org/scilab/modules/scinotes/ScilabEditorPane.java
+++ scilab/modules/scinotes/src/java/org/scilab/modules/scinotes/ScilabEditorPane.java
@@ -1362,7 +1362,7 @@ public class ScilabEditorPane extends JEditorPane implements Highlighter.Highlig
     public void highlightWords(String word, boolean exact) {
         removeHighlightedWords();
         if (word != null && word.length() != 0) {
-            String text = getText();
+            String text = ((ScilabDocument) getDocument()).getText();
             if (!exact) {
                 text = text.toLowerCase();
                 word = word.toLowerCase();
@@ -1390,7 +1390,7 @@ public class ScilabEditorPane extends JEditorPane implements Highlighter.Highlig
         if (pattern != null) {
             removeHighlightedWords();
             int first = -1;
-            String text = getText();
+            String text = ((ScilabDocument) getDocument()).getText();
             Matcher matcher = pattern.matcher(text);
 
             Highlighter highlighter = getHighlighter();
