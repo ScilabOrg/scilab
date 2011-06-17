6fadcf9ebda6d1e29e2bbda4f26ec22b19e28fd7
Pierre LANDO <pierre.lando@scilab.org>
2011-06-17 15:18:56 +0200

    Improve Graphic interaction.
    - Add zoom interaction.
    - Add translation interaction XY mode and Z mode.
    - Add some comments.
    - Axes drawing now use 'zoomBounds' and 'zoomEnable' properties.
    
    Change-Id: I4e1a37d4dcbdc6bf0fb47d83545b6ea26f189931

--- scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/axes/AxesDrawer.java
+++ scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/axes/AxesDrawer.java
@@ -107,7 +107,7 @@ public class AxesDrawer {
         modelViewStack.pushRightMultiply(dataTransformation);
 
         // TODO implement clipgrf
-        Double[] bounds = axes.getDataBounds();
+        Double[] bounds = getCurrentBounds(axes);
         Vector4d[] equations = new Vector4d[]{
                 new Vector4d(+1, 0, 0, -bounds[0]),
                 new Vector4d(-1, 0, 0, +bounds[1]),
@@ -242,6 +242,19 @@ public class AxesDrawer {
         return zoneTranslation.rightTimes(zoneScale);
     }
 
+    /**
+     * Return the current visible bounds of the given axes.
+     * // TODO : tight limit.
+     * @param axes the given axes.
+     * @return the current visible bounds of the given axes.
+     */
+    private Double[] getCurrentBounds(Axes axes) {
+        if (axes.getZoomEnabled()) {
+            return axes.getZoomBox();
+        } else {
+            return axes.getDataBounds();
+        }
+    }
 
     /**
      * Compute data transformation for the given {@see Axes}.
@@ -252,7 +265,7 @@ public class AxesDrawer {
      * @return data transformation.
      */
     private Transformation computeDataTransformation(Axes axes) {
-        Double[] bounds = axes.getDataBounds();
+        Double[] bounds = getCurrentBounds(axes);
 
         // Reverse data if needed.
         Transformation transformation = TransformationFactory.getScaleTransformation(
@@ -291,8 +304,7 @@ public class AxesDrawer {
      * @return box transformation for the given axes.
      */
     private Transformation computeBoxTransformation(Axes axes, Canvas canvas) {
-        // TODO : zoom box ?
-        Double[] bounds = axes.getDataBounds();
+        Double[] bounds = getCurrentBounds(axes);
 
         double tmpX;
         double tmpY;
