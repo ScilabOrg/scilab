7d7f48eaa7e3f403598692ec815b3cdf1ced7c73
Manuel Juliachs <manuel.juliachs@scilab.org>
2011-06-28 14:45:47 +0200

    -sci_plot2d and plot2d initialization functions corrected  (sciPointObj* replaced by identifiers).
    -cloneGraphicContext call added to allocatePolyline to initialize
     its default Contour properties.
    
    Change-Id: Ib3eeb4f668a7fd0f99fcfb4b8f64c1231bce2d5f

--- scilab/modules/graphics/src/c/BuildObjects.c
+++ scilab/modules/graphics/src/c/BuildObjects.c
@@ -1088,6 +1088,7 @@ char* allocatePolyline(char* pparentsubwinUID, double *pvecx, double *pvecy, dou
      * Initializes the contour properties (background, foreground, etc)
      * to the default values (those of the parent Axes).
      */
+    cloneGraphicContext(pparentsubwinUID, pobjUID);
 
     // FIXME ???
     //sciInitGraphicContext(pobj);
