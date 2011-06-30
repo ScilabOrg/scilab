a5098aca50124ff24647bb3fad74f1b642a3939e
Manuel Juliachs <manuel.juliachs@scilab.org>
2011-06-30 11:50:36 +0200

    ConstructCompoundSeq function corrected (sciPointObj* replaced by identifiers).
    Change-Id: I11968a4da60ad3717007d0d633215d233311338e

--- scilab/modules/graphics/includes/BuildObjects.h
+++ scilab/modules/graphics/includes/BuildObjects.h
@@ -99,7 +99,7 @@ GRAPHICS_IMPEXP sciPointObj *ConstructSegs (sciPointObj * pparentsubwin, int typ
               int *style, double arsize,  int colored, int typeofchamp);  /* BUILD */
 
 GRAPHICS_IMPEXP char *ConstructCompound (long *tabpointobj, int number); /* BUILD */
-GRAPHICS_IMPEXP sciPointObj *ConstructCompoundSeq (int number); /* BUILD */
+GRAPHICS_IMPEXP char *ConstructCompoundSeq (int number); /* BUILD */
 
 GRAPHICS_IMPEXP sciPointObj * ConstructLabel (sciPointObj * pparentsubwin, char *text, int type); /* BUILD */
 
