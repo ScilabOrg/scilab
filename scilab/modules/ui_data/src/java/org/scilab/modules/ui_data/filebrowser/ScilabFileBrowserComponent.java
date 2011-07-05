0dfe78cc431b3010e387c06b360e1f3b0e020ee6
Calixte DENIZET <calixte.denizet@scilab.org>
2011-07-05 18:08:07 +0200

    File Browser: work in progress...
    Change-Id: Ie371c797102b2299e6ceff9e77a1531f6606d6ef

---
+++ scilab/modules/ui_data/src/java/org/scilab/modules/ui_data/filebrowser/ScilabFileBrowserComponent.java
@@ -0,0 +1,51 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2011 - DIGITEO - Calixte DENIZET
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+package org.scilab.modules.ui_data.filebrowser;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JViewport;
+import javax.swing.SwingUtilities;
+
+import org.scilab.modules.gui.events.callback.CallBack;
+import org.scilab.modules.ui_data.filebrowser.actions.ChangeCWDAction;
+
+public class ScilabFileBrowserComponent extends JPanel {
+
+    private SwingScilabFileBrowser filebrowser;
+    private SwingScilabTreeTable stt;
+
+    public ScilabFileBrowserComponent() {
+	super(new BorderLayout());
+	ScilabFileSelectorComboBox combobox = new ScilabFileSelectorComboBox();
+	stt = new SwingScilabTreeTable(new ScilabFileBrowserModel(), combobox);
+	add(combobox, BorderLayout.PAGE_START);
+	JScrollPane jsp = new JScrollPane(stt);
+	jsp.getViewport().setScrollMode(JViewport.BACKINGSTORE_SCROLL_MODE);
+	add(jsp, BorderLayout.CENTER);
+    }
+
+    public void setBaseDir(final String baseDir) {
+	SwingUtilities.invokeLater(new Runnable() {
+		public void run() {
+		    stt.setBaseDir(baseDir);
+		}
+	    });
+    }
+
+    public SwingScilabFileBrowser getFileBrowser() {
+	return filebrowser;
+    }
+}
