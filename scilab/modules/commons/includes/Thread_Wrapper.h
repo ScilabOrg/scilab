cf474d7b445a977aa4bf7c0dcb2fee3e4cccb7b1
Bruno JOFRET <bruno.jofret@scilab.org>
2011-07-11 09:47:18 +0200

    Split Thread wrapping functions. Move Threads to common module. Increase CallStack under:     - MacosX up to basic Linux 64b value.     - Linux to what seems to be the max possible one.
    Change-Id: Ie81c7c6d2d4bb87748fd532616355244fae95049

---
+++ scilab/modules/commons/includes/Thread_Wrapper.h
@@ -0,0 +1,96 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *  Copyright (C) 2008-2008 - INRIA - Allan CORNET
+ *  Copyright (C) 2011-2011 - DIGITEO - Bruno JOFRET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+/*
+** Thread wrapper to have an easy way to manage
+** those features on each platform
+*/
+
+#ifndef __THREAD_WRAPPER_H__
+#define __THREAD_WRAPPER_H__
+
+
+/*
+** WINDOWS
+*/
+#ifdef _MSC_VER
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x500
+#endif
+#include <Windows.h>
+#include <process.h>
+
+typedef DWORD                           __threadKey;
+typedef HANDLE                          __threadId;
+typedef HANDLE                          __threadLock;
+typedef CRITICAL_SECTION                __threadSignalLock;
+typedef HANDLE                          __threadSignal;
+#define __StaticInitLock                NULL
+
+#define __StaticInitThreadSignal        NULL
+
+#else
+/*
+** LINUX & Mac
+*/
+#include <pthread.h>
+#include <signal.h>
+#include <stdlib.h> // malloc
+
+typedef pthread_t                       __threadKey;
+typedef pthread_t                       __threadId;
+typedef pthread_mutex_t                 __threadLock;
+typedef pthread_mutex_t                 __threadSignalLock;
+typedef pthread_cond_t                  __threadSignal;
+#define __StaticInitLock                PTHREAD_MUTEX_INITIALIZER
+
+#define __StaticInitThreadSignal        PTHREAD_COND_INITIALIZER
+#endif
+
+void __InitLock(__threadLock *lockName);
+
+void __Lock(__threadLock *lockName);
+
+void __UnLock(__threadLock *lockName);
+
+void __InitSignalLock(__threadLock *lockName);
+
+void __LockSignal(__threadSignalLock *lockName);
+
+void __UnLockSignal(__threadSignalLock *lockName);
+
+void __InitSignal(__threadSignal *signalName);
+
+void __Signal(__threadSignal *signalName);
+
+void __Wait(__threadSignal *signalName, __threadSignalLock *lockName);
+
+void __CreateThread(__threadId *threadId, __threadKey *threadKey, void *(*functionName) (void *));
+
+void __CreateThreadWithParams(__threadId *threadId, __threadKey *threadKey, void *(*functionName) (void *), void *params);
+
+void __WaitThreadDie(__threadId threadId);
+
+void __Terminate(__threadId threadId);
+
+__threadId __GetCurrentThreadId();
+
+__threadKey __GetCurrentThreadKey();
+
+void __SuspendThread(__threadId ThreadId);
+
+void __ResumeThread(__threadId ThreadId);
+
+#endif /* !__THREAD_WRAPPER_H__ */
+
