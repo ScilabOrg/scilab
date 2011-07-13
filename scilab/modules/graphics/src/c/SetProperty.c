48079a8cb863abffbe4ca6c05c82c67c8bf70607
Manuel Juliachs <manuel.juliachs@scilab.org>
2011-07-13 10:59:14 +0200

    sci_newaxes and ConstructSubWin functions corrected:
    -sciPointObj* replaced by identifiers.
    
    -obsolete initialization code removed from ConstructSubWin.
    
    -sciSetSelectedSubWin function cleaned up.
    
    Change-Id: I6d0b8f74a248ea70fbbf1c8507cbde27eff95442

--- scilab/modules/graphics/src/c/SetProperty.c
+++ scilab/modules/graphics/src/c/SetProperty.c
@@ -2325,25 +2325,7 @@ sciSetSelectedSubWin (char * psubwinobjUID)
 
     setGraphicObjectProperty(parent, __GO_SELECTED_CHILD__, psubwinobjUID, jni_string, 1);
 
-    /* To be deleted */
-#if 0
-    /* on verifie que l'entite passee en argument est bien une sous fenetre */
-    if (sciGetEntityType (psubwinobj) != SCI_SUBWIN)
-    {
-        Scierror(999, _("Handle is not a SubWindow.\n"));
-        return -1;
-    }
-
-    /* on verifie que la sous fenetre donnee n'est pas deja selectionnee */
-    /* No need to check anymore, as we set it anyway. */
-    if (sciGetIsSelected(psubwinobj))
-    {
-        /* nothing to do then */
-        return 1 ;
-    }
-
-    return sciInitSelectedSubWin( psubwinobj );
-#endif
+    return 0;
 }
 
 /*-------------------------------------------------------------------------------*/
