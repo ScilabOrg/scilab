20a07d9b04acd680637d3915e82ce194d83edce4
Calixte DENIZET <calixte.denizet@scilab.org>
2011-09-14 18:23:08 +0200

    Console: Improve the rendering
    Change-Id: I6da12c07be5b5ebeb7c5f4b821df7370b2fe6258

--- scilab/modules/console/src/java/org/scilab/modules/console/SciInputCommandView.java
+++ scilab/modules/console/src/java/org/scilab/modules/console/SciInputCommandView.java
@@ -13,7 +13,9 @@
 
 package org.scilab.modules.console;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FontMetrics;
@@ -35,6 +37,12 @@ import javax.swing.event.CaretListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultCaret;
+import javax.swing.text.Element;
+import javax.swing.text.PlainView;
+import javax.swing.text.StyledEditorKit;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
 
 import org.scilab.modules.commons.gui.ScilabCaret;
 import org.scilab.modules.console.utils.ScilabLaTeXViewer;
@@ -50,7 +58,7 @@ import com.artenum.rosetta.util.StringConstants;
  * @author Vincent COUVERT
  * @author Calixte DENIZET
  */
-public class SciInputCommandView extends ConsoleTextPane implements InputCommandView, CaretListener {
+public class SciInputCommandView extends ConsoleTextPane implements InputCommandView, CaretListener, ViewFactory {
 
     private static final long serialVersionUID = 1L;
     private static final String END_LINE = "\n";
@@ -71,11 +79,20 @@ public class SciInputCommandView extends ConsoleTextPane implements InputCommand
     private int width = -1;
     private boolean isLatex;
 
+    private PlainView plainView;
+
     /**
      * Constructor
      */
     public SciInputCommandView() {
         super();
+
+        setEditorKit(new StyledEditorKit() {
+                public ViewFactory getViewFactory() {
+                    return SciInputCommandView.this;
+                }
+            });
+
         setBorder(BorderFactory.createEmptyBorder(TOP_BORDER, 0, BOTTOM_BORDER, 0));
 
         // Input command line is not editable when created
@@ -183,11 +200,10 @@ public class SciInputCommandView extends ConsoleTextPane implements InputCommand
 
                 public void insertUpdate(DocumentEvent e) {
                     // Validates commands if followed by a carriage return
-                    String wholeTxt = console.getConfiguration().getInputParsingManager().getCommandLine();
+                    final String wholeTxt = console.getConfiguration().getInputParsingManager().getCommandLine();
                     if ((e.getLength()) > 1 && (wholeTxt.lastIndexOf(StringConstants.NEW_LINE) == (wholeTxt.length() - 1))) {
                         EventQueue.invokeLater(new Runnable() {
                                 public void run() {
-                                    String wholeTxt = console.getConfiguration().getInputParsingManager().getCommandLine();
                                     console.sendCommandsToScilab(wholeTxt, true, true);
                                 };
                             });
@@ -201,7 +217,7 @@ public class SciInputCommandView extends ConsoleTextPane implements InputCommand
 
         this.addKeyListener(new KeyListener() {
                 public void keyPressed (KeyEvent e) {
-                    if (e.getKeyCode()==KeyEvent.VK_BACK_SPACE) {
+                    if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
                         if (console.getConfiguration().getHistoryManager().isInHistory()) {
                             //console.getConfiguration().getInputParsingManager().reset();
                             //console.getConfiguration().getInputParsingManager().append(console.getConfiguration().getHistoryManager().getTmpEntry());
@@ -261,6 +277,10 @@ public class SciInputCommandView extends ConsoleTextPane implements InputCommand
         }
     }
 
+    public int getCaretHeight() {
+        return ((ScilabCaret) getCaret()).height;
+    }
+
     /**
      * Set the height of this text component.
      * @param height the height, -1 to have the natural preferred height
@@ -297,5 +317,19 @@ public class SciInputCommandView extends ConsoleTextPane implements InputCommand
             dim.width = Math.max(width, dim.width);
         }
         return dim;
+    }
+
+    public void setForeground(Color fg) {
+        super.setForeground(fg);
+        setCaretColor(fg);
+        repaint();
+    }
+
+    public View create(Element e) {
+        return new PlainView(e) {
+            public Container getContainer() {
+                return SciInputCommandView.this;
+            }
+        };
     }
 }
