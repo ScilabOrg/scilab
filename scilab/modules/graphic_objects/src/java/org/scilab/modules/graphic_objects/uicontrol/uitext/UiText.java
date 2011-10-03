0a8b27f76b43a69499e893bc08e9065d8b397f0a
Vincent COUVERT <vincent.couvert@scilab.org>
2011-10-03 14:09:47 +0200

    Add relief properties in propertiesMap
    Change-Id: Id5ee1f4c3d53ed6fb967413179e059a9ac1b417b

--- scilab/modules/graphic_objects/src/java/org/scilab/modules/graphic_objects/uicontrol/uitext/UiText.java
+++ scilab/modules/graphic_objects/src/java/org/scilab/modules/graphic_objects/uicontrol/uitext/UiText.java
@@ -12,6 +12,7 @@
 
 package org.scilab.modules.graphic_objects.uicontrol.uitext;
 
+import static org.scilab.modules.graphic_objects.graphicObject.GraphicObjectProperties.__GO_UI_FLAT_RELIEF__;
 import static org.scilab.modules.graphic_objects.graphicObject.GraphicObjectProperties.__GO_UI_TEXT__;
 
 import org.scilab.modules.graphic_objects.uicontrol.Uicontrol;
@@ -27,7 +28,7 @@ public class UiText extends Uicontrol {
 	public UiText() {
 		super();
 		setStyle(__GO_UI_TEXT__);
-		setRelief("flat");
+		setRelief(__GO_UI_FLAT_RELIEF__);
 	}
 
 }
