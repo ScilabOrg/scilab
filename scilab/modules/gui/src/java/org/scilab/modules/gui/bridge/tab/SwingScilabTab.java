ScilabOrg/scilab

commit c84ef7736b1faeda8e8442604c5ee4fa580fe266

An exception was thrown on closing when historybrowser was docked as tab in the console
Change-Id: I91d8f6a5312914f66692794acbc13cf980b36f68

scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/tab/SwingScilabTab.java
@@ -1078,7 +1078,6 @@ public class SwingScilabTab extends View implements SimpleTab, FocusListener {
         setInfoBar(null);
         setTitlebar(null);
         removeAll();
-        setActive(false);
 
         scrolling = null;
         contentPane = null;

commit e218167b00eccd390e1077e5e6f1c626477ffd9d

Improve commit c84ef7736b1faeda8e8442604c5ee4fa580fe266 which introduced a new bug (close docked browsevar with internal cross and reopen it)
Change-Id: I48f6bfde0a6a458b69f6ac2616bcef37259c9ccf

scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/tab/SwingScilabTab.java
@@ -138,7 +138,7 @@ public class SwingScilabTab extends View implements SimpleTab, FocusListener {
 
         getTitlebar().addFocusListener(this);
         addFocusListener(this);
-	setCallback(null);
+        setCallback(null);
     }
 
     /**
@@ -168,7 +168,7 @@ public class SwingScilabTab extends View implements SimpleTab, FocusListener {
 
         getTitlebar().addFocusListener(this);
         addFocusListener(this);
-	setCallback(null);
+        setCallback(null);
     }
 
     /**
@@ -180,22 +180,22 @@ public class SwingScilabTab extends View implements SimpleTab, FocusListener {
     }
 
     public static void removeActions(SwingScilabTab tab) {
-	tab.setActionBlocked(DockingConstants.CLOSE_ACTION, true);
-	tab.setActionBlocked(UNDOCK, true);
-	tab.getTitlebar().revalidate();
+        tab.setActionBlocked(DockingConstants.CLOSE_ACTION, true);
+        tab.setActionBlocked(UNDOCK, true);
+        tab.getTitlebar().revalidate();
     }
 
     public static void addActions(SwingScilabTab tab) {
-	tab.setActionBlocked(DockingConstants.CLOSE_ACTION, false);
-	tab.setActionBlocked(UNDOCK, false);
-	tab.getTitlebar().revalidate();
+        tab.setActionBlocked(DockingConstants.CLOSE_ACTION, false);
+        tab.setActionBlocked(UNDOCK, false);
+        tab.getTitlebar().revalidate();
     }
 
     /**
      * @param e the FocusEvent
      */
     public void focusGained(FocusEvent e) {
-	//ActiveDockableTracker.requestDockableActivation(this);
+        //ActiveDockableTracker.requestDockableActivation(this);
         if (contentPane != null) {
             contentPane.requestFocus();
         } else if (getContentPane() != null) {
@@ -250,7 +250,7 @@ public class SwingScilabTab extends View implements SimpleTab, FocusListener {
                 }
             }
         } else {
-	    removeActions(this);
+            removeActions(this);
         }
     }
 
@@ -916,13 +916,13 @@ public class SwingScilabTab extends View implements SimpleTab, FocusListener {
         }
 
         action.putValue(Action.NAME, DockingConstants.CLOSE_ACTION);
-	((Titlebar) getTitlePane()).removeAction(DockingConstants.CLOSE_ACTION);
+        ((Titlebar) getTitlePane()).removeAction(DockingConstants.CLOSE_ACTION);
         addAction(action);
 
         /* Undock button */
         SciUndockingAction undockAction = new SciUndockingAction(this);
         undockAction.putValue(Action.NAME, UNDOCK);
-	((Titlebar) getTitlePane()).removeAction(UNDOCK);
+        ((Titlebar) getTitlePane()).removeAction(UNDOCK);
         addAction(undockAction);
     }
 
@@ -1078,6 +1078,7 @@ public class SwingScilabTab extends View implements SimpleTab, FocusListener {
         setInfoBar(null);
         setTitlebar(null);
         removeAll();
+        setActive(false);
 
         scrolling = null;
         contentPane = null;

scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/window/SwingScilabWindow.java
@@ -358,8 +358,8 @@ public class SwingScilabWindow extends JFrame implements SimpleWindow {
     public void removeTabs(SwingScilabTab[] tabs) {
         for (SwingScilabTab tab : tabs) {
             DockingManager.unregisterDockable((Dockable) tab);
-            DockingManager.close(tab);
             tab.close();
+            DockingManager.close(tab);
         }
         if (getDockingPort().getDockables().isEmpty()) {
             // remove xxxBars