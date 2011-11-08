fe53d88c78c904661fdea8bfad80611de728ae51
Calixte DENIZET <calixte.denizet@scilab.org>
2011-11-08 22:32:50 +0100

    XML: attributes can now be accessed via index
    Change-Id: I31b90dd32f73002aba2d36e7ea4e1bec38ef8d28

--- scilab/modules/xml/sci_gateway/cpp/sci_percent_XMLAttr_e.cpp
+++ scilab/modules/xml/sci_gateway/cpp/sci_percent_XMLAttr_e.cpp
@@ -35,6 +35,9 @@ int sci_percent_XMLAttr_e(char * fname, unsigned long fname_len)
     int id;
     SciErr err;
     int * prefixaddr = 0;
+    double * indexes = 0;
+    int rows;
+    int cols;
     int * nameaddr = 0;
     int * mlistaddr = 0;
     char * name = 0;
@@ -51,38 +54,53 @@ int sci_percent_XMLAttr_e(char * fname, unsigned long fname_len)
         return 0;
     }
 
-    if (!isStringType(pvApiCtx, prefixaddr))
+    if (Rhs == 2 && isDoubleType(pvApiCtx, prefixaddr))
     {
-        Scierror(999, gettext("%s: Wrong type for input argument #%i: A string expected.\n"), fname, 1);
-        return 0;
-    }
-
-    getAllocatedSingleString(pvApiCtx, prefixaddr, &prefix);
-
-    if (Rhs == 3)
-    {
-        err = getVarAddressFromPosition(pvApiCtx, 2, &nameaddr);
-        if (err.iErr)
+        err = getMatrixOfDouble(pvApiCtx, prefixaddr, &rows, &cols, &indexes);
+        if (rows != 1 || cols != 1)
         {
-            freeAllocatedSingleString(prefix);
-            printError(&err, 0);
+            Scierror(999, gettext("%s: Wrong type for input argument #%i: A string or a single integer expected\n"), fname, 1);
             return 0;
         }
-
-        if (!isStringType(pvApiCtx, nameaddr))
+    }
+    else
+    {
+        if (!isStringType(pvApiCtx, prefixaddr))
         {
-            freeAllocatedSingleString(prefix);
             Scierror(999, gettext("%s: Wrong type for input argument #%i: A string expected.\n"), fname, 1);
             return 0;
         }
 
-        getAllocatedSingleString(pvApiCtx, nameaddr, &name);
+        getAllocatedSingleString(pvApiCtx, prefixaddr, &prefix);
+
+        if (Rhs == 3)
+        {
+            err = getVarAddressFromPosition(pvApiCtx, 2, &nameaddr);
+            if (err.iErr)
+            {
+                freeAllocatedSingleString(prefix);
+                printError(&err, 0);
+                return 0;
+            }
+
+            if (!isStringType(pvApiCtx, nameaddr))
+            {
+                freeAllocatedSingleString(prefix);
+                Scierror(999, gettext("%s: Wrong type for input argument #%i: A string expected.\n"), fname, 1);
+                return 0;
+            }
+
+            getAllocatedSingleString(pvApiCtx, nameaddr, &name);
+        }
     }
 
     err = getVarAddressFromPosition(pvApiCtx, Rhs, &mlistaddr);
     if (err.iErr)
     {
-        freeAllocatedSingleString(prefix);
+        if (prefix)
+        {
+            freeAllocatedSingleString(prefix);
+        }
         if (name)
         {
             freeAllocatedSingleString(name);
@@ -96,7 +114,10 @@ int sci_percent_XMLAttr_e(char * fname, unsigned long fname_len)
 
     if (!attr)
     {
-        freeAllocatedSingleString(prefix);
+        if (prefix)
+        {
+            freeAllocatedSingleString(prefix);
+        }
         if (name)
         {
             freeAllocatedSingleString(name);
@@ -111,10 +132,20 @@ int sci_percent_XMLAttr_e(char * fname, unsigned long fname_len)
     }
     else
     {
-        value = attr->getAttributeValue(const_cast<const char *>(prefix));
+        if (indexes)
+        {
+            value = attr->getAttributeValue((int)(*indexes));
+        }
+        else
+        {
+            value = attr->getAttributeValue(const_cast<const char *>(prefix));
+        }
     }
 
-    freeAllocatedSingleString(prefix);
+    if (prefix)
+    {
+        freeAllocatedSingleString(prefix);
+    }
     if (name)
     {
         freeAllocatedSingleString(name);
