d593339fb50e5b061928a632be086f529fbf09fe
Antoine ELIAS <antoine.elias@scilab.org>
2012-01-17 16:39:31 +0100

    plugged save/load to export/import hdf5 functions and add management of graphic handles
    Change-Id: I8595b0258163d17842a7d104cb1c33b077c19616

--- scilab/modules/io/sci_gateway/c/sci_save.c
+++ scilab/modules/io/sci_gateway/c/sci_save.c
@@ -11,12 +11,127 @@
  */
 
 #include "gw_io.h"
+#include "stack-c.h"
+#include "api_scilab.h"
 /*--------------------------------------------------------------------------*/
 extern int C2F(intsave)(); /* fortran subroutine */
 /*--------------------------------------------------------------------------*/
 int sci_save(char *fname,unsigned long fname_len)
 {
-	C2F(intsave)();
+    SciErr sciErr;
+
+    int iOldSave    = FALSE;
+
+    int* piAddr1    = NULL;
+    int iType1      = 0;
+    int iRows1      = 0;
+    int iCols1      = 0;
+
+    CheckRhs(1, 100000);
+    CheckLhs(0, 1);
+
+    //filename or file descriptor
+    sciErr = getVarAddressFromPosition(pvApiCtx, 1, &piAddr1);
+    if(sciErr.iErr)
+    {
+        printError(&sciErr, 0);
+        return 1;
+    }
+
+    sciErr = getVarType(pvApiCtx, piAddr1, &iType1);
+    if(sciErr.iErr)
+    {
+        printError(&sciErr, 0);
+        return 1;
+    }
+
+    if(iType1 == sci_strings)
+    {
+        int* piAddrI    = NULL;
+        int* piAddrI2   = NULL;
+        int iTypeI      = 0;
+        int iRowsI      = 0;
+        int iColsI      = 0;
+        char* pstVarI   = NULL;
+
+        int i = 0;
+        for(i = 2 ; i <= Rhs ; i++)
+        {
+            sciErr = getVarAddressFromPosition(pvApiCtx, i, &piAddrI);
+            if(sciErr.iErr)
+            {
+                printError(&sciErr, 0);
+                return 1;
+            }
+
+            sciErr = getVarType(pvApiCtx, piAddrI, &iTypeI);
+            if(sciErr.iErr)
+            {
+                printError(&sciErr, 0);
+                return 1;
+            }
+
+            if(iTypeI != sci_strings)
+            {
+                iOldSave = TRUE;
+                break;
+            }
+
+            sciErr = getVarDimension(pvApiCtx, piAddrI, &iRowsI, &iColsI);
+            if(sciErr.iErr)
+            {
+                printError(&sciErr, 0);
+                return 1;
+            }
+
+            if(iRowsI != 1 || iColsI != 1)
+            {
+                iOldSave = TRUE;
+                break;
+            }
+
+            if(getAllocatedSingleString(pvApiCtx, piAddrI, &pstVarI))
+            {
+                return 1;
+            }
+
+            //try to get varaible by name
+            sciErr = getVarAddressFromName(pvApiCtx, pstVarI, &piAddrI2);
+            if(sciErr.iErr)
+            {
+                printError(&sciErr, 0);
+                return 1;
+            }
+
+            if(piAddrI2 == 0)
+            {
+                iOldSave = TRUE;
+                break;
+            }
+
+            freeAllocatedSingleString(pstVarI);
+
+            {
+                int lw = 0;
+                //call "overload" to prepare data to export_to_hdf5 function.
+                C2F(overload) (&lw, "save", (unsigned long)strlen("save"));
+            }
+
+        }
+    }
+    else
+    {
+        iOldSave = TRUE;
+    }
+
+
+    //new -> %_export_to_hdf5
+    //old
+    if(iOldSave)
+    {
+	    C2F(intsave)();
+    }
+
 	return 0;
 }
 /*--------------------------------------------------------------------------*/
