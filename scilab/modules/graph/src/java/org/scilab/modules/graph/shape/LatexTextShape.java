9664fa45bf3d654befe2b675dba4335559f842ba
Bruno JOFRET <bruno.jofret@scilab-enterprises.com>
2012-01-24 16:44:17 +0100

    Change CLR Style to display Transfer Function.
    Change-Id: I3f53e61dc7cf367d0a39af47be8e8c9aeabf5caa

--- scilab/modules/graph/src/java/org/scilab/modules/graph/shape/LatexTextShape.java
+++ scilab/modules/graph/src/java/org/scilab/modules/graph/shape/LatexTextShape.java
@@ -30,59 +30,67 @@ import com.mxgraph.util.mxUtils;
 import com.mxgraph.view.mxCellState;
 
 /**
- * Implement a text shape that can draw LaTEX text. 
+ * Implement a text shape that can draw LaTEX text.
  */
 public class LatexTextShape implements mxITextShape {
 
-	/**
-	 * Painter 
-	 * 
-	 * @param canvas the current canvas
-	 * @param text the text to render
-	 * @param state the current state
-	 * @param style the current style
-	 * @see com.mxgraph.shape.mxITextShape#paintShape(com.mxgraph.canvas.mxGraphics2DCanvas, java.lang.String, com.mxgraph.util.mxRectangle, java.util.Map)
-	 */
-	@Override
-	public void paintShape(mxGraphics2DCanvas canvas, String text,
-			mxCellState state, Map<String, Object> style) {
-		
-		CellRendererPane rendererPane = canvas.getRendererPane();
-		Rectangle rect = state.getRectangle();
-		if (rect.isEmpty()) {
-			rect.grow(1, 1);
-		}
-		Graphics2D g = canvas.getGraphics();
-		
-		if (rendererPane != null
-				&& (g.getClipBounds() == null || g.getClipBounds().intersects(
-						rect))) {
-			double scale = canvas.getScale();
-			int x = rect.x;
-			int y = rect.y;
-			int w = rect.width;
-			int h = rect.height;
-			
-			if (g.hitClip(x, y, w, h)) {
-				AffineTransform at = g.getTransform();
+    /**
+     * Painter
+     * 
+     * @param canvas
+     *            the current canvas
+     * @param text
+     *            the text to render
+     * @param state
+     *            the current state
+     * @param style
+     *            the current style
+     * @see com.mxgraph.shape.mxITextShape#paintShape(com.mxgraph.canvas.mxGraphics2DCanvas,
+     *      java.lang.String, com.mxgraph.util.mxRectangle, java.util.Map)
+     */
+    @Override
+    public void paintShape(mxGraphics2DCanvas canvas, String text, mxCellState state, Map<String, Object> style) {
 
-				int sx = (int) (x / scale) + mxConstants.LABEL_INSET;
-				int sy = (int) (y / scale) + mxConstants.LABEL_INSET;
-				g.scale(scale, scale);
-				
-				Color textColor = mxUtils.getColor(style, mxConstants.STYLE_FONTCOLOR, Color.BLACK);
-				rendererPane.setForeground(textColor);
-				
-				// parse the text and cache it if valid. Will throw an exception
-				// if the text is not valid but the text must have been already
-				// checked on ScilabCanvas#getTextShape(...). 
-				Icon icon = ScilabGraphUtils.getTexIcon(text);
-				icon.paintIcon(rendererPane, g, sx, sy);
+        CellRendererPane rendererPane = canvas.getRendererPane();
+        Rectangle rect = state.getRectangle();
+        if (rect.isEmpty()) {
+            rect.grow(1, 1);
+        }
+        Graphics2D g = canvas.getGraphics();
 
-				// Restores the previous transformation
-				g.setTransform(at);
-			}
-		}
-	}
+        if (rendererPane != null && (g.getClipBounds() == null || g.getClipBounds().intersects(rect))) {
+            final double scale = canvas.getScale();
+            final int x = rect.x;
+            final int y = rect.y;
+            final int w = rect.width;
+            final int h = rect.height;
+
+            if (g.hitClip(x, y, w, h)) {
+                AffineTransform at = g.getTransform();
+
+                final int sx = (int) (x / scale) + mxConstants.LABEL_INSET;
+                final int sy = (int) (y / scale) + mxConstants.LABEL_INSET;
+                g.scale(scale, scale);
+
+                Color textColor = mxUtils.getColor(style, mxConstants.STYLE_FONTCOLOR, Color.BLACK);
+                rendererPane.setForeground(textColor);
+
+                // parse the text and cache it if valid. Will throw an exception
+                // if the text is not valid but the text must have been already
+                // checked on ScilabCanvas#getTextShape(...).
+                Icon icon = ScilabGraphUtils.getTexIcon(text);
+                final int iw = icon.getIconWidth();
+                final int ih = icon.getIconHeight();
+                
+                final int dx = ((int)(w / scale) - iw) / 2;
+                final int dy = ((int) (h / scale) - ih) / 2;
+
+                icon.paintIcon(rendererPane, g, sx + dx, sy + dy);
+
+                // Restores the previous transformation
+                g.setTransform(at);
+            }
+        }
+    }
 
 }
