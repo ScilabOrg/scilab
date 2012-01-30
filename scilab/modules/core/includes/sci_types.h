9387f5fb062b2b59e833980d74aa88185ecacd24
Clément DAVID <clement.david@scilab-enterprises.com>
2012-01-30 11:20:59 +0100

    WORKAROUND: hdf5: export c/u functions as error strings
    Change-Id: Ie312945efd47e2c57f87d5e47667b0ccee94ef02

--- scilab/modules/core/includes/sci_types.h
+++ scilab/modules/core/includes/sci_types.h
@@ -18,32 +18,35 @@
  * types
  * -------------------------------------------------*/
 
-typedef enum {
-  sci_matrix = 1 ,
-  sci_poly = 2 ,
-  sci_boolean = 4 ,
-  sci_sparse = 5  ,
-  sci_boolean_sparse = 6,
-  sci_matlab_sparse = 7, /* matlab sparse matrix  */
-  sci_ints = 8,
-  sci_handles =9,
-  sci_strings = 10,
-  sci_u_function = 11,
-  sci_c_function = 13 ,
-  sci_lib =  14,
-  sci_list = 15,
-  sci_tlist = 16,
-  sci_mlist = 17,
-  sci_pointer = 128, /* pointer */
-  sci_implicit_poly = 129,
-  sci_intrinsic_function = 130
+typedef enum
+{
+    sci_invalid = -1,
+    sci_undefined = 0,
 
+    sci_matrix = 1,
+    sci_poly = 2,
+    sci_boolean = 4,
+    sci_sparse = 5,
+    sci_boolean_sparse = 6,
+    sci_matlab_sparse = 7,      /* matlab sparse matrix  */
+    sci_ints = 8,
+    sci_handles = 9,
+    sci_strings = 10,
+    sci_u_function = 11,
+    sci_c_function = 13,
+    sci_lib = 14,
+    sci_list = 15,
+    sci_tlist = 16,
+    sci_mlist = 17,
+    sci_pointer = 128,          /* pointer */
+    sci_implicit_poly = 129,
+    sci_intrinsic_function = 130
 } sci_types;
-
 
 /* These values are also used in:
  * modules/api_scilab/includes/api_int.h */
-typedef enum {
+typedef enum
+{
     sci_int8 = 1,
     sci_int16 = 2,
     sci_int32 = 4,
@@ -54,9 +57,8 @@ typedef enum {
     sci_uint64 = 18
 } sci_int_types;
 
-
  /* lufact pointer... Done for backward compatibility. 
-  *	sci_lufact_pointer has been added to be able to handle umfpack data type
+  * sci_lufact_pointer has been added to be able to handle umfpack data type
   * Since the use of pointer data structure is very interesting for Scilab 
   * we decided to rename it to remove the reference of the initial use.
   */
