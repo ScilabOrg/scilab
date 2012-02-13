049dea39c6482fc5c018422ce1201e23841d1a3c
Pierre LANDO <pierre.lando@scilab.org>
2012-02-13 16:11:30 +0100

    Add licence.
    Change-Id: If0166e18b4716e4221436f4fbcb631e7bf21e230

--- scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/util/FormattedTextSpriteDrawer.java
+++ scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/util/FormattedTextSpriteDrawer.java
@@ -1,3 +1,14 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2009-2012 - DIGITEO - Pierre Lando
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ */
+
 package org.scilab.modules.renderer.JoGLView.util;
 
 import org.scilab.forge.jlatexmath.TeXConstants;
