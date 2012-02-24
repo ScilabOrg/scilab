3fdbbdcbe0dc7be783259825cb768402788563b9
Clément DAVID <clement.david@scilab-enterprises.com>
2012-02-24 12:16:46 +0100

    Java: safely destroy the JVM after use
    Some tools (mainly JVM agent) require a clean java termination.
    
    Change-Id: Id62c31827025e6056c0c1cc8b4a65a76aa5e0e29

--- scilab/modules/core/src/java/org/scilab/modules/core/Scilab.java
+++ scilab/modules/core/src/java/org/scilab/modules/core/Scilab.java
@@ -19,9 +19,11 @@ package org.scilab.modules.core;
 
 import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import javax.swing.JPopupMenu;
 import javax.swing.SwingUtilities;
@@ -64,7 +66,6 @@ public class Scilab {
     private static final String MACOS = "mac";
 
     private static boolean success;
-    private static boolean finish;
     private static boolean exitCalled;
     private static int mode;
 
@@ -95,11 +96,11 @@ public class Scilab {
              * Set Java directories to Scilab ones
              */
             if (mode != 1) {
-            /* only modify these properties if Scilab is not called by another application */
-            /* In this case, we let the calling application to use its own properties */
-            System.setProperty("java.io.tmpdir", ScilabConstants.TMPDIR.getCanonicalPath());
-            System.setProperty("user.home", ScilabConstants.SCIHOME.getCanonicalPath());
-          }
+                /* only modify these properties if Scilab is not called by another application */
+                /* In this case, we let the calling application to use its own properties */
+                System.setProperty("java.io.tmpdir", ScilabConstants.TMPDIR.getCanonicalPath());
+                System.setProperty("user.home", ScilabConstants.SCIHOME.getCanonicalPath());
+            }
 
         } catch (Exception e) {
             System.err.println("Cannot retrieve the variable SCI. Please report on http://bugzilla.scilab.org/");
@@ -225,26 +226,20 @@ public class Scilab {
      * @return true if the console is closed
      */
     public static boolean canClose() {
+        final Lock lock = new ReentrantLock();
+        final Condition finish = lock.newCondition();
+
         SwingUtilities.invokeLater(new Runnable() {
-                @Override
-                public void run() {
-                    exitCalled = true;
-                    success = ClosingOperationsManager.startClosingOperationOnRoot();
-                    exitCalled = false;
-                    finish = true;
-                }
-            });
-
-        while (!finish) {
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                System.err.println(e);
+            @Override
+            public void run() {
+                exitCalled = true;
+                success = ClosingOperationsManager.startClosingOperationOnRoot();
+                exitCalled = false;
+                finish.signal();
             }
-        }
+        });
 
-        finish = false;
-
+        finish.awaitUninterruptibly();
         return success;
     }
 
