051293db14446b2558ec86b2ed2b72ce76015ebc
Cedric Delamarre <cedric.delamarre@scilab.org>
2012-02-27 09:38:45 +0100

    elementary_functions module.
    Change-Id: I5290543100f70ba42e592afde987b446b7756f32

---
+++ scilab/modules/elementary_functions/src/cpp/cumprod.hxx
@@ -0,0 +1,22 @@
+/*
+*  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+*  Copyright (C) 2012 - DIGITEO - cedric delamarre
+*
+*  This file must be used under the terms of the CeCILL.
+*  This source file is licensed as described in the file COPYING, which
+*  you should have received as part of this distribution.  The terms
+*  are also available at
+*  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
+
+#ifndef __CUMPROD_H__
+#define __CUMPROD_H__
+
+#include "double.hxx"
+#include "polynom.hxx"
+
+int cumprod(types::Double* pIn,  int iOrientation, types::Double* pOut);
+int cumprod(types::Polynom* pIn,  int iOrientation, types::Polynom* pOut);
+
+#endif /* __CUMPROD_H__ */
