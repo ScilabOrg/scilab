d63e1d0429e3b33b3eb3b245347690fbaff9d3cc
Pierre LANDO <pierre.lando@scilab.org>
2012-02-29 16:24:54 +0100

    Fix color in 'Segs'.
    Change-Id: Ic4f63e82f3bb1f82aa0e961b9751d30cbba66934

--- scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/DrawerVisitor.java
+++ scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/DrawerVisitor.java
@@ -590,7 +590,8 @@ public class DrawerVisitor implements Visitor, Drawer, GraphicView {
 
                 if (segs.getLineMode()) {
                     Appearance segmentAppearance = new Appearance();
-                    segmentAppearance.setLineColor(ColorFactory.createColor(colorMap, segs.getLineColor()));
+                    //segmentAppearance.setLineColor(ColorFactory.createColor(colorMap, segs.getLineColor()));
+                    segmentAppearance.setLineColor(null);
                     segmentAppearance.setLineWidth(segs.getLineThickness().floatValue());
                     segmentAppearance.setLinePattern(segs.getLineStyleAsEnum().asPattern());
                     drawingTools.draw(segments, segmentAppearance);
