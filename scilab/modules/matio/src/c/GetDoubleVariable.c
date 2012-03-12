1e86ec613eb169c7940a353a02a6f09632fb490e
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2012-03-12 13:31:38 +0100

    Port to matio 1.5
    Change-Id: Ib15dd089e6172ebb84f5f3b6dbd93741cba0db53

--- scilab/modules/matio/src/c/GetDoubleVariable.c
+++ scilab/modules/matio/src/c/GetDoubleVariable.c
@@ -1,12 +1,12 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) 2008 - INRIA - Vincent COUVERT 
+ * Copyright (C) 2008 - INRIA - Vincent COUVERT
  * Copyright (C) 2010 - DIGITEO - Yann COLLETTE
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -23,89 +23,97 @@
 
 matvar_t * GetDoubleVariable(int iVar, const char* name, int matfile_version, int * parent, int item_position)
 {
-  double * realDataAdr = NULL, * complexDataAdr = NULL;
-  int rank = 0;
-  int *dims = NULL;
-  int isComplex = 0;
-  struct ComplexSplit mat5ComplexData;
-  matvar_t *createdVar = NULL;
-  int * var_addr = NULL;
-  int * item_addr = NULL;
-  int var_type;
-  SciErr _SciErr;
+    double * realDataAdr = NULL, * complexDataAdr = NULL;
+    int rank = 0;
+    int *dims = NULL;
+    int isComplex = 0;
+    struct mat_complex_split_t mat5ComplexData;
+    matvar_t *createdVar = NULL;
+    int * var_addr = NULL;
+    int * item_addr = NULL;
+    int var_type;
+    SciErr _SciErr;
 
-  if (parent==NULL)
+    if (parent == NULL)
     {
-      _SciErr = getVarAddressFromPosition(pvApiCtx, iVar, &var_addr); MATIO_ERROR;
-      _SciErr = getVarType(pvApiCtx, var_addr, &var_type); MATIO_ERROR;
-      isComplex = isVarComplex(pvApiCtx, var_addr);
+        _SciErr = getVarAddressFromPosition(pvApiCtx, iVar, &var_addr);
+        MATIO_ERROR;
+        _SciErr = getVarType(pvApiCtx, var_addr, &var_type);
+        MATIO_ERROR;
+        isComplex = isVarComplex(pvApiCtx, var_addr);
     }
-  else
+    else
     {
-      _SciErr = getListItemAddress(pvApiCtx, parent, item_position, &item_addr); MATIO_ERROR;
-      _SciErr = getVarType(pvApiCtx, item_addr, &var_type); MATIO_ERROR;
-      isComplex = isVarComplex(pvApiCtx, item_addr);
+        _SciErr = getListItemAddress(pvApiCtx, parent, item_position, &item_addr);
+        MATIO_ERROR;
+        _SciErr = getVarType(pvApiCtx, item_addr, &var_type);
+        MATIO_ERROR;
+        isComplex = isVarComplex(pvApiCtx, item_addr);
     }
 
-  if(var_type == sci_matrix) /* 2-D array */
+    if (var_type == sci_matrix) /* 2-D array */
     {
-      rank = 2;
-      if ((dims = (int*)MALLOC(sizeof(int)*rank)) == NULL)
+        rank = 2;
+        if ((dims = (int*)MALLOC(sizeof(int) * rank)) == NULL)
         {
-          Scierror(999, _("%s: No more memory.\n"), "GetDoubleVariable");
-          return NULL;
+            Scierror(999, _("%s: No more memory.\n"), "GetDoubleVariable");
+            return NULL;
         }
 
 
-      if (isComplex)
-	{
-	  if (parent==NULL)
-	    {
-	      _SciErr = getComplexMatrixOfDouble(pvApiCtx, var_addr, &dims[0], &dims[1], &realDataAdr, &complexDataAdr); MATIO_ERROR;
-	    }
-	  else
-	    {
-	      _SciErr = getComplexMatrixOfDoubleInList(pvApiCtx, parent, item_position, &dims[0], &dims[1], 
-						       &realDataAdr, &complexDataAdr); MATIO_ERROR;
-	    }
-	}
-      else
-	{
-	  if (parent==NULL)
-	    {
-	      _SciErr = getMatrixOfDouble(pvApiCtx, var_addr, &dims[0], &dims[1], &realDataAdr); MATIO_ERROR;
-	    }
-	  else
-	    {
-	      _SciErr = getMatrixOfDoubleInList(pvApiCtx, parent, item_position, &dims[0], &dims[1], &realDataAdr); MATIO_ERROR;
-	    }
-	}
-
-      if (isComplex==0)
+        if (isComplex)
         {
-          createdVar = Mat_VarCreate(name, MAT_C_DOUBLE, MAT_T_DOUBLE, rank, dims, realDataAdr, 0);
-        }
-      else
-        {
-          if(matfile_version==MAT_FT_MAT4) /* MATLAB4: data is a table of value */
+            if (parent == NULL)
             {
-              createdVar = Mat_VarCreate(name, MAT_C_DOUBLE, MAT_T_DOUBLE, rank, dims, realDataAdr, MAT_F_COMPLEX);
+                _SciErr = getComplexMatrixOfDouble(pvApiCtx, var_addr, &dims[0], &dims[1], &realDataAdr, &complexDataAdr);
+                MATIO_ERROR;
             }
-          else /* MATLAB5 file: data is a ComplexSplit */
+            else
             {
-              mat5ComplexData.Re = realDataAdr;
-              mat5ComplexData.Im = complexDataAdr;
-              createdVar = Mat_VarCreate(name, MAT_C_DOUBLE, MAT_T_DOUBLE, rank, dims, &mat5ComplexData, MAT_F_COMPLEX);
+                _SciErr = getComplexMatrixOfDoubleInList(pvApiCtx, parent, item_position, &dims[0], &dims[1],
+                          &realDataAdr, &complexDataAdr);
+                MATIO_ERROR;
             }
-          createdVar->isComplex = 1;
+        }
+        else
+        {
+            if (parent == NULL)
+            {
+                _SciErr = getMatrixOfDouble(pvApiCtx, var_addr, &dims[0], &dims[1], &realDataAdr);
+                MATIO_ERROR;
+            }
+            else
+            {
+                _SciErr = getMatrixOfDoubleInList(pvApiCtx, parent, item_position, &dims[0], &dims[1], &realDataAdr);
+                MATIO_ERROR;
+            }
+        }
+
+        if (isComplex == 0)
+        {
+            createdVar = Mat_VarCreate(name, MAT_C_DOUBLE, MAT_T_DOUBLE, rank, dims, realDataAdr, 0);
+        }
+        else
+        {
+            if (matfile_version == MAT_FT_MAT4) /* MATLAB4: data is a table of value */
+            {
+                createdVar = Mat_VarCreate(name, MAT_C_DOUBLE, MAT_T_DOUBLE, rank, dims, realDataAdr, MAT_F_COMPLEX);
+            }
+            else /* MATLAB5 file: data is a mat_complex_split_t */
+            {
+                mat5ComplexData.Re = realDataAdr;
+                mat5ComplexData.Im = complexDataAdr;
+                createdVar = Mat_VarCreate(name, MAT_C_DOUBLE, MAT_T_DOUBLE, rank, dims, &mat5ComplexData, MAT_F_COMPLEX);
+            }
+            createdVar->isComplex = 1;
         }
     }
-  else
+    else
     {
-      Scierror(999, _("%s: Wrong type for first input argument: Double matrix expected.\n"), "GetDoubleVariable");
+        Scierror(999, _("%s: Wrong type for first input argument: Double matrix expected.\n"), "GetDoubleVariable");
     }
 
-  FREE(dims);
+    FREE(dims);
 
-  return createdVar;
+    return createdVar;
 }
