0f8eb0fe1a8ef0bc70b39c7d0c81b209ceae8fee
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2012-03-14 21:03:16 +0100

    Drop the wrapping function on memory management under Linux & Mac OS X. Remains the same under Windows
    Change-Id: I979be8dc75f2795e06e2887c82023bd0f32c052a

--- scilab/libs/MALLOC/includes/sci_mem_alloc.h
+++ scilab/libs/MALLOC/includes/sci_mem_alloc.h
@@ -1,15 +1,15 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) INRIA - 2005 - Allan CORNET
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
- 
+
 
 #ifndef __SCI_MEM_ALLOC__
 #define __SCI_MEM_ALLOC__
@@ -19,6 +19,7 @@
 #ifdef _MSC_VER
 #error This file should not be used on windows
 #else
+#include <stdlib.h>
 #include <stddef.h> /* for size_t */
 #endif
 
@@ -39,64 +40,18 @@
 #endif
 
 #ifdef NULL
-	#undef NULL
+#undef NULL
 #endif
 #define NULL 0
 
-#include "ExportImport.h" /* IMPORT_EXPORT_MALLOC_DLL */
-
-/**
- * Reallocate the memory
- * Unix/Linux only
- *
- * @param lpAddress Address 
- * @param dwSize Size
- * @param file Which file is calling the function (used debug)
- * @param line Which line is calling the function (used debug)
- * @return <ReturnValue>
- */
- void * MyReAlloc(void * lpAddress,size_t dwSize,const char *file,int line);
- 
- /**
- * Allocate the memory
- * Unix/Linux only
- *
- * @param dwSize Size 
- * @param file Which file is calling the function (used debug) 
- * @param line Which line is calling the function (used debug) 
- * @return <ReturnValue>
- */
- void * MyAlloc(size_t dwSize,const char *file,int line);
- 
-   /**
- * 
- * Unix/Linux only
- *
- * @param x
- * @param y
- * @param file Which file is calling the function (used debug) 
- * @param line Which line is calling the function (used debug) 
- * @return <ReturnValue>
- */
- void * MyCalloc(size_t x, size_t y, const char *file,int line);
- 
-  /**
- * Free the memory
- * Unix/Linux only
- *
- * @param lpAddress Address 
- * @return <ReturnValue>
- */
-void MyFree(void *lpAddress);
-
-#define MALLOC(x) MyAlloc((size_t)x,__FILE__,__LINE__)
-#define FREE(x) MyFree((void *) x)
-#define REALLOC(x,y) MyReAlloc((void *)x, y,__FILE__,__LINE__)
-#define CALLOC(x,y) MyCalloc((size_t) x, (size_t) y,__FILE__,__LINE__)
+#define MALLOC(x) malloc((size_t)x)
+#define FREE(x) free((void *) x)
+#define REALLOC(x,y) realloc((void *)x, y)
+#define CALLOC(x,y) calloc((size_t) x, (size_t) y)
 
 /* Only for Scilab Stacksize use VirtualAlloc on Window */
-#define SCISTACKMALLOC(x) MyAlloc((unsigned long)x,__FILE__,__LINE__)
-#define SCISTACKFREE(x) MyFree((void *) x)
+#define SCISTACKMALLOC(x) malloc((unsigned long)x)
+#define SCISTACKFREE(x) free((void *) x)
 
 #define Top C2F(vstk).top
 
