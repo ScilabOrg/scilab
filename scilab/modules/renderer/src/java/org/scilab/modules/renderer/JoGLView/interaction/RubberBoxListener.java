8c79451021fc02357c690df3c6039f282fac41c7
Bruno JOFRET <bruno.jofret@scilab-enterprises.com>
2012-03-15 15:48:02 +0100

    Plug back interactive Zoom, using MVC.
    * Work fine in 2D.
    * 3D is all new.
    
    Change-Id: If9d56e15f715d7c78104d9ec40f9b3955be22024

---
+++ scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/interaction/RubberBoxListener.java
@@ -0,0 +1,32 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2009-2010 - DIGITEO - Pierre Lando
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ */
+
+package org.scilab.modules.renderer.JoGLView.interaction;
+
+import org.scilab.forge.scirenderer.tranformations.Vector3d;
+
+import java.util.EventListener;
+
+/**
+ * @author Pierre Lando
+ */
+public interface RubberBoxListener extends EventListener {
+
+    /**
+     * Notification for a rubber box end.
+     * @param firstPoint the first corner of the rubber box.
+     * @param secondPoint the second corner of the rubber box.
+     */
+    void rubberBoxEnd(Vector3d firstPoint, Vector3d secondPoint);
+
+    /** Notification for a rubber box stop */
+    void rubberBoxStopped();
+}
