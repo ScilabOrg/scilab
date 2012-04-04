a327a689c6cf269582489ad76a8113d69837f2f7
steer <serge.steer@scilab.org>
2012-04-04 14:26:28 +0200

    improved fft function based on fftw, taking data symmetry into account, hard coded multivariate fft,...
    Change-Id: I952a61144d605082637c9373f46ab41e71709ebd

--- scilab/modules/fftw/src/c/fftw_utilities.h
+++ scilab/modules/fftw/src/c/fftw_utilities.h
@@ -2,6 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006/2007 - INRIA - Alan Layec
  * Copyright (C) 2008 - INRIA - Allan CORNET
+ * Copyright (C) 2012 - INRIA - Serge STEER
  * 
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -13,11 +14,10 @@
 #ifndef __FFTW_UTILITIES__
 #define __FFTW_UTILITIES__
 
+#include "stack-c.h"
 #include <string.h>
 #include <stdio.h>
 #include "fftw3.h"
-#include "stack-c.h"
-
 
 /* definiton of a guru_dim structure type */
 typedef struct guru_dim_st
@@ -28,10 +28,18 @@ typedef struct guru_dim_st
   fftw_iodim *howmany_dims;
 } guru_dim_struct;
 
-/* definiton of a FFTW_Plan structure type */
+enum Plan_Type {
+  C2C_PLAN=0, 
+  R2C_PLAN=1, 
+  C2R_PLAN=2, 
+  R2R_PLAN=3
+};
+
+/* definition of a FFTW_Plan structure type */
 typedef struct fftw_plan_st
 {
-  /* stored parmeters of fftw_plan_guru_split_dft function */
+  /* stored parameters of fftw_plan_guru_split_dft function */
+  enum Plan_Type plan_type;
   fftw_plan p;
   guru_dim_struct gdim;
   unsigned flags;
@@ -40,7 +48,7 @@ typedef struct fftw_plan_st
 extern int C2F(dset)(int *n, double *dx, double *dy, int *incy);
 
 /* prototypes of utilities functions */
-fftw_plan GetFFTWPlan(guru_dim_struct *gdim,
+fftw_plan GetFFTWPlan(enum Plan_Type type, guru_dim_struct *gdim,
                       double *ri, double *ii,
                       double *ro, double *io,
                       unsigned flags, int isn);
@@ -50,7 +58,10 @@ int FreeFFTWPlan(FFTW_Plan_struct *Sci_Plan);
 int CheckGuruDims(guru_dim_struct *gdim1,
                   guru_dim_struct *gdim2);
 
+void ExecuteFFTWPlan(enum Plan_Type type, const fftw_plan p, double *ri, double *ii,double *ro, double *io);
 
-
+int is_real(double *Ar,double *Ai, int ndims, int *dims);
+int check_array_symmetry(double *Ar,double *Ai, guru_dim_struct gdim);
+int complete_array(double *Ar,double *Ai, guru_dim_struct gdim);
 #endif /* __FFTW_UTILITIES__ */
 /*--------------------------------------------------------------------------*/
