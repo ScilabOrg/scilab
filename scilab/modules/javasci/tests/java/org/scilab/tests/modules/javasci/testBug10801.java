a6660973e94254f47bc48caae01fc676e9e75f08
Vincent COUVERT <vincent.couvert@scilab.org>
2012-04-11 21:23:09 +0200

    Bug #10801 fixed - Graphics were not completely cleanup by TerminateScilab
    Change-Id: I50c82e71fb05a886daaf4567cc2f893b84603144

---
+++ scilab/modules/javasci/tests/java/org/scilab/tests/modules/javasci/testBug10801.java
@@ -0,0 +1,71 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2010 - DIGITEO - Sylvestre LEDRU
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+package org.scilab.tests.modules.javasci;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import org.scilab.modules.javasci.Scilab;
+import org.scilab.modules.javasci.JavasciException;
+import org.scilab.modules.javasci.JavasciException.InitializationException;
+import org.scilab.modules.javasci.JavasciException.UnsupportedTypeException;
+import org.scilab.modules.javasci.JavasciException.UndefinedVariableException;
+import org.scilab.modules.types.ScilabType;
+import org.scilab.modules.types.ScilabDouble;
+import org.scilab.modules.types.ScilabBoolean;
+import org.scilab.modules.types.ScilabString;
+import org.scilab.modules.types.ScilabTypeEnum;
+
+public class testBug10801 {
+    private Scilab sci;
+
+    /*
+     * This method will be called for each test.
+     * with @After, this ensures that all the time the engine is closed
+     * especially in case of error.
+     * Otherwise, the engine might be still running and all subsequent tests
+     * would fail.
+     */
+    @Before
+    public void open() throws NullPointerException, JavasciException {
+        sci = new Scilab();
+        assertTrue(sci.open());
+    }
+
+    @Test()
+    public void nonRegBug10801() throws NullPointerException, JavasciException {
+        try {
+            Scilab sci = new Scilab(true);
+            sci.open();
+            sci.exec("plot3d()");
+            System.out.println("sci.isGraphicOpened() " + sci.isGraphicOpened());
+            sci.exec("disp(winsid())");
+            sci.close();
+            sci.open();
+            System.out.println("sci.isGraphicOpened() " + sci.isGraphicOpened());
+            sci.exec("disp(winsid())");
+            sci.close();
+        } catch (Exception e) {
+            System.err.print("e = " + e);
+        }
+
+    }
+
+    /**
+     * See #open()
+     */
+    @After
+    public void close() {
+        sci.close();
+
+    }
+}
