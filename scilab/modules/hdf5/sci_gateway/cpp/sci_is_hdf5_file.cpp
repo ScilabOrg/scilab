a65b787675a1347cfb2a91b6e111853b1ef1cf4b
Antoine ELIAS <antoine.elias@scilab.org>
2012-04-18 18:20:47 +0200

    bug 10959 : listvarinfile is broken with new variable format
    Change-Id: I2346c4478295b21231e9fe696750876772543df7

---
+++ scilab/modules/hdf5/sci_gateway/cpp/sci_is_hdf5_file.cpp
@@ -0,0 +1,67 @@
+/*
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2012 - DIGITEO - Antoine ELIAS
+*
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at
+* http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
+
+extern "C"
+{
+#include <hdf5.h>
+#include "gw_hdf5.h"
+#include "Scierror.h"
+#include "localization.h"
+#include "api_scilab.h"
+#include "../../../call_scilab/includes/call_scilab.h"
+#include "h5_fileManagement.h"
+}
+
+#include "forceJHDF5load.hxx"
+
+int sci_is_hdf5_file(char *fname, unsigned long fname_len)
+{
+    SciErr sciErr;
+    int *piAddr     = NULL;
+    char* pstFile   = NULL;
+    int iFile       = 0;
+
+
+    CheckRhs(1, 1);
+    CheckLhs(1, 4);
+
+#ifndef _MSC_VER
+    forceJHDF5load();
+#endif
+
+    sciErr = getVarAddressFromPosition(pvApiCtx, 1, &piAddr);
+    if(sciErr.iErr)
+    {
+        printError(&sciErr, 0);
+        return 1;
+    }
+
+    if(getAllocatedSingleString(pvApiCtx, piAddr, &pstFile))
+    {
+        Scierror(999, _("%s: Wrong size for input argument #%d: A string expected.\n"), fname, 1);
+        return 1;
+    }
+
+    iFile = openHDF5File(pstFile);
+    if(iFile < 0)
+    {
+        createScalarBoolean(pvApiCtx, Rhs + 1, 0);
+    }
+    else
+    {
+        createScalarBoolean(pvApiCtx, Rhs + 1, 1);
+    }
+
+    LhsVar(1) = Rhs + 1;
+    PutLhsVar();
+    return 0;
+}
