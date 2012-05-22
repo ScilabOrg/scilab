432ffe20fd57a815855c00a9581ae7817622e222
Vincent COUVERT <vincent.couvert@scilab.org>
2012-05-22 22:08:52 +0200

    Bug 10662 fix
    Change-Id: Ide7acf7b4f1b9ea699c01b7de35173def9bb3faa

--- scilab/modules/io/sci_gateway/c/sci_load.c
+++ scilab/modules/io/sci_gateway/c/sci_load.c
@@ -1,11 +1,11 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -18,11 +18,11 @@
 /*--------------------------------------------------------------------------*/
 extern int C2F(intload)(); /* fortran subroutine */
 /*--------------------------------------------------------------------------*/
-int sci_load(char *fname,unsigned long fname_len)
+int sci_load(char *fname, unsigned long fname_len)
 {
     SciErr sciErr;
     int* piAddr = NULL;
-
+    sciprint("[in]sci_load\n");
     //check first input argument type ( double can be file descriptor )
     sciErr = getVarAddressFromPosition(pvApiCtx, 1, &piAddr);
     if (sciErr.iErr)
@@ -31,32 +31,42 @@ int sci_load(char *fname,unsigned long fname_len)
         return 1;
     }
 
-    if(isDoubleType(pvApiCtx, piAddr))
+    if (isDoubleType(pvApiCtx, piAddr))
     {
-	    int Val=40;
-	    int k1=0;
+        int Val = 40;
+        int k1 = 0;
         if (getWarningMode())
         {
             sciprint(_("%s: Feature %s is obsolete.\n"), _("Warning"), "load(file_descriptor)");
             sciprint(_("%s: See help('load') for the rational.\n"), _("Warning"));
             sciprint(_("%s: This feature will be permanently removed in Scilab %s\n\n"), _("Warning"), "6.0.0");
         }
-	    C2F(intload)(&Val,&k1);
+        C2F(intload)(&Val, &k1);
     }
-    else
+    else //if(isStringType(pvApiCtx, piAddr))
     {
         int lw = 0;
         //call "overload" to prepare data to export_to_hdf5 function.
         C2F(overload) (&lw, "sodload", (unsigned long)strlen("sodload"));
     }
-	return 0;
+    /*else
+    {
+        int Val=40;
+        int k1=0;
+        C2F(intload)(&Val,&k1);
+        }*/
+    sciprint("[out]sci_load\n");
+
+    return 0;
 }
 
-int sci_percent_load(char *fname,unsigned long fname_len)
+int sci_percent_load(char *fname, unsigned long fname_len)
 {
-	int Val=40;
-	int k1=0;
-	C2F(intload)(&Val,&k1);
-	return 0;
+    int Val = 40;
+    int k1 = 0;
+    sciprint("[IN]sci_percent_load\n");
+    C2F(intload)(&Val, &k1);
+    sciprint("[OUT]sci_percent_load\n");
+    return 0;
 }
 /*--------------------------------------------------------------------------*/
