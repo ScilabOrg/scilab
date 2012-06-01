bd2cf41e1dde108ddba1c1001b1064d38f1eadb4
Vincent COUVERT <vincent.couvert@scilab.org>
2012-06-01 17:04:26 +0200

    * Bug #11089 fixed - rubberbox crashed Scilab.
    Plug rubberbox function
    - 2D draw some rectangle
    - 3D draw some cube
    
    Change-Id: I22b389dedd3fafcac717d5b044107c1b39bab02f

--- scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/interaction/InteractionManager.java
+++ scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/interaction/InteractionManager.java
@@ -1,6 +1,7 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2009-2010 - DIGITEO - Pierre Lando
+ * Copyright (C) 2012 - Scilab Enterprises - Bruno JOFRET
  *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -22,19 +23,19 @@ public class InteractionManager implements RubberBoxListener {
     private RubberBox rubberBox;
 
     /** The drag, zoom, rotate interaction manager */
-    DragZoomRotateInteraction dragZoomRotateInteraction;
+    private final DragZoomRotateInteraction dragZoomRotateInteraction;
+
+    /** Parent drawer visitor */
+    private DrawerVisitor drawerVisitor;
 
     /**
      * Default constructor.
      * @param drawerVisitor parent drawer visitor.
      */
     public InteractionManager(DrawerVisitor drawerVisitor) {
+        this.drawerVisitor = drawerVisitor;
         dragZoomRotateInteraction = new DragZoomRotateInteraction(drawerVisitor);
         dragZoomRotateInteraction.setEnable(true);
-
-        rubberBox = new RubberBox(drawerVisitor);
-        rubberBox.addListener(this);
-        drawerVisitor.addPostRendering(rubberBox);
     }
 
     /**
@@ -42,7 +43,9 @@ public class InteractionManager implements RubberBoxListener {
      * @exception Throwable the <code>Exception</code> raised by this method
      */
     public void finalize() throws Throwable {
-        rubberBox.removeListener(this);
+        if (rubberBox != null) {
+            rubberBox.removeListener(this);
+        }
         super.finalize();
     }
 
@@ -51,11 +54,67 @@ public class InteractionManager implements RubberBoxListener {
      */
     public void startInteractiveZoom() {
         dragZoomRotateInteraction.setEnable(false);
+        rubberBox = new ZoomRubberBox(drawerVisitor);
+        rubberBox.addListener(this);
+        drawerVisitor.addPostRendering(rubberBox);
         rubberBox.setEnable(true);
     }
 
+    public int[] startClickRubberBox() {
+        final Object lock = new Object();
+        dragZoomRotateInteraction.setEnable(false);
+        rubberBox = new RubberBox(drawerVisitor);
+        rubberBox.addListener(new RubberBoxListener() {
+            @Override
+            public void rubberBoxEnd() {
+                synchronized (lock) {
+                    lock.notify();
+                }
+                dragZoomRotateInteraction.setEnable(true);
+                rubberBox = null;
+            }
+        });
+        drawerVisitor.addPostRendering(rubberBox);
+        rubberBox.setEnable(true);
+        try {
+            synchronized (lock) {
+                lock.wait();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public int[] startDragRubberBox() {
+        final Object lock = new Object();
+        dragZoomRotateInteraction.setEnable(false);
+        rubberBox = new RubberBox(drawerVisitor);
+        rubberBox.addListener(new RubberBoxListener() {
+            @Override
+            public void rubberBoxEnd() {
+                synchronized (lock) {
+                    lock.notify();
+                }
+                dragZoomRotateInteraction.setEnable(true);
+                rubberBox = null;
+            }
+        });
+        drawerVisitor.addPostRendering(rubberBox);
+        rubberBox.setEnable(true);
+        try {
+            synchronized (lock) {
+                lock.wait();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
     @Override
     public void rubberBoxEnd() {
         dragZoomRotateInteraction.setEnable(true);
+        rubberBox = null;
     }
 }
