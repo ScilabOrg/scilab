787f6d36e93d19fb723fe35f5fabf399c53d4ed8
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2012-08-20 12:10:41 +0200

    Switch to api_scilab. Most of the work has been done automatically with the translation tool
    Change-Id: Ib375830327cb4c8a281673e594d6d12ee19418c9

--- scilab/modules/gui/sci_gateway/c/sci_x_choose_modeless.c
+++ scilab/modules/gui/sci_gateway/c/sci_x_choose_modeless.c
@@ -1,15 +1,16 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2008 - INRIA - Vincent COUVERT
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
- 
+
+#include "api_scilab.h"
 #include "gw_gui.h"
 #include "stack-c.h"
 #include "localization.h"
@@ -18,94 +19,227 @@
 #include "getPropertyAssignedValue.h"
 #include "freeArrayOfString.h"
 /*--------------------------------------------------------------------------*/
-int sci_x_choose_modeless(char *fname,unsigned long fname_len)
+int sci_x_choose_modeless(char *fname, unsigned long fname_len)
 {
-  int nbRow = 0, nbCol = 0;
-  int nbRowItems = 0, nbColItems = 0;
+    int nbRow = 0, nbCol = 0;
+    int nbRowItems = 0, nbColItems = 0;
 
-  int messageBoxID = 0;
+    int messageBoxID = 0;
 
-  char **itemsAdr = NULL;
-  char **buttonLabelAdr = NULL;
+    char **itemsAdr = NULL;
+    char **buttonLabelAdr = NULL;
 
-  char **messageAdr = NULL;
+    char **messageAdr = NULL;
 
-  int userValueAdr = 0;
-  int userValue = 0;
+    int userValueAdr = 0;
+    int userValue = 0;
 
-  CheckRhs(2,3);
-  CheckLhs(0,1);
+    int* piAddr1 = NULL;
+    int* piAddr2 = NULL;
+    int* piAddr3 = NULL;
 
-  if (VarType(1) == sci_strings)
+    int i;
+    int* piLen         = NULL;
+
+    SciErr sciErr;
+    CheckInputArgument(pvApiCtx, 2, 3);
+    CheckOutputArgument(pvApiCtx, 0, 1);
+
+
+
+    //get variable address
+    sciErr = getVarAddressFromPosition(pvApiCtx, 1, &piAddr1);
+    if (sciErr.iErr)
     {
-      GetRhsVar(1, MATRIX_OF_STRING_DATATYPE, &nbRowItems, &nbColItems, &itemsAdr);
-    }
-  else
+        printError(&sciErr, 0);
+        return 0;
+    }//
+    if (isStringType(pvApiCtx, piAddr1))
     {
-      Scierror(999, _("%s: Wrong type for input argument #%d: Vector of strings expected.\n"), fname, 1);
-      return FALSE;
-    }
 
-  if (VarType(2) == sci_strings)
-    {
-      GetRhsVar(2, MATRIX_OF_STRING_DATATYPE, &nbRow, &nbCol, &messageAdr);
-    }
-  else
-    {
-      freeArrayOfString(itemsAdr, nbColItems*nbRowItems);
-      Scierror(999, _("%s: Wrong type for input argument #%d: Vector of strings expected.\n"), fname, 2);
-      return FALSE;
-    }
-
-  /* Create the Java Object */
-  messageBoxID = createMessageBox();
-
-  /* Title is a default title */
-  setMessageBoxTitle(messageBoxID, _("Scilab Choose Message"));
-  /* Message */
-  setMessageBoxMultiLineMessage(messageBoxID, getStringMatrixFromStack((size_t)messageAdr), nbCol*nbRow);
-  /* ListBox Items */
-  setMessageBoxListBoxItems(messageBoxID, getStringMatrixFromStack((size_t)itemsAdr), nbColItems*nbRowItems);
-  /* Modality */
-  setMessageBoxModal(messageBoxID, FALSE);
-    
-  freeArrayOfString(itemsAdr, nbColItems*nbRowItems);
-  freeArrayOfString(messageAdr, nbCol*nbRow);
-    
-  if (Rhs == 3)
-    {
-      if (VarType(3) ==  sci_strings)
+        //fisrt call to retrieve dimensions
+        sciErr = getMatrixOfString(pvApiCtx, piAddr1, &nbRowItems, &nbColItems, NULL, NULL);
+        if (sciErr.iErr)
         {
-          GetRhsVar(3,MATRIX_OF_STRING_DATATYPE,&nbRow,&nbCol,&buttonLabelAdr);
-          if (nbRow*nbCol != 1)
-          {
-            freeArrayOfString(buttonLabelAdr, nbRow*nbCol);
-            Scierror(999, _("%s: Wrong size for input argument #%d: A string expected.\n"), fname, 3);
+            printError(&sciErr, 0);
+            return 0;
+        }
+
+        piLen = (int*)malloc(sizeof(int) * nbRowItems * nbColItems);
+
+        //second call to retrieve length of each string
+        sciErr = getMatrixOfString(pvApiCtx, piAddr1, &nbRowItems, &nbColItems, piLen, NULL);
+        if (sciErr.iErr)
+        {
+            printError(&sciErr, 0);
+            return 0;
+        }
+
+        itemsAdr = (char**)malloc(sizeof(char*) * nbRowItems * nbColItems);
+        for (i = 0 ; i < nbRowItems * nbColItems ; i++)
+        {
+            itemsAdr[i] = (char*)malloc(sizeof(char) * (piLen[i] + 1));//+ 1 for null termination
+        }
+
+        //third call to retrieve data
+        sciErr = getMatrixOfString(pvApiCtx, piAddr1, &nbRowItems, &nbColItems, piLen, itemsAdr);
+        if (sciErr.iErr)
+        {
+            printError(&sciErr, 0);
+            return 0;
+        }
+    }
+    else
+    {
+        Scierror(999, _("%s: Wrong type for input argument #%d: Vector of strings expected.\n"), fname, 1);
+        return FALSE;
+    }
+
+
+
+    //get variable address
+    sciErr = getVarAddressFromPosition(pvApiCtx, 2, &piAddr2);
+    if (sciErr.iErr)
+    {
+        printError(&sciErr, 0);
+        return 0;
+    }//
+    if (isStringType(pvApiCtx, piAddr2))
+    {
+
+        //fisrt call to retrieve dimensions
+        sciErr = getMatrixOfString(pvApiCtx, piAddr2, &nbRow, &nbCol, NULL, NULL);
+        if (sciErr.iErr)
+        {
+            printError(&sciErr, 0);
+            return 0;
+        }
+
+        piLen = (int*)malloc(sizeof(int) * nbRow * nbCol);
+
+        //second call to retrieve length of each string
+        sciErr = getMatrixOfString(pvApiCtx, piAddr2, &nbRow, &nbCol, piLen, NULL);
+        if (sciErr.iErr)
+        {
+            printError(&sciErr, 0);
+            return 0;
+        }
+
+        messageAdr = (char**)malloc(sizeof(char*) * nbRow * nbCol);
+        for (i = 0 ; i < nbRow * nbCol ; i++)
+        {
+            messageAdr[i] = (char*)malloc(sizeof(char) * (piLen[i] + 1));//+ 1 for null termination
+        }
+
+        //third call to retrieve data
+        sciErr = getMatrixOfString(pvApiCtx, piAddr2, &nbRow, &nbCol, piLen, messageAdr);
+        if (sciErr.iErr)
+        {
+            printError(&sciErr, 0);
+            return 0;
+        }
+    }
+    else
+    {
+        freeArrayOfString(itemsAdr, nbColItems * nbRowItems);
+        Scierror(999, _("%s: Wrong type for input argument #%d: Vector of strings expected.\n"), fname, 2);
+        return FALSE;
+    }
+
+    /* Create the Java Object */
+    messageBoxID = createMessageBox();
+
+    /* Title is a default title */
+    setMessageBoxTitle(messageBoxID, _("Scilab Choose Message"));
+    /* Message */
+    setMessageBoxMultiLineMessage(messageBoxID, getStringMatrixFromStack((size_t)messageAdr), nbCol * nbRow);
+    /* ListBox Items */
+    setMessageBoxListBoxItems(messageBoxID, getStringMatrixFromStack((size_t)itemsAdr), nbColItems * nbRowItems);
+    /* Modality */
+    setMessageBoxModal(messageBoxID, FALSE);
+
+    freeArrayOfString(itemsAdr, nbColItems * nbRowItems);
+    freeArrayOfString(messageAdr, nbCol * nbRow);
+
+    if (nbInputArgument(pvApiCtx) == 3)
+    {
+
+
+        //get variable address
+        sciErr = getVarAddressFromPosition(pvApiCtx, 3, &piAddr3);
+        if (sciErr.iErr)
+        {
+            printError(&sciErr, 0);
+            return 0;
+        }//
+        if (isStringType(pvApiCtx, piAddr3))
+        {
+
+            //fisrt call to retrieve dimensions
+            sciErr = getMatrixOfString(pvApiCtx, piAddr3, &nbRow, &nbCol, NULL, NULL);
+            if (sciErr.iErr)
+            {
+                printError(&sciErr, 0);
+                return 0;
+            }
+
+            piLen = (int*)malloc(sizeof(int) * nbRow * nbCol);
+
+            //second call to retrieve length of each string
+            sciErr = getMatrixOfString(pvApiCtx, piAddr3, &nbRow, &nbCol, piLen, NULL);
+            if (sciErr.iErr)
+            {
+                printError(&sciErr, 0);
+                return 0;
+            }
+
+            buttonLabelAdr = (char**)malloc(sizeof(char*) * nbRow * nbCol);
+            for (i = 0 ; i < nbRow * nbCol ; i++)
+            {
+                buttonLabelAdr[i] = (char*)malloc(sizeof(char) * (piLen[i] + 1));//+ 1 for null termination
+            }
+
+            //third call to retrieve data
+            sciErr = getMatrixOfString(pvApiCtx, piAddr3, &nbRow, &nbCol, piLen, buttonLabelAdr);
+            if (sciErr.iErr)
+            {
+                printError(&sciErr, 0);
+                return 0;
+            }
+            if (nbRow*nbCol != 1)
+            {
+                freeArrayOfString(buttonLabelAdr, nbRow * nbCol);
+                Scierror(999, _("%s: Wrong size for input argument #%d: A string expected.\n"), fname, 3);
+                return FALSE;
+            }
+        }
+        else
+        {
+            Scierror(999, _("%s: Wrong type for input argument #%d: A string expected.\n"), fname, 3);
             return FALSE;
-          }
-        }
-      else 
-        {
-          Scierror(999, _("%s: Wrong type for input argument #%d: A string expected.\n"), fname, 3);
-          return FALSE;
         }
 
-      setMessageBoxButtonsLabels(messageBoxID, getStringMatrixFromStack((size_t)buttonLabelAdr), nbCol*nbRow);
-      freeArrayOfString(buttonLabelAdr, nbRow*nbCol);
+        setMessageBoxButtonsLabels(messageBoxID, getStringMatrixFromStack((size_t)buttonLabelAdr), nbCol * nbRow);
+        freeArrayOfString(buttonLabelAdr, nbRow * nbCol);
     }
 
-  /* Display it and wait for a user input */
-  messageBoxDisplayAndWait(messageBoxID);
+    /* Display it and wait for a user input */
+    messageBoxDisplayAndWait(messageBoxID);
 
-  /* Read the user answer */
-  userValue = getMessageBoxSelectedItem(messageBoxID);
+    /* Read the user answer */
+    userValue = getMessageBoxSelectedItem(messageBoxID);
 
-  nbRow = 1;nbCol = 1;
-  CreateVar(Rhs+1, MATRIX_OF_DOUBLE_DATATYPE, &nbRow, &nbCol, &userValueAdr);
-  *stk(userValueAdr) = userValue;
+    /* Create the matrix as return of the function */
+    createScalarDouble(pvApiCtx, nbInputArgument(pvApiCtx) + 1, userValue);
+    if (sciErr.iErr)
+    {
+        free(userValueAdr); // Make sure everything is cleanup in case of error
+        printError(&sciErr, 0);
+        return 0;
+    }
 
-  LhsVar(1) = Rhs+1;
-  PutLhsVar();
-  return TRUE;
+    AssignOutputVariable(pvApiCtx, 1) = nbInputArgument(pvApiCtx) + 1;
+    ReturnArguments(pvApiCtx);
+    return TRUE;
 }
 /*--------------------------------------------------------------------------*/
