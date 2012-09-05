9ebc05bf71e0f4d74524ee9f6c102af1305bd76c
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2012-09-05 13:51:34 +0200

    sum function.
    Change-Id: Ib2a379935a6fb2794e06e865c7be0f0b6dd2cd29

--- scilab/modules/elementary_functions/sci_gateway/cpp/sci_sum.cpp
+++ scilab/modules/elementary_functions/sci_gateway/cpp/sci_sum.cpp
@@ -2,6 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
  * Copyright (C) 2011 - DIGITEO - Antoine ELIAS
+ * Copyright (C) 2012 - Scilab Enterprises - Cedric Delamarre
  *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -17,7 +18,7 @@
 #include "string.hxx"
 #include "overload.hxx"
 #include "execvisitor.hxx"
-#include "getmode.hxx"
+#include "sum.hxx"
 
 extern "C"
 {
@@ -27,120 +28,390 @@ extern "C"
 #include "basic_functions.h"
 }
 
-using namespace types;
 /*--------------------------------------------------------------------------*/
-Function::ReturnValue sci_sum(types::typed_list &in, int _iRetCount, types::typed_list &out)
+types::Function::ReturnValue sci_sum(types::typed_list &in, int _iRetCount, types::typed_list &out)
 {
-    int iMode = 0;
+    types::Double* pDblIn       = NULL;
+    types::Double* pDblOut      = NULL;
+    types::Polynom* pPolyIn     = NULL;
+    types::Polynom* pPolyOut    = NULL;
 
-    if(in.size() < 1 || in.size() > 3)
+    int iOrientation    = 0;
+    int iOuttype        = 1; // 1 = native | 2 = double (type of output value)
+
+    if (in.size() < 1 || in.size() > 3)
     {
-        Scierror(999,_("%s: Wrong number of input arguments: %d or %d expected.\n"), L"sum", 1, 2);
+        Scierror(999, _("%s: Wrong number of input arguments: %d or %d expected.\n"), L"sum", 1, 3);
         return Function::Error;
     }
 
-    if(in[0]->isDouble() == false && in[0]->isPoly() == false /*&& in[0]->isSparse() == false*/)
-    {//call overload
+    if (_iRetCount > 1)
+    {
+        ScierrorW(78, _W("%ls: Wrong number of output argument(s): %d expected.\n"), L"sum", 1);
+        return types::Function::Error;
+    }
+
+    /***** get data *****/
+    if (in[0]->isDouble()) // double
+    {
+        pDblIn = in[0]->getAs<types::Double>();
+    }
+    else if (in[0]->isBool()) // bool
+    {
+        types::Bool* pBIn = in[0]->getAs<types::Bool>();
+        pDblIn = new types::Double(pBIn->getDims(), pBIn->getDimsArray());
+        for (int i = 0; i < pBIn->getSize(); i++)
+        {
+            pBIn->get(i) ? pDblIn->set(i, 1) : pDblIn->set(i, 0);
+        }
+        iOuttype = 2;
+    }
+    else if (in[0]->isPoly()) // polynom
+    {
+        pPolyIn = in[0]->getAs<types::Polynom>();
+    }
+    else if (in[0]->isInt8()) // int
+    {
+        types::Int8* pIIn = in[0]->getAs<types::Int8>();
+        pDblIn = new types::Double(pIIn->getDims(), pIIn->getDimsArray());
+        for (int i = 0; i < pIIn->getSize(); i++)
+        {
+            pDblIn->set(i, static_cast<double>(pIIn->get(i)));
+        }
+    }
+    else if (in[0]->isInt16())
+    {
+        types::Int16* pIIn = in[0]->getAs<types::Int16>();
+        pDblIn = new types::Double(pIIn->getDims(), pIIn->getDimsArray());
+        for (int i = 0; i < pIIn->getSize(); i++)
+        {
+            pDblIn->set(i, static_cast<double>(pIIn->get(i)));
+        }
+    }
+    else if (in[0]->isInt32())
+    {
+        types::Int32* pIIn = in[0]->getAs<types::Int32>();
+        pDblIn = new types::Double(pIIn->getDims(), pIIn->getDimsArray());
+        for (int i = 0; i < pIIn->getSize(); i++)
+        {
+            pDblIn->set(i, static_cast<double>(pIIn->get(i)));
+        }
+    }
+    else if (in[0]->isInt64()) // uint
+    {
+        types::Int64* pIIn = in[0]->getAs<types::Int64>();
+        pDblIn = new types::Double(pIIn->getDims(), pIIn->getDimsArray());
+        for (int i = 0; i < pIIn->getSize(); i++)
+        {
+            pDblIn->set(i, static_cast<double>(pIIn->get(i)));
+        }
+    }
+    else if (in[0]->isUInt8())
+    {
+        types::UInt8* pIIn = in[0]->getAs<types::UInt8>();
+        pDblIn = new types::Double(pIIn->getDims(), pIIn->getDimsArray());
+        for (int i = 0; i < pIIn->getSize(); i++)
+        {
+            pDblIn->set(i, static_cast<double>(pIIn->get(i)));
+        }
+    }
+    else if (in[0]->isUInt16())
+    {
+        types::UInt16* pIIn = in[0]->getAs<types::UInt16>();
+        pDblIn = new types::Double(pIIn->getDims(), pIIn->getDimsArray());
+        for (int i = 0; i < pIIn->getSize(); i++)
+        {
+            pDblIn->set(i, static_cast<double>(pIIn->get(i)));
+        }
+    }
+    else if (in[0]->isUInt32())
+    {
+        types::UInt32* pIIn = in[0]->getAs<types::UInt32>();
+        pDblIn = new types::Double(pIIn->getDims(), pIIn->getDimsArray());
+        for (int i = 0; i < pIIn->getSize(); i++)
+        {
+            pDblIn->set(i, static_cast<double>(pIIn->get(i)));
+        }
+    }
+    else if (in[0]->isUInt64())
+    {
+        types::UInt64* pIIn = in[0]->getAs<types::UInt64>();
+        pDblIn = new types::Double(pIIn->getDims(), pIIn->getDimsArray());
+        for (int i = 0; i < pIIn->getSize(); i++)
+        {
+            pDblIn->set(i, static_cast<double>(pIIn->get(i)));
+        }
+    }
+    else
+    {
         std::wstring wstFuncName = L"%"  + in[0]->getShortTypeStr() + L"_sum";
         return Overload::call(wstFuncName, in, _iRetCount, out, new ExecVisitor());
     }
 
-    //don't care of 3rd paramter for double or equivalent type
-    if(in.size() >= 2)
+    if (in.size() == 3)
     {
-         iMode = getMode(in, 1, 0);
+        if (in[2]->isString() == false)
+        {
+            ScierrorW(999, _W("%ls: Wrong type for input argument #%d : A string expected.\n"), L"sum", 3);
+            return types::Function::Error;
+        }
+
+        wchar_t* wcsString = in[2]->getAs<types::String>()->get(0);
+
+        if (wcscmp(wcsString, L"native") == 0)
+        {
+            iOuttype = 1;
+        }
+        else if (wcscmp(wcsString, L"double") == 0)
+        {
+            iOuttype = 2;
+        }
+        else
+        {
+            ScierrorW(999, _W("%ls: Wrong value for input argument #%d : It must be one of the following strings : native or double.\n"), L"sum", 3);
+            return types::Function::Error;
+        }
     }
 
-    if(in[0]->isDouble())
+    if (in.size() >= 2)
     {
-        Double* pIn = in[0]->getAs<Double>();
-        if(pIn->isEmpty())
+        if (in[1]->isDouble())
         {
-            if(iMode == 0)
+            types::Double* pDbl = in[1]->getAs<types::Double>();
+
+            if (pDbl->isScalar() == false)
             {
-                out.push_back(new Double(0));
+                ScierrorW(999, _W("%ls: Wrong value for input argument #%d : A positive scalar expected.\n"), L"sum", 2);
+                return types::Function::Error;
+            }
+
+            iOrientation = static_cast<int>(pDbl->get(0));
+
+            if (iOrientation <= 0)
+            {
+                ScierrorW(999, _W("%ls: Wrong value for input argument #%d : A positive scalar expected.\n"), L"sum", 2);
+                return types::Function::Error;
+            }
+        }
+        else if (in[1]->isString())
+        {
+            wchar_t* wcsString = in[1]->getAs<types::String>()->get(0);
+
+            if (wcscmp(wcsString, L"*") == 0)
+            {
+                iOrientation = 0;
+            }
+            else if (wcscmp(wcsString, L"r") == 0)
+            {
+                iOrientation = 1;
+            }
+            else if (wcscmp(wcsString, L"c") == 0)
+            {
+                iOrientation = 2;
+            }
+            else if (wcscmp(wcsString, L"m") == 0)
+            {
+                int iDims = 0;
+                int* piDimsArray = NULL;
+
+                if (pDblIn)
+                {
+                    iDims = pDblIn->getDims();
+                    piDimsArray = pDblIn->getDimsArray();
+                }
+                else
+                {
+                    iDims = pPolyIn->getDims();
+                    piDimsArray = pPolyIn->getDimsArray();
+                }
+
+                // old function was "mtlsel"
+                for (int i = 0; i < iDims; i++)
+                {
+                    if (piDimsArray[i] > 1)
+                    {
+                        iOrientation = i + 1;
+                        break;
+                    }
+                }
+            }
+            else if ((wcscmp(wcsString, L"native") == 0) && (in.size() == 2))
+            {
+                iOuttype = 1;
+            }
+            else if ((wcscmp(wcsString, L"double") == 0) && (in.size() == 2))
+            {
+                iOuttype = 2;
             }
             else
             {
-                out.push_back(Double::Empty());
+                if (in.size() == 2)
+                {
+                    ScierrorW(999, _W("%ls: Wrong value for input argument #%d : It must be one of the following strings : *, r, c, m, native or double.\n"), L"sum", 2);
+                }
+                else
+                {
+                    ScierrorW(999, _W("%ls: Wrong value for input argument #%d : It must be one of the following strings : *, r, c or m.\n"), L"sum", 2);
+                }
+
+                return types::Function::Error;
             }
         }
         else
         {
-            Double* pOut = NULL;
-            if(iMode == 0)
+            ScierrorW(999, _W("%ls: Wrong type for input argument #%d : A real matrix or a string expected.\n"), L"sum", 2);
+            return types::Function::Error;
+        }
+    }
+
+    /***** perform operation *****/
+    if (pDblIn)
+    {
+        if (pDblIn->isEmpty())
+        {
+            if (iOrientation == 0)
             {
-                double dblR = 0;
-                double dblI = 0;
-
-                for(int i = 0 ; i < pIn->getSize() ; i++)
-                {
-                    dblR += pIn->get(i);
-                }
-
-                pOut = new Double(dblR);
-                if(pIn->isComplex())
-                {
-                    pOut->setComplex(true);
-                    for(int i = 0 ; i < pIn->getSize() ; i++)
-                    {
-                        dblI += pIn->getImg(i);
-                    }
-                    pOut->setImg(0, dblI);
-                }
+                out.push_back(new Double(0));
+                return types::Function::OK;
             }
             else
             {
-                int iDims = pIn->getDims();
-                int* piDims = new int[iDims];
-
-                for(int i = 0 ; i < iDims ; i++)
-                {
-                    if(iMode == i + 1)
-                    {
-                        piDims[i] = 1;
-                    }
-                    else
-                    {
-                        piDims[i] = pIn->getDimsArray()[i];
-                    }
-                }
-
-                pOut = new Double(iDims, piDims);
-                pOut->setZeros();
-
-                double* pdblIn  = pIn->get();
-                double* pdblOut = pOut->get();
-
-                int* piIndex = new int[iDims];
-
-                for(int i = 0 ; i < pIn->getSize() ; i++)
-                {
-                    //get value
-                    double dbl = pdblIn[i];
-
-                    //get array of dim
-                    pIn->getIndexes(i, piIndex);
-
-                    //convert indexes for result
-                    piIndex[iMode - 1] = 0;
-                    int iIndex = pOut->getIndex(piIndex);
-                    pdblOut[iIndex] += dbl;
-                }
+                out.push_back(Double::Empty());
+                return types::Function::OK;
+            }
+        }
+        else if (iOrientation > pDblIn->getDims())
+        {
+            if (in[0]->isDouble())
+            {
+                pDblOut = pDblIn->clone()->getAs<types::Double>();
+            }
+            else
+            {
+                pDblOut = pDblIn;
             }
 
-            out.push_back(pOut);
+            if (in[0]->isBool() == false)
+            {
+                iOuttype = 2;
+            }
+        }
+        else
+        {
+            pDblOut = sum(pDblIn, iOrientation);
+            if (in[0]->isDouble() == false)
+            {
+                delete pDblIn;
+                pDblIn = NULL;
+            }
         }
     }
-    else if(in[0]->isPoly())
+    else if (pPolyIn)
     {
+        iOuttype = 1;
+        if (iOrientation > pPolyIn->getDims())
+        {
+            pPolyOut = pPolyIn->clone()->getAs<types::Polynom>();
+        }
+        else
+        {
+            pPolyOut = sum(pPolyIn, iOrientation);
+        }
     }
-    //else if(in[0]->isBoolean())
-    //{
-    //}
-    //else if(in[0]->isSparse())
-    //{
-    //}
+
+    /***** set result *****/
+    if ((iOuttype == 1) && (in[0]->isDouble() == false))
+    {
+        if (in[0]->isBool())
+        {
+            types::Bool* pBOut = new types::Bool(pDblOut->getDims(), pDblOut->getDimsArray());
+            for (int i = 0; i < pDblOut->getSize(); i++)
+            {
+                pDblOut->get(i) ? pBOut->set(i, 1) : pBOut->set(i, 0);
+            }
+            out.push_back(pBOut);
+        }
+        else if (in[0]->isPoly())
+        {
+            out.push_back(pPolyOut);
+        }
+        else if (in[0]->isInt8())
+        {
+            types::Int8* pIOut = new types::Int8(pDblOut->getDims(), pDblOut->getDimsArray());
+            for (int i = 0; i < pIOut->getSize(); i++)
+            {
+                pIOut->set(i, static_cast<char>(pDblOut->get(i)));
+            }
+            out.push_back(pIOut);
+        }
+        else if (in[0]->isInt16())
+        {
+            types::Int16* pIOut = new types::Int16(pDblOut->getDims(), pDblOut->getDimsArray());
+            for (int i = 0; i < pIOut->getSize(); i++)
+            {
+                pIOut->set(i, static_cast<short int>(pDblOut->get(i)));
+            }
+            out.push_back(pIOut);
+        }
+        else if (in[0]->isInt32())
+        {
+            types::Int32* pIOut = new types::Int32(pDblOut->getDims(), pDblOut->getDimsArray());
+            for (int i = 0; i < pIOut->getSize(); i++)
+            {
+                pIOut->set(i, static_cast<int>(pDblOut->get(i)));
+            }
+            out.push_back(pIOut);
+        }
+        else if (in[0]->isInt64())
+        {
+            types::Int64* pIOut = new types::Int64(pDblOut->getDims(), pDblOut->getDimsArray());
+            for (int i = 0; i < pIOut->getSize(); i++)
+            {
+                pIOut->set(i, static_cast<long long int>(pDblOut->get(i)));
+            }
+            out.push_back(pIOut);
+        }
+        else if (in[0]->isUInt8())
+        {
+            types::UInt8* pIOut = new types::UInt8(pDblOut->getDims(), pDblOut->getDimsArray());
+            for (int i = 0; i < pIOut->getSize(); i++)
+            {
+                pIOut->set(i, static_cast<unsigned char>(pDblOut->get(i)));
+            }
+            out.push_back(pIOut);
+        }
+        else if (in[0]->isUInt16())
+        {
+            types::UInt16* pIOut = new types::UInt16(pDblOut->getDims(), pDblOut->getDimsArray());
+            for (int i = 0; i < pIOut->getSize(); i++)
+            {
+                pIOut->set(i, static_cast<unsigned short int>(pDblOut->get(i)));
+            }
+            out.push_back(pIOut);
+        }
+        else if (in[0]->isUInt32())
+        {
+            types::UInt32* pIOut = new types::UInt32(pDblOut->getDims(), pDblOut->getDimsArray());
+            for (int i = 0; i < pIOut->getSize(); i++)
+            {
+                pIOut->set(i, static_cast<unsigned int>(pDblOut->get(i)));
+            }
+            out.push_back(pIOut);
+        }
+        else if (in[0]->isUInt64())
+        {
+            types::UInt64* pIOut = new types::UInt64(pDblOut->getDims(), pDblOut->getDimsArray());
+            for (int i = 0; i < pIOut->getSize(); i++)
+            {
+                pIOut->set(i, static_cast<unsigned long long int>(pDblOut->get(i)));
+            }
+            out.push_back(pIOut);
+        }
+    }
+    else
+    {
+        out.push_back(pDblOut);
+    }
 
     return Function::OK;
 }
