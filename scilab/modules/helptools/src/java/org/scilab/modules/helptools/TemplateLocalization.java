a2f3670403379b89ed1bdc79d1ca16d269e45b6f
Rui Hirokawa <rui.hirokawa@contrib.scilab.org>
2012-09-26 14:37:15 +0200

    Japanese translation
    Change-Id: I99c08d7c581cd79fc702a4ae6a8d701ca4c86ff1

--- scilab/modules/helptools/src/java/org/scilab/modules/helptools/TemplateLocalization.java
+++ scilab/modules/helptools/src/java/org/scilab/modules/helptools/TemplateLocalization.java
@@ -28,6 +28,7 @@ public final class TemplateLocalization {
         report_an_issue.put("fr_FR", "Reporter un bug");
         report_an_issue.put("pt_BR", "Reportero a bugo");
         report_an_issue.put("es_ES", "Raportare el bugo");
+        report_an_issue.put("ja_JP", "問題を報告");
 
         map.put("Report an issue", report_an_issue);
     }
