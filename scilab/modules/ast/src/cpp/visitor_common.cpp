935e1ece18cc17c652e3a916282248c8dcb5fb50
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2012-11-07 14:26:04 +0100

    add increase and decrease ref for user_data handle property + fix bug with static struct in plot2d functions ( some others need same changes )
    Change-Id: Ic3b47e7fbe87e1d3cd538217e047a531ede22944

--- scilab/modules/ast/src/cpp/visitor_common.cpp
+++ scilab/modules/ast/src/cpp/visitor_common.cpp
@@ -604,7 +604,6 @@ const std::wstring* getStructNameFromExp(const Exp* _pExp)
     {
         std::wostringstream os;
         os << L"Unknow expression";
-        //os << ((Location)e.right_exp_get().location_get()).location_getString() << std::endl;
         throw ScilabError(os.str(), 999, _pExp->location_get());
     }
     return NULL;
@@ -780,15 +779,15 @@ bool getStructFromExp(const Exp* _pExp, types::InternalType** _pMain, types::Int
                             in.push_back(pList);
                         }
 
-                        in.push_back(pMain);
+                        in.push_back(pCurrent);
                         in.front()->IncreaseRef();
-                        pMain->IncreaseRef();
+                        pCurrent->IncreaseRef();
 
                         Function* pCall = (Function*)symbol::Context::getInstance()->get(symbol::Symbol(L"%h_e"));
                         Callable::ReturnValue ret =  pCall->call(in, opt, 1, out, &exec);
 
                         in.front()->DecreaseRef();
-                        pMain->DecreaseRef();
+                        pCurrent->DecreaseRef();
 
                         if (in.front()->isList())
                         {
@@ -849,16 +848,16 @@ bool getStructFromExp(const Exp* _pExp, types::InternalType** _pMain, types::Int
                     std::wstring str = L"%" + _pIT->getShortTypeStr() + L"_i_h";
 
                     in.push_back(_pIT);
-                    in.push_back(pMain);
+                    in.push_back(pCurrent);
                     in.front()->IncreaseRef();
                     _pIT->IncreaseRef();
-                    pMain->IncreaseRef();
+                    pCurrent->IncreaseRef();
 
                     Function* pCall = (Function*)symbol::Context::getInstance()->get(symbol::Symbol(str));
                     Callable::ReturnValue ret =  pCall->call(in, opt, 1, out, &exec);
                     in.front()->DecreaseRef();
                     //_pIT->DecreaseRef();
-                    pMain->DecreaseRef();
+                    pCurrent->DecreaseRef();
 
                     if (in.front()->isList())
                     {
@@ -977,7 +976,6 @@ bool getStructFromExp(const Exp* _pExp, types::InternalType** _pMain, types::Int
                     //manage error
                     std::wostringstream os;
                     os << _W("Invalid Index.\n");
-                    //os << ((Location)e.right_exp_get().location_get()).location_getString() << std::endl;
                     throw ScilabError(os.str(), 999, (*(pCall->args_get().begin()))->location_get());
                 }
 
@@ -1071,6 +1069,20 @@ bool getStructFromExp(const Exp* _pExp, types::InternalType** _pMain, types::Int
                 {
                     //handle
                     GraphicHandle* pCurH = pCurrent->getAs<GraphicHandle>();
+                    InternalType* pIT = pCurH->extract(pCurrentArgs);
+                    if (pIT == NULL)
+                    {
+                        //manage error
+                        std::wostringstream os;
+                        os << _W("Invalid Index.\n");
+                        throw ScilabError(os.str(), 999, (*(pCall->args_get().begin()))->location_get());
+                    }
+                    else
+                    {
+                        delete pIT;
+                    }
+
+                    *_pArgs = pCurrentArgs;
                 }
             }
         }
@@ -1089,7 +1101,6 @@ bool getStructFromExp(const Exp* _pExp, types::InternalType** _pMain, types::Int
     {
         std::wostringstream os;
         os << L"Unknown expression";
-        //os << ((Location)e.right_exp_get().location_get()).location_getString() << std::endl;
         throw ScilabError(os.str(), 999, _pExp->location_get());
     }
     return false;
