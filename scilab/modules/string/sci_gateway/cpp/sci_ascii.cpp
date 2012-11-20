c34a7f82578737715cd56c1ced52deeb65eb7ffb
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2012-11-20 14:31:08 +0100

    rewrite StringToDouble call by ascii gateway
    Change-Id: I84f05689be1618eb475c1adb26bafaf6695a7664

--- scilab/modules/string/sci_gateway/cpp/sci_ascii.cpp
+++ scilab/modules/string/sci_gateway/cpp/sci_ascii.cpp
@@ -21,6 +21,7 @@
 #include "function.hxx"
 #include "string.hxx"
 #include "double.hxx"
+#include "int.hxx"
 
 extern "C"
 {
@@ -29,9 +30,31 @@ extern "C"
 #include "Scierror.h"
 #include "localization.h"
 #include "sciprint.h"
+#include "sci_warning.h"
 }
 
-//#include "sci_warning.h"
+/* Benchmark
+str_test_mat =  ["abscefghijklmnopqrstuvxyz","abscefghijklmnopqrstuvxyz", ..
+"abscefghijklmnopqrstuvxyz","abscefghijklmnopqrstuvxyz"; ..
+"abscefghijklmnopqrstuvxyz","abscefghijklmnopqrstuvxyz", ..
+"abscefghijklmnopqrstuvxyz","abscefghijklmnopqrstuvxyz"];
+
+tic();
+for i=1:10000000
+ascii(["abscefghijklmnopqrstuvxyz","abscefghijklmnopqrstuvxyz"]);
+end
+duree = toc();
+
+printf("\nDUREE 1 = %d seconds\n\n",duree);
+*/
+
+/* on windows 17 3770 @ 3.4 GHz */
+/* scilab 4.1.2 : 17 s */
+/* scilab 5.4.0 : 32 s */
+/* scilab 6 dev : 16 s */
+
+
+
 
 using namespace types;
 
@@ -42,335 +65,45 @@ Double* StringToDouble(String* _pst);
 /*----------------------------------------------------------------------------*/
 Function::ReturnValue sci_ascii(typed_list &in, int _iRetCount, typed_list &out)
 {
-    if(in.size() != 1)
+    if (in.size() != 1)
     {
         Scierror(77, _("%s: Wrong number of input argument(s): %d expected.\n"), "ascii", 1);
         return Function::Error;
     }
 
     InternalType* pOut;
-    switch(in[0]->getType())
+    switch (in[0]->getType())
     {
-    case InternalType::RealDouble :
+        case InternalType::RealDouble :
         {
             pOut = DoubleToString(in[0]->getAs<Double>());
             break;
         }
-    case InternalType::RealString :
+        case InternalType::RealString :
         {
             pOut = StringToDouble(in[0]->getAs<types::String>());
             break;
         }
-    case InternalType::RealInt8 :
-    case InternalType::RealUInt8 :
-    case InternalType::RealInt16 :
-    case InternalType::RealUInt16 :
-    case InternalType::RealInt32 :
-    case InternalType::RealUInt32 :
-    case InternalType::RealInt64 :
-    case InternalType::RealUInt64 :
+        case InternalType::RealInt8 :
+        case InternalType::RealUInt8 :
+        case InternalType::RealInt16 :
+        case InternalType::RealUInt16 :
+        case InternalType::RealInt32 :
+        case InternalType::RealUInt32 :
+        case InternalType::RealInt64 :
+        case InternalType::RealUInt64 :
         {
             pOut = IntToString(in[0]);
             break;
         }
-    default :
-        Scierror(999, _("%s: Wrong type for argument #%d: Matrix of strings or Integer matrix expected.\n"), "ascii", 1);
-        return Function::Error;
+        default :
+            Scierror(999, _("%s: Wrong type for argument #%d: Matrix of strings or Integer matrix expected.\n"), "ascii", 1);
+            return Function::Error;
     }
 
     out.push_back(pOut);
     return Function::OK;
 
-//	CheckRhs(1,1);
-//	CheckLhs(0,1);
-//
-//	switch ( GetType(1))
-//	{
-//		case sci_strings:
-//			asciiStrings(fname);
-//		break;
-//
-//		case sci_matrix :
-//			asciiMatrix(fname);
-//		break;
-//
-//		case sci_ints :
-//			asciiIntMatrix(fname);
-//		break;
-//
-//		default:
-//			asciiOthers(fname);
-//		break;
-//  }
-//  return 0;
-//}
-///*--------------------------------------------------------------------------*/
-//static int asciiStrings(char *fname)
-//{
-//	/* interface written with stack3 */
-//	/* 3 phases :
-//		 1] get data from stack
-//			conversion scilab code to ascii and strings (char **)
-//		 2] algo. conversion ascii to values
-//		 3] put results on stack
-//	*/
-//
-//	/* interface written with stack1*/
-//	/* it works immediately on stack (read and write)
-//	conversion scilab code to ascii values
-//	*/
-//
-//	/* Benchmark
-//	str_test_mat =  ["abscefghijklmnopqrstuvxyz","abscefghijklmnopqrstuvxyz", ..
-//	"abscefghijklmnopqrstuvxyz","abscefghijklmnopqrstuvxyz"; ..
-//	"abscefghijklmnopqrstuvxyz","abscefghijklmnopqrstuvxyz", ..
-//	"abscefghijklmnopqrstuvxyz","abscefghijklmnopqrstuvxyz"];
-//
-//	tic();
-//	for i=1:10000000
-//	ascii(["abscefghijklmnopqrstuvxyz","abscefghijklmnopqrstuvxyz"]);
-//	end
-//	duree = toc();
-//
-//	printf("\nDUREE 1 = %d seconds\n\n",duree);
-//	*/
-//
-//	/* on windows C2D 6600 2400 mhz */
-//	/* scilab 4.1.2 : 40 s */
-//	/* scilab with stack3 interface : 75 s*/
-//	/* scilab with stack1 interface : 41 s */
-//
-///*
-//	char **Input_StringMatrix = NULL;
-//	int x = 0,y = 0,Row_Num = 0,Col_Num = 0;
-//
-//	int Length_Output_Matrix = 0;
-//	double *Output_IntMatrix = NULL;
-//	int nbOutput_IntMatrix = 0;
-//	int numRow = 1;
-//
-//	GetRhsVar(1,MATRIX_OF_STRING_DATATYPE,&Row_Num,&Col_Num,&Input_StringMatrix);
-//
-//	Length_Output_Matrix = 0;
-//	for (x = 0;x < Row_Num*Col_Num;x++) Length_Output_Matrix = Length_Output_Matrix + (int)strlen(Input_StringMatrix[x]);
-//
-//	if (Length_Output_Matrix !=0)
-//	{
-//		Output_IntMatrix = (double*)MALLOC(sizeof(double)*(Length_Output_Matrix));
-//	}
-//	else Output_IntMatrix = (double*)MALLOC(sizeof(double));
-//
-//	if (Output_IntMatrix == NULL)
-//	{
-//		freeArrayOfString(Input_StringMatrix,Row_Num*Col_Num);
-//		Scierror(999,_("%s: Memory allocation error.\n"),fname);
-//		return 0;
-//	}
-//
-//	for (x = 0; x < Row_Num*Col_Num; x++)
-//	{
-//		for (y = 0;y < (int)strlen(Input_StringMatrix[x]); y++)
-//		{
-//			Output_IntMatrix[nbOutput_IntMatrix]=Input_StringMatrix[x][y];
-//			nbOutput_IntMatrix++;
-//		}
-//	}
-//
-//	freeArrayOfString(Input_StringMatrix,Row_Num*Col_Num);
-//
-//
-//	CreateVarFromPtr(Rhs + 1,MATRIX_OF_DOUBLE_DATATYPE,&numRow,&nbOutput_IntMatrix,&Output_IntMatrix);
-//
-//	LhsVar(1) = Rhs + 1 ;
-//	C2F(putlhsvar)();
-//	if (Output_IntMatrix) { FREE(Output_IntMatrix); Output_IntMatrix=NULL;}
-//	return 0;
-//*/
-//
-//	BOOL is_a_reference_on_stack = FALSE; /* variable is a reference on stack */
-//	int i = 0, j = 0;
-//	int nbr_characters  = 0;
-//	int  l = 0, il = 0, lr = 0;
-//	int ilr = 0;
-//
-//	il = iadr( C2F(vstk).lstk[Top - 1] );
-//
-//	ilr = il;
-//	if (*istk(il) < 0)
-//	{
-//		il = iadr(*istk(il+1));
-//	}
-//	/* check if parameter is a reference */
-//	is_a_reference_on_stack = (il != ilr);
-//
-//	/* find number of characters */
-//	nbr_characters = *istk(il + 4 + *istk(il + 1) * *istk(il + 2)) - 1;
-//
-//	l = il + 5 + *istk(il+1) * *istk(il+2);
-//
-//	if (is_a_reference_on_stack)
-//	{
-//		/* it is a reference on stack : txt = 'Scilab' ; ascii(txt) */
-//		j = ilr + 4;
-//		Err = j / 2 + 1 + nbr_characters - C2F(vstk).lstk[Bot - 1];
-//		if (Err > 0)
-//		{
-//			/* stacksize exceeded */
-//			SciError(17);
-//			return 0;
-//		}
-//	}
-//	else
-//	{
-//		/* it is not a reference on stack  ascii('Scilab') */
-//		int one = 1;
-//		int lw = l + nbr_characters;
-//
-//		j = lw + nbr_characters;
-//		Err = j / 2 + 1 - C2F(vstk).lstk[Bot - 1];
-//		if (Err > 0)
-//		{
-//			/* stacksize exceeded */
-//			SciError(17);
-//			return 0;
-//		}
-//		C2F(icopy)(&nbr_characters, istk(l), &one, istk(l + nbr_characters), &one);
-//		l = lw;
-//	}
-//
-//	/* create output int matrix , see C2F(crematvar) stack1.c */
-//	*istk(ilr) = sci_matrix;
-//	*istk(ilr+1) = 1;
-//	*istk(ilr+2) = nbr_characters;
-//	*istk(ilr+3) = 0;
-//
-//	j = ilr + 4;
-//	lr = j / 2 + 1;
-//
-//	/* put each value on stack */
-//	for (i = 0; i < nbr_characters; i++)
-//	{
-//		int scilab_code = *istk(l + i);
-//		*stk(lr + i) = convertScilabCodeToAsciiCode(scilab_code);
-//	}
-//	/* update Top of stack */
-//	C2F(vstk).lstk[Top] = lr + nbr_characters;
-//
-//	return 0;
-//
-//}
-///*--------------------------------------------------------------------------*/
-//static int asciiMatrix(char *fname)
-//{
-//	int Row_Num = 0,Col_Num = 0,Stack_Pos = 0;
-//	int outIndex = 0 ;
-//	int len = 0;
-//	BOOL FirstWarning = TRUE;
-//	/*When input vector of int ascii codes  */
-//	GetRhsVar(1,MATRIX_OF_INTEGER_DATATYPE,&Row_Num,&Col_Num,&Stack_Pos);
-//	len = Row_Num * Col_Num ;
-//
-//	if (len != 0)
-//	{
-//		int x = 0;
-//		int one = 1;
-//		int *Input_IntMatrix = NULL;
-//		char *Output_StringMatrix = NULL;
-//
-//		Input_IntMatrix = istk(Stack_Pos);
-//
-//		outIndex = 0 ;
-//		CreateVar(Rhs+1,STRING_DATATYPE,&len,&one,&outIndex);
-//		Output_StringMatrix = cstk(outIndex);
-//		for (x = 0; x < len; x++)
-//		{
-//			if ( FirstWarning )
-//			{
-//				if ( (Input_IntMatrix[x] < ASCII_MIN) || (Input_IntMatrix[x] > ASCII_MAX) )
-//				{
-//					if (getWarningMode())
-//					{
-//						sciprint(_("WARNING : \n"));
-//						sciprint(_("%s: Wrong value for input argument #%d: Must be between %d and %d.\n"),fname,1,ASCII_MIN,ASCII_MAX);
-//					}
-//					FirstWarning = FALSE;
-//				}
-//			}
-//
-//			if (isascii(Input_IntMatrix[x]))
-//			{
-//				Output_StringMatrix[x] = (char)toascii(Input_IntMatrix[x]);
-//			}
-//			else
-//			{
-//				Output_StringMatrix[x] = (char)Input_IntMatrix[x];
-//			}
-//		}
-//		Output_StringMatrix[len] = '\0';
-//	}
-//	else
-//	{
-//		#define EMPTY_STR ""
-//		int one    = 1 ;
-//		int lenStr   = (int)strlen(EMPTY_STR);
-//		outIndex = 0 ;
-//
-//		CreateVar(Rhs+1,STRING_DATATYPE,&lenStr,&one,&outIndex);
-//		strcpy(cstk(outIndex),EMPTY_STR);
-//	}
-//
-//	LhsVar(1) = Rhs+1 ;
-//	C2F(putlhsvar)();
-//	return 0;
-//}
-//
-///*--------------------------------------------------------------------------*/
-//static int asciiIntMatrix(char *fname)
-//{
-//	int Row_Num = 0,Col_Num = 0;
-//	int outIndex = 0 ;
-//	int len = 0;
-//
-//	SciIntMat M;
-//
-//	GetRhsVar(1,MATRIX_OF_VARIABLE_SIZE_INTEGER_DATATYPE,&Row_Num,&Col_Num,&M);
-//	len = Row_Num * Col_Num ;
-//
-//	if (len != 0)
-//	{
-//		int one        = 1;
-//		int ichar      = I_UCHAR;
-//		static int inc = 1;
-//
-//		char *Output_StringMatrix = NULL;
-//
-//		CreateVar(Rhs+1,STRING_DATATYPE,&len,&one,&outIndex);
-//		Output_StringMatrix = cstk(outIndex);
-//
-//		/* from intxx to char */
-//		C2F(tpconv)(&M.it,&ichar,&len, M.D, &inc, Output_StringMatrix, &inc);
-//		Output_StringMatrix[len] = '\0';
-//	}
-//	else
-//	{
-//		#define EMPTY_STR ""
-//		int one    = 1 ;
-//		int lenStr = (int)strlen(EMPTY_STR);
-//
-//		CreateVar(Rhs+1,STRING_DATATYPE,&lenStr,&one,&outIndex);
-//		strcpy(cstk(outIndex),EMPTY_STR);
-//	}
-//
-//	LhsVar(1) = Rhs+1 ;
-//	C2F(putlhsvar)();
-//	return 0;
-//}
-//
-///*--------------------------------------------------------------------------*/
-//static int asciiOthers(char *fname)
-//{
-//	Scierror(999,_("%s: Wrong type for argument #%d: Matrix of strings or Integer matrix expected.\n"),fname,1);
-//	return 0;
 }
 /*--------------------------------------------------------------------------*/
 String* DoubleToString(Double* _pdbl)
@@ -380,16 +113,24 @@ String* DoubleToString(Double* _pdbl)
     memset(pst, 0x00, _pdbl->getSize() + 1);
     double* pdbl = _pdbl->getReal();
 
-    bool bWarning = false;
-    for(int i = 0 ; i < _pdbl->getSize() ; i++)
+    bool bWarning = getWarningMode() == 0;
+    for (int i = 0 ; i < _pdbl->getSize() ; i++)
     {
-        if(bWarning == false && pdbl[i] > 255)
+        if (bWarning == false && pdbl[i] > 255)
         {
             sciprint(_("WARNING : \n"));
             sciprint(_("%ls: Wrong value for input argument #%d: Must be between %d and %d.\n"), L"ascii", 1, 0, 255);
             bWarning = true;
         }
-        pst[i] = (char)toascii((int)pdbl[i]);
+
+        if (pdbl[i] == 0)
+        {
+            pst[i] = ' ';
+        }
+        else
+        {
+            pst[i] = (char)pdbl[i];
+        }
     }
 
     wchar_t* pwst = to_wide_string(pst);
@@ -401,6 +142,166 @@ String* DoubleToString(Double* _pdbl)
 String* IntToString(InternalType* _pIT)
 {
     String* pOut = NULL;
+    char* pst = NULL;
+    switch (_pIT->getType())
+    {
+        case InternalType::RealInt8 :
+        {
+            Int8* pI = _pIT->getAs<Int8>();
+            char* p = pI->get();
+            pst = (char*)MALLOC(sizeof(char) * pI->getSize() + 1);
+
+            for (int i = 0 ; i < pI->getSize() ; i++)
+            {
+                if (p[i] == 0)
+                {
+                    pst[i] = ' ';
+                }
+                else
+                {
+                    pst[i] = p[i];
+                }
+            }
+            break;
+        }
+        case InternalType::RealUInt8 :
+        {
+            UInt8* pI = _pIT->getAs<UInt8>();
+            unsigned char* p = pI->get();
+            pst = (char*)MALLOC(sizeof(char) * pI->getSize() + 1);
+
+            for (int i = 0 ; i < pI->getSize() ; i++)
+            {
+                if (p[i] == 0)
+                {
+                    pst[i] = ' ';
+                }
+                else
+                {
+                    pst[i] = p[i];
+                }
+            }
+            break;
+        }
+        case InternalType::RealInt16 :
+        {
+            Int16* pI = _pIT->getAs<Int16>();
+            short* p = pI->get();
+            pst = (char*)MALLOC(sizeof(char) * pI->getSize() + 1);
+
+            for (int i = 0 ; i < pI->getSize() ; i++)
+            {
+                if (p[i] == 0)
+                {
+                    pst[i] = ' ';
+                }
+                else
+                {
+                    pst[i] = p[i];
+                }
+            }
+            break;
+        }
+        case InternalType::RealUInt16 :
+        {
+            UInt16* pI = _pIT->getAs<UInt16>();
+            unsigned short* p = pI->get();
+            pst = (char*)MALLOC(sizeof(char) * pI->getSize() + 1);
+
+            for (int i = 0 ; i < pI->getSize() ; i++)
+            {
+                if (p[i] == 0)
+                {
+                    pst[i] = ' ';
+                }
+                else
+                {
+                    pst[i] = p[i];
+                }
+            }
+            break;
+        }
+        case InternalType::RealInt32 :
+        {
+            Int32* pI = _pIT->getAs<Int32>();
+            int* p = pI->get();
+            pst = (char*)MALLOC(sizeof(char) * pI->getSize() + 1);
+
+            for (int i = 0 ; i < pI->getSize() ; i++)
+            {
+                if (p[i] == 0)
+                {
+                    pst[i] = ' ';
+                }
+                else
+                {
+                    pst[i] = p[i];
+                }
+            }
+            break;
+        }
+        case InternalType::RealUInt32 :
+        {
+            UInt32* pI = _pIT->getAs<UInt32>();
+            unsigned int* p = pI->get();
+            pst = (char*)MALLOC(sizeof(char) * pI->getSize() + 1);
+
+            for (int i = 0 ; i < pI->getSize() ; i++)
+            {
+                if (p[i] == 0)
+                {
+                    pst[i] = ' ';
+                }
+                else
+                {
+                    pst[i] = p[i];
+                }
+            }
+            break;
+        }
+        case InternalType::RealInt64 :
+        {
+            Int64* pI = _pIT->getAs<Int64>();
+            long long* p = pI->get();
+            pst = (char*)MALLOC(sizeof(char) * pI->getSize() + 1);
+
+            for (int i = 0 ; i < pI->getSize() ; i++)
+            {
+                if (p[i] == 0)
+                {
+                    pst[i] = ' ';
+                }
+                else
+                {
+                    pst[i] = p[i];
+                }
+            }
+            break;
+        }
+        case InternalType::RealUInt64 :
+        {
+            UInt64* pI = _pIT->getAs<UInt64>();
+            unsigned long long* p = pI->get();
+            pst = (char*)MALLOC(sizeof(char) * pI->getSize() + 1);
+
+            for (int i = 0 ; i < pI->getSize() ; i++)
+            {
+                if (p[i] == 0)
+                {
+                    pst[i] = ' ';
+                }
+                else
+                {
+                    pst[i] = p[i];
+                }
+            }
+            break;
+        }
+    }
+
+    wchar_t* pwst = to_wide_string(pst);
+    pOut = new String(1, 1);
+    pOut->set(0, 0, pwst);
     return pOut;
 }
 /*--------------------------------------------------------------------------*/
@@ -408,35 +309,44 @@ Double* StringToDouble(String* _pst)
 {
     Double* pOut = NULL;
     /*compute total length*/
-    for(int i = 0 ; i < _pst->getSize() ; i++)
+    int iTotalLen = 0;
+    int iSize = _pst->getSize();
+    char** pst = new char*[iSize];
+    int* pstLen = new int[iSize];
+    for (int i = 0 ; i < iSize ; i++)
     {
-        char* pst = wide_string_to_UTF8(_pst->get(i));
-        if(pOut == NULL)
-        {
-            pOut = new Double(1, (int)strlen(pst));
-            int iLen = (int)strlen(pst);
-            double* pD = pOut->getReal();
-            for(int j = 0 ; j < iLen ; j++)
-            {
-                pD[j] = pst[j];
-            }
-        }
-        else
-        {
-            int iLen = (int)strlen(pst);
-            Double *pIn = new Double(1, iLen);
-            double* pD = pIn->getReal();
-            for(int j = 0 ; j < iLen ; j++)
-            {
-                pD[j] = pst[j];
-            }
+        pst[i] = wide_string_to_UTF8(_pst->get(i));
+        pstLen[i] = (int)strlen(pst[i]);
+        iTotalLen += pstLen[i];
+    }
 
-            int iOldCols = pOut->getCols();
-            pOut->resize(1, iOldCols + iLen);
-            pOut->append(0, iOldCols, pIn);
-            delete pIn;
+    if (iTotalLen == 0)
+    {
+        return Double::Empty();
+    }
+
+    pOut = new Double(1, iTotalLen);
+    double* pdbl = pOut->get();
+    int index = 0;
+
+    //for each element of input string matrix
+    for (int i = 0 ; i < iSize ; i++)
+    {
+        //for each character of input string matrix
+        for (int j = 0 ; j < pstLen[i] ; j++)
+        {
+            //transform character value as double.
+            pdbl[index++] = pst[i][j];
         }
     }
+
+    delete[] pstLen;
+    for (int i = 0 ; i < iSize ; i++)
+    {
+        delete pst[i];
+    }
+
+    delete[] pst;
     return pOut;
 }
 /*--------------------------------------------------------------------------*/
