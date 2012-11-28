15020e08dbc8a8daabdc9852aa05e2e6449d0b50
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2012-11-28 17:36:40 +0100

    module polynom plugged.
    Change-Id: I6d3552ad852057c1c26a2e74f9d5c3960cf0f7a5

---
+++ scilab/modules/polynomials/sci_gateway/cpp/polynomials_gw.cpp
@@ -0,0 +1,27 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2012 - Scilab Enterprises - Cedric DELAMARRE
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+#include "polynomials_gw.hxx"
+#include "context.hxx"
+
+#define MODULE_NAME L"polynomials"
+extern "C"
+{
+#include "gw_polynomials.h"
+}
+
+int PolynomialsModule::Load()
+{
+    symbol::Context::getInstance()->AddFunction(types::Function::createFunction(L"poly", &sci_poly, MODULE_NAME));
+    return 1;
+}
+
