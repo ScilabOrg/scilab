11fb163457c2a22b9bf176ee4cdfc3a893e73fdc
Calixte DENIZET <calixte.denizet@scilab-enterprises.com>
2013-01-18 11:40:48 +0100

    Fix for images loading in help under Windows
    Change-Id: Ia792eba1461a1eb2d9a6ae377ae97315c3f7b52e

--- scilab/modules/commons/src/java/org/scilab/modules/commons/jarsci/Handler.java
+++ scilab/modules/commons/src/java/org/scilab/modules/commons/jarsci/Handler.java
@@ -25,7 +25,7 @@ import org.scilab.modules.commons.ScilabConstants;
  */
 public class Handler extends URLStreamHandler {
     
-    private static String JARPATH = new File(ScilabConstants.SCI, "/modules/helptools/jar/scilab_images.jar").getAbsolutePath();
+    private static String JARPATH = new File(ScilabConstants.SCI, "/modules/helptools/jar/scilab_images.jar").getAbsoluteFile().toURI().toString();
 
     public Handler() {
 	
@@ -40,7 +40,7 @@ public class Handler extends URLStreamHandler {
 	    path = "/" + path;
 	}
 
-        URL jar = new URL("jar:file://" + JARPATH + "!" + path);
+        URL jar = new URL("jar:" + JARPATH + "!" + path);
 	return jar.openConnection();
     }
 }
