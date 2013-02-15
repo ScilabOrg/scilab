36f959816e731f5323e89e7d9b863a3fcaafcc79
Calixte DENIZET <calixte.denizet@scilab-enterprises.com>
2013-02-15 14:44:33 +0100

    Bug 12248 fixed: SciNotes toolbar had disappeared...
    Change-Id: I0cedcab6020f4177d241ba06bd7be027f26d2175

--- scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/tab/SwingScilabTab.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/tab/SwingScilabTab.java
@@ -1142,12 +1142,13 @@ public class SwingScilabTab extends View implements SwingViewObject, SimpleTab, 
      */
     @Override
     public void setMenuBar(MenuBar newMenuBar) {
-        if (this.menuBar != null) {
-            ((SwingScilabMenuBar) this.menuBar.getAsSimpleMenuBar()).close();
+        if (this.menuBar != newMenuBar) {
+            if (this.menuBar != null) {
+                ((SwingScilabMenuBar) this.menuBar.getAsSimpleMenuBar()).close();
+            }
+            this.menuBar = newMenuBar;
         }
-        this.menuBar = newMenuBar;
     }
-
 
     /**
      * Getter for MenuBar
@@ -1166,10 +1167,12 @@ public class SwingScilabTab extends View implements SwingViewObject, SimpleTab, 
      */
     @Override
     public void setToolBar(ToolBar newToolBar) {
-        if (this.toolBar != null) {
-            ((SwingScilabToolBar) this.toolBar.getAsSimpleToolBar()).close();
+        if (this.toolBar != newToolBar) {
+            if (this.toolBar != null) {
+                ((SwingScilabToolBar) this.toolBar.getAsSimpleToolBar()).close();
+            }
+            this.toolBar = newToolBar;
         }
-        this.toolBar = newToolBar;
     }
 
     /**
