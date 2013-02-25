3d7bc814713697f1308201d3c698b750c0d51bc2
Paul BIGNIER <paul.bignier@scilab-enterprises.com>
2013-02-25 11:49:52 +0100

    Improvements in scicos.c
    Corrected typos and created function pointers for more efficiency and readability
    
    Change-Id: I855a6a5af7a82cdf8fe660f9d51852370b11ef44

--- scilab/modules/scicos/src/c/scicos.c
+++ scilab/modules/scicos/src/c/scicos.c
@@ -47,7 +47,7 @@
 #include <ida/ida_direct.h>
 #include <nvector/nvector_serial.h>  /* serial N_Vector types, fcts., and macros */
 #include <sundials/sundials_dense.h> /* prototypes for various DlsMat operations */
-#include <sundials/sundials_direct.h> /* definitions DlsMat and DENSE_ELEM */
+#include <sundials/sundials_direct.h> /* definitions of DlsMat and DENSE_ELEM */
 #include <sundials/sundials_types.h> /* definition of type realtype */
 #include <sundials/sundials_math.h>
 #include <kinsol/kinsol.h>
@@ -108,7 +108,7 @@ SCICOS_IMPEXP SCSPTR_struct C2F(scsptr);
 	if ( ng>0 ) FREE(zcros);
 
 
-/* TJacque allocates by sundials */
+/* TJacque allocated by sundials */
 #define freeallx				\
 	if (*neq>0) free(TJacque);	\
 	if (*neq>0) FREE(data->rwork);		\
@@ -788,7 +788,7 @@ int C2F(scicos)(double *x_in, int *xptr_in, double *z__,
 
     if (*flag__ == 1)   /*start*/
     {
-        /*     initialisation des blocks */
+        /*      blocks initialization */
         for (kf = 0; kf < nblk; ++kf)
         {
             *(Blocks[kf].work) = NULL;
@@ -808,40 +808,21 @@ int C2F(scicos)(double *x_in, int *xptr_in, double *z__,
     {
 
         /*     integration */
-        if (C2F(cmsolver).solver == 0)        /*  LSODAR: Method: DYNAMIC, Nonlinear solver= DYNAMIC */
+        if (C2F(cmsolver).solver >= 0 && C2F(cmsolver).solver <= 7)
         {
+            /*  0 - LSODAR: Method: DYNAMIC, Nonlinear solver= DYNAMIC */
+            /*  1 - CVODE: Method: BDF,   Nonlinear solver= NEWTON     */
+            /*  2 - CVODE: Method: BDF,   Nonlinear solver= FUNCTIONAL */
+            /*  3 - CVODE: Method: ADAMS, Nonlinear solver= NEWTON     */
+            /*  4 - CVODE: Method: ADAMS, Nonlinear solver= FUNCTIONAL */
+            /*  5 - DOPRI: Method: Dormand-Prince, Nonlinear solver=  */
+            /*  6 - RK45: Method: Runge-Kutta, Nonlinear solver=  */
+            /*  7 - ImpRK45: Method: Runge-Kutta, Nonlinear solver= FIXED-POINT */
             cossim(t0);
         }
-        else if (C2F(cmsolver).solver == 1)   /*  CVODE: Method: BDF,   Nonlinear solver= NEWTON     */
+        else if (C2F(cmsolver).solver == 100)
         {
-            cossim(t0);
-        }
-        else if (C2F(cmsolver).solver == 2)   /*  CVODE: Method: BDF,   Nonlinear solver= FUNCTIONAL */
-        {
-            cossim(t0);
-        }
-        else if (C2F(cmsolver).solver == 3)   /*  CVODE: Method: ADAMS, Nonlinear solver= NEWTON     */
-        {
-            cossim(t0);
-        }
-        else if (C2F(cmsolver).solver == 4)   /*  CVODE: Method: ADAMS, Nonlinear solver= FUNCTIONAL */
-        {
-            cossim(t0);
-        }
-        else if (C2F(cmsolver).solver == 5)   /*  DOPRI: Method: Dormand-Prince, Nonlinear solver=  */
-        {
-            cossim(t0);
-        }
-        else if (C2F(cmsolver).solver == 6)   /*  RK45: Method: Runge-Kutta, Nonlinear solver=  */
-        {
-            cossim(t0);
-        }
-        else if (C2F(cmsolver).solver == 7)   /*  ImpRK45: Method: Runge-Kutta, Nonlinear solver= FIXED-POINT */
-        {
-            cossim(t0);
-        }
-        else if (C2F(cmsolver).solver == 100)  /* IDA  : Method:       , Nonlinear solver=  */
-        {
+            /* IDA  : Method:       , Nonlinear solver=  */
             cossimdaskr(t0);
         }
         else
@@ -860,7 +841,7 @@ int C2F(scicos)(double *x_in, int *xptr_in, double *z__,
     }
     else if (*flag__ == 3)     /*finish*/
     {
-        /*     fermeture des blocks */
+        /*     blocks closing */
         cosend(t0);
     }
     else if (*flag__ == 4)     /*linear*/
@@ -877,7 +858,7 @@ int C2F(scicos)(double *x_in, int *xptr_in, double *z__,
                 return 0;
             }
 
-            /*---------à la place de old simblk--------*/
+            /*---------instead of old simblk--------*/
             /*  C2F(simblk)(&nx, t0, x, W);  */
 
             if (ng > 0 && nmod > 0)
@@ -1329,6 +1310,29 @@ static void cossim(double *told)
     void *cvode_mem = NULL;
     int flag = 0, flagr = 0;
     int cnt = 0;
+    // Defining function pointers, for more readability
+    int (* ODE) (void*, realtype, N_Vector, realtype*, enum iTask_t);
+    int (* ODEInit) (void*, void*, realtype, N_Vector);
+    int (* ODEReInit) (void*, realtype, N_Vector);
+    int (* ODERootInit) (void*, int, void*);
+    int (* ODESetMaxStep) (void*, realtype);
+    int (* ODESetStopTime) (void*, realtype);
+    int (* ODEGetRootInfo) (void*, int*);
+    int (* ODESStolerances) (void*, realtype, realtype);
+    int (* ODESetErrHandlerFn) (void*, CVErrHandlerFn, void*);
+    void (*simb), (*grb);
+    int isCVode = C2F(cmsolver).solver != 0; // Boolean
+    simb = isCVode ? &simblk : simblklsodar;
+    grb = isCVode ? &grblk : grblklsodar;
+	ODE = isCVode ? &CVode : &LSodar;
+	ODEInit = isCVode ? &CVodeInit : &LSodarInit;
+	ODEReInit = isCVode ? &CVodeReInit : &LSodarReInit;
+	ODERootInit = isCVode ? &CVodeRootInit : &LSodarRootInit;
+    ODESetMaxStep = isCVode ? &CVodeSetMaxStep : &LSodarSetMaxStep;
+	ODESetStopTime = isCVode ? &CVodeSetStopTime : &LSodarSetStopTime;
+	ODEGetRootInfo = isCVode ? &CVodeGetRootInfo : &LSodarGetRootInfo;
+    ODESStolerances = isCVode ? &CVodeSStolerances : &LSodarSStolerances;
+	ODESetErrHandlerFn = isCVode ? &CVodeSetErrHandlerFn : &LSodarSetErrHandlerFn;
     jroot = NULL;
     if (ng != 0)
     {
@@ -1413,12 +1417,7 @@ static void cossim(double *told)
             return;
         }
 
-        if (!C2F(cmsolver).solver)
-        {
-            flag = LSodarSetErrHandlerFn(cvode_mem, SundialsErrHandler, NULL);
-        }
-        else
-        flag = CVodeSetErrHandlerFn(cvode_mem, SundialsErrHandler, NULL);
+        flag = ODESetErrHandlerFn(cvode_mem, SundialsErrHandler, NULL);
         if (check_flag(&flag, "CVodeSetErrHandlerFn", 1))
         {
             *ierr = 300 + (-flag);
@@ -1426,12 +1425,7 @@ static void cossim(double *told)
             return;
         }
 
-        if (!C2F(cmsolver).solver)
-        {
-            flag = LSodarInit(cvode_mem, simblklsodar, T0, y);
-        }
-        else
-        flag = CVodeInit(cvode_mem, simblk, T0, y);
+        flag = ODEInit (cvode_mem, simb, T0, y);
         if (check_flag(&flag, "CVodeInit", 1))
         {
             *ierr = 300 + (-flag);
@@ -1439,12 +1433,7 @@ static void cossim(double *told)
             return;
         }
 
-        if (!C2F(cmsolver).solver)
-        {
-            flag = LSodarSStolerances(cvode_mem, reltol, abstol);
-        }
-        else
-        flag = CVodeSStolerances(cvode_mem, reltol, abstol);
+        flag = ODESStolerances(cvode_mem, reltol, abstol);
         if (check_flag(&flag, "CVodeSStolerances", 1))
         {
             *ierr = 300 + (-flag);
@@ -1452,12 +1441,7 @@ static void cossim(double *told)
             return;
         }
 
-        if (!C2F(cmsolver).solver)
-        {
-            flag = LSodarRootInit(cvode_mem, ng, grblklsodar);
-        }
-        else
-        flag = CVodeRootInit(cvode_mem, ng, grblk);
+        flag = ODERootInit(cvode_mem, ng, grb);
         if (check_flag(&flag, "CVodeRootInit", 1))
         {
             *ierr = 300 + (-flag);
@@ -1465,7 +1449,7 @@ static void cossim(double *told)
             return;
         }
 
-        if (C2F(cmsolver).solver)
+        if (isCVode)
         /* Call CVDense to specify the CVDENSE dense linear solver */
         flag = CVDense(cvode_mem, *neq);
         if (check_flag(&flag, "CVDense", 1))
@@ -1477,12 +1461,7 @@ static void cossim(double *told)
 
         if (hmax > 0)
         {
-            if (!C2F(cmsolver).solver)
-            {
-                flag = LSodarSetMaxStep(cvode_mem, (realtype) hmax);
-            }
-            else
-            flag = CVodeSetMaxStep(cvode_mem, (realtype) hmax);
+            flag = ODESetMaxStep(cvode_mem, (realtype) hmax);
             if (check_flag(&flag, "CVodeSetMaxStep", 1))
             {
                 *ierr = 300 + (-flag);
@@ -1516,12 +1495,12 @@ static void cossim(double *told)
             ++jj;
         }
     }
-    /*     . Il faut:  ng >= jj */
+    /*     . ng >= jj required */
     if (jj != ng)
     {
         zcros[jj] = -1;
     }
-    /*     initialisation (propagation of constant blocks outputs) */
+    /*     initialization (propagation of constant blocks outputs) */
     idoit(told);
     if (*ierr != 0)
     {
@@ -1550,7 +1529,7 @@ static void cossim(double *told)
         while (ismenu()) //** if the user has done something, do the actions
         {
             int ierr2 = 0;
-            SeqSync = GetCommand(CommandToUnstack); //** get at the action
+            SeqSync = GetCommand(CommandToUnstack); //** get to the action
             CommandLength = (int)strlen(CommandToUnstack);
             syncexec(CommandToUnstack, &CommandLength, &ierr2, &one, CommandLength); //** execute it
         }
@@ -1645,12 +1624,7 @@ L30:
 
                 if (hot == 0) /* hot==0 : cold restart*/
                 {
-                    if (!C2F(cmsolver).solver)
-                    {
-                        flag = LSodarSetStopTime(cvode_mem, (realtype)tstop);
-                    }
-                    else
-                    flag = CVodeSetStopTime(cvode_mem, (realtype)tstop);  /* Setting the stop time*/
+                    flag = ODESetStopTime(cvode_mem, (realtype)tstop);  /* Setting the stop time*/
                     if (check_flag(&flag, "CVodeSetStopTime", 1))
                     {
                         *ierr = 300 + (-flag);
@@ -1658,12 +1632,7 @@ L30:
                         return;
                     }
 
-                    if (!C2F(cmsolver).solver)
-                    {
-                        flag = LSodarReInit(cvode_mem, (realtype)(*told), y);
-                    }
-                    else
-                    flag = CVodeReInit(cvode_mem, (realtype)(*told), y);
+                    flag = ODEReInit(cvode_mem, (realtype)(*told), y);
                     if (check_flag(&flag, "CVodeReInit", 1))
                     {
                         *ierr = 300 + (-flag);
@@ -1709,12 +1678,7 @@ L30:
                 if (Discrete_Jump == 0) /* if there was a dzero, its event should be activated*/
                 {
                     phase = 2;
-                    if (!C2F(cmsolver).solver)
-                    {
-                        flag = LSodar(cvode_mem, t, y, told, LS_NORMAL);
-                    }
-                    else
-                    flag = CVode(cvode_mem, t, y, told, CV_NORMAL);
+                    flag = ODE(cvode_mem, t, y, told, CV_NORMAL);
                     if (*ierr != 0)
                     {
                         freeall;
@@ -1760,7 +1724,7 @@ L30:
                 }
                 else
                 {
-                    if (flag < 0) *ierr = 300 + (-flag); /* raising errors due to internal errors, other wise erros due to flagr*/
+                    if (flag < 0) *ierr = 300 + (-flag); /* raising errors due to internal errors, otherwise error due to flagr*/
                     freeall;
                     return;
                 }
@@ -1776,12 +1740,7 @@ L30:
                     hot = 0;
                     if (Discrete_Jump == 0)
                     {
-                        if (!C2F(cmsolver).solver)
-                        {
-                            flagr = LSodarGetRootInfo(cvode_mem, jroot);
-                        }
-                        else
-                        flagr = CVodeGetRootInfo(cvode_mem, jroot);
+                        flagr = ODEGetRootInfo(cvode_mem, jroot);
                         if (check_flag(&flagr, "CVodeGetRootInfo", 1))
                         {
                             *ierr = 300 + (-flagr);
@@ -2026,7 +1985,7 @@ static void cossimdaskr(double *told)
     }
 
     reltol = (realtype) rtol;
-    abstol = (realtype) Atol;  /*  Ith(abstol,1) = realtype) Atol;*/
+    abstol = (realtype) Atol;  /*  Ith(abstol,1) = (realtype) Atol;*/
 
     if (*neq > 0)
     {
@@ -2262,7 +2221,7 @@ static void cossimdaskr(double *told)
             }
         }
 
-        maxnj = 100; /* setting the maximum number of Jacobian evaluation during a Newton step */
+        maxnj = 100; /* setting the maximum number of Jacobian evaluations during a Newton step */
         flag = IDASetMaxNumJacsIC(ida_mem, maxnj);
         if (check_flag(&flag, "IDASetMaxNumJacsIC", 1))
         {
@@ -2271,7 +2230,7 @@ static void cossimdaskr(double *told)
             return;
         }
 
-        maxnit = 10; /* setting the maximum number of Newton iterations in any one attemp to solve CIC */
+        maxnit = 10; /* setting the maximum number of Newton iterations in any attempt to solve CIC */
         flag = IDASetMaxNumItersIC(ida_mem, maxnit);
         if (check_flag(&flag, "IDASetMaxNumItersIC", 1))
         {
@@ -2324,12 +2283,12 @@ static void cossimdaskr(double *told)
             ++jj;
         }
     }
-    /*     . Il faut:  ng >= jj */
+    /*     . ng >= jj required */
     if (jj != ng)
     {
         zcros[jj] = -1;
     }
-    /*     initialisation (propagation of constant blocks outputs) */
+    /*     initialization (propagation of constant blocks outputs) */
     idoit(told);
     if (*ierr != 0)
     {
@@ -2356,7 +2315,7 @@ static void cossimdaskr(double *told)
         while (ismenu()) //** if the user has done something, do the actions
         {
             int ierr2 = 0;
-            SeqSync = GetCommand(CommandToUnstack); //** get at the action
+            SeqSync = GetCommand(CommandToUnstack); //** get to the action
             CommandLength = (int)strlen(CommandToUnstack);
             syncexec(CommandToUnstack, &CommandLength, &ierr2, &one, CommandLength); //** execute it
         }
@@ -2429,7 +2388,7 @@ L20:
 L30:
                     if (rhotmp < tstop)
                     {
-                        hot = 0;/* Do cold-restart the solver:if the new TSTOP isn't beyong the previous one*/
+                        hot = 0;/* Cold-restart the solver if the new TSTOP isn't beyong the previous one*/
                     }
                 }
                 tstop = rhotmp;
@@ -2514,7 +2473,7 @@ L30:
                     //   freeallx;
                     //   return;
                     // };
-                    // flag=IDASetMaxNumItersIC(ida_mem, 10);/* (def=10) setting the maximum number of Newton iterations in any one attemp to solve CIC */
+                    // flag=IDASetMaxNumItersIC(ida_mem, 10);/* (def=10) setting the maximum number of Newton iterations in any attempt to solve CIC */
                     // if (check_flag(&flag, "IDASetMaxNumItersIC", 1)) {
                     //   *ierr=200+(-flag);
                     //   freeallx;
@@ -2533,7 +2492,7 @@ L30:
                         while (ismenu()) //** if the user has done something, do the actions
                         {
                             int ierr2 = 0;
-                            SeqSync = GetCommand(CommandToUnstack); //** get at the action
+                            SeqSync = GetCommand(CommandToUnstack); //** get to the action
                             CommandLength = (int)strlen(CommandToUnstack);
                             syncexec(CommandToUnstack, &CommandLength, &ierr2, &one, CommandLength); //** execute it
                         }
@@ -2631,7 +2590,7 @@ L30:
                     }/* mode-CIC  counter*/
                     if (Mode_change == 1)
                     {
-                        /* In tghis case, we try again by relaxing all modes and calling IDA_calc again
+                        /* In this case, we try again by relaxing all modes and calling IDA_calc again
                         /Masoud */
                         phase = 1;
                         copy_IDA_mem->ida_kk = 1;
@@ -2725,7 +2684,7 @@ L30:
                 }
                 else
                 {
-                    if (flagr < 0) *ierr = 200 + (-flagr); /* raising errors due to internal errors, other wise erros due to flagr*/
+                    if (flagr < 0) *ierr = 200 + (-flagr); /* raising errors due to internal errors, otherwise error due to flagr*/
                     freeallx;
                     return;
                 }
@@ -2861,7 +2820,7 @@ L30:
                 while (ismenu()) //** if the user has done something, do the actions
                 {
                     int ierr2 = 0;
-                    SeqSync = GetCommand(CommandToUnstack); //** get at the action
+                    SeqSync = GetCommand(CommandToUnstack); //** get to the action
                     CommandLength = (int)strlen(CommandToUnstack);
                     syncexec(CommandToUnstack, &CommandLength, &ierr2, &one, CommandLength); //** execute it
                 }
