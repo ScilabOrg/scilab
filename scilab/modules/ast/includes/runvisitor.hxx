fdac61cdeadc03a4cdd4ffa20415ce02814389d8
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-03-19 12:30:11 +0100

    management of not int added.
    a=uint32(12)
    b=~a
    ~b
    
    Change-Id: I411c3966c9b00c9f162e22cc45a0ed2f6bb4ef66

--- scilab/modules/ast/includes/runvisitor.hxx
+++ scilab/modules/ast/includes/runvisitor.hxx
@@ -1343,6 +1343,126 @@ public :
 
             result_set(pReturn);
         }
+        else if (result_get()->isInt())
+        {
+            InternalType* pVar      = result_get();
+            InternalType* pReturn   = NULL;
+
+            switch (pVar->getType())
+            {
+                case types::InternalType::RealUInt8 :
+                {
+                    types::UInt8* pIntIn  = pVar->getAs<types::UInt8>();
+                    types::UInt8* pIntOut = new types::UInt8(pIntIn->getCols(), pIntIn->getRows());
+                    unsigned char* pIn  = pIntIn->get();
+                    unsigned char* pOut = pIntOut->get();
+                    for (int i = 0; i < pIntIn->getRows() * pIntIn->getCols(); i++)
+                    {
+                        pOut[i] = ~pIn[i];
+                    }
+                    pReturn = pIntOut;
+                    break;
+                }
+                case types::InternalType::RealInt8 :
+                {
+                    types::Int8* pIntIn  = pVar->getAs<types::Int8>();
+                    types::Int8* pIntOut = new types::Int8(pIntIn->getCols(), pIntIn->getRows());
+                    char* pIn  = pIntIn->get();
+                    char* pOut = pIntOut->get();
+                    for (int i = 0; i < pIntIn->getRows() * pIntIn->getCols(); i++)
+                    {
+                        pOut[i] = ~pIn[i];
+                    }
+                    pReturn = pIntOut;
+                    break;
+                }
+                case types::InternalType::RealUInt16 :
+                {
+                    types::UInt16* pIntIn  = pVar->getAs<types::UInt16>();
+                    types::UInt16* pIntOut = new types::UInt16(pIntIn->getCols(), pIntIn->getRows());
+                    unsigned short* pIn  = pIntIn->get();
+                    unsigned short* pOut = pIntOut->get();
+                    for (int i = 0; i < pIntIn->getRows() * pIntIn->getCols(); i++)
+                    {
+                        pOut[i] = ~pIn[i];
+                    }
+                    pReturn = pIntOut;
+                    break;
+                }
+                case types::InternalType::RealInt16 :
+                {
+                    types::Int16* pIntIn  = pVar->getAs<types::Int16>();
+                    types::Int16* pIntOut = new types::Int16(pIntIn->getCols(), pIntIn->getRows());
+                    short* pIn  = pIntIn->get();
+                    short* pOut = pIntOut->get();
+                    for (int i = 0; i < pIntIn->getRows() * pIntIn->getCols(); i++)
+                    {
+                        pOut[i] = ~pIn[i];
+                    }
+                    pReturn = pIntOut;
+                    break;
+                }
+                case types::InternalType::RealUInt32 :
+                {
+                    types::UInt32* pIntIn  = pVar->getAs<types::UInt32>();
+                    types::UInt32* pIntOut = new types::UInt32(pIntIn->getCols(), pIntIn->getRows());
+                    unsigned int* pIn  = pIntIn->get();
+                    unsigned int* pOut = pIntOut->get();
+                    for (int i = 0; i < pIntIn->getRows() * pIntIn->getCols(); i++)
+                    {
+                        pOut[i] = ~pIn[i];
+                    }
+                    pReturn = pIntOut;
+                    break;
+                }
+                case types::InternalType::RealInt32 :
+                {
+                    types::Int32* pIntIn  = pVar->getAs<types::Int32>();
+                    types::Int32* pIntOut = new types::Int32(pIntIn->getCols(), pIntIn->getRows());
+                    int* pIn  = pIntIn->get();
+                    int* pOut = pIntOut->get();
+                    for (int i = 0; i < pIntIn->getRows() * pIntIn->getCols(); i++)
+                    {
+                        pOut[i] = ~pIn[i];
+                    }
+                    pReturn = pIntOut;
+                    break;
+                }
+                case types::InternalType::RealUInt64 :
+                {
+                    types::UInt64* pIntIn  = pVar->getAs<types::UInt64>();
+                    types::UInt64* pIntOut = new types::UInt64(pIntIn->getCols(), pIntIn->getRows());
+                    unsigned long long* pIn  = pIntIn->get();
+                    unsigned long long* pOut = pIntOut->get();
+                    for (int i = 0; i < pIntIn->getRows() * pIntIn->getCols(); i++)
+                    {
+                        pOut[i] = ~pIn[i];
+                    }
+                    pReturn = pIntOut;
+                    break;
+                }
+                case types::InternalType::RealInt64 :
+                {
+                    types::Int64* pIntIn  = pVar->getAs<types::Int64>();
+                    types::Int64* pIntOut = new types::Int64(pIntIn->getCols(), pIntIn->getRows());
+                    long long* pIn  = pIntIn->get();
+                    long long* pOut = pIntOut->get();
+                    for (int i = 0; i < pIntIn->getRows() * pIntIn->getCols(); i++)
+                    {
+                        pOut[i] = ~pIn[i];
+                    }
+                    pReturn = pIntOut;
+                    break;
+                }
+            }
+
+            if (result_get()->isDeletable())
+            {
+                delete result_get();
+            }
+
+            result_set(pReturn);
+        }
     }
 
 
