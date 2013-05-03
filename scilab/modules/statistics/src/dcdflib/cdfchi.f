afd7551f82d4d53d2bb30dc770954a2205d76e24
Paul BIGNIER <paul.bignier@scilab-enterprises.com>
2013-05-03 11:29:11 +0200

    * Bug #7296 fixed - Enabling %nan, %inf and -%inf for the cdf* functions
    Change-Id: Iecf0699cd87975c827f51639da446e3b42e2c00f

--- scilab/modules/statistics/src/dcdflib/cdfchi.f
+++ scilab/modules/statistics/src/dcdflib/cdfchi.f
@@ -106,6 +106,17 @@ C     .. Executable Statements ..
 C
 C     Check arguments
 C
+C     Testing x for NaN or +Inf. Not for -Inf because the lower limit is 0
+      IF (x.GT.inf) THEN
+         p = 1
+         q = 0
+         RETURN
+      ENDIF
+      IF (ISANAN(x).EQ.1) THEN
+         CALL RETURNANANFORTRAN(p)
+         CALL RETURNANANFORTRAN(q)
+         RETURN
+      ENDIF
       IF (.NOT. ((which.LT.1).OR. (which.GT.3))) GO TO 30
       IF (.NOT. (which.LT.1)) GO TO 10
       bound = 1.0D0
