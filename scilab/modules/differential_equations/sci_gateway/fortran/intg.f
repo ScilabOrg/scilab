00d1066dbe63f2aff8ec5f1e686ec055f0ce0057
Paul BIGNIER <paul.bignier@scilab-enterprises.com>
2013-05-14 11:16:20 +0200

    Differential_equations: Quadpack update
    'intg.f' calls Netlib's Quadpack.
    The upstream is now matched.
    
    Updated the doc.
    
    Change-Id: Ia92fe0b437717590b633af8691242e4aa77f62ac

--- scilab/modules/differential_equations/sci_gateway/fortran/intg.f
+++ scilab/modules/differential_equations/sci_gateway/fortran/intg.f
@@ -23,7 +23,7 @@ c      implicit undefined (a-z)
       logical getexternal, getscalar,type ,cremat
       integer topk,lr,katop,kydot,top2,lra,lrb,lc
       integer iipal,lpal,lw,liw,lpali,ifail
-      integer iadr,sadr
+      integer iadr,sadr, vfinite
       external setfintg
 c
       iadr(l)=l+l-1
@@ -43,7 +43,7 @@ c
       else
          epsr=1.0d-8
       endif
-      if (rhs.ge.4) then 
+      if (rhs.ge.4) then
          if (.not.getscalar(fname,topk,top,lr)) return
          epsa=stk(lr)
          top=top-1
@@ -57,10 +57,20 @@ c     cas standard
       top=top-1
       if (.not.getscalar(fname,topk,top,lrb)) return
       b=stk(lrb)
+      if (isanan(b).eq.1.or.vfinite(1,b).eq.0) then
+         err = 2
+         call error(264)
+         return
+      endif
       top=top-1
       katop=top
       if (.not.getscalar(fname,topk,top,lra)) return
       a=stk(lra)
+      if (isanan(a).eq.1.or.vfinite(1,a).eq.0) then
+         err = 1
+         call error(264)
+         return
+      endif
 c     tableaux de travail 
       top=top2+1
       lw=3000
@@ -79,17 +89,43 @@ c
       istk(iipal+2)=kydot
       istk(iipal+3)=katop
       lstk(top+1)=sadr(iipal+4)
-      if(type) then 
-         call dqag0(fintg,a,b,epsa,epsr,val,abserr,
-     +        stk(lpal),lw,stk(lpali),liw,ifail)
+      if(type) then
+         call dqags(fintg,a,b,epsa,epsr,val,abserr,
+     +        neval,ifail,lw/4,lw,last,stk(lpali),stk(lpal))
       else
-         call dqag0(bintg,a,b,epsa,epsr,val,abserr,
-     +        stk(lpal),lw,stk(lpali),liw,ifail)
+         call dqags(bintg,a,b,epsa,epsr,val,abserr,
+     +        neval,ifail,lw/4,lw,last,stk(lpali),stk(lpal))
       endif
-      if(err.gt.0.or.err1.gt.0)return
+      if(err.gt.0.or.err1.gt.0) return
       if(ifail.gt.0) then
-         call error(24)
-         return
+         select case (ifail)
+            case (1)
+               call erro('Error: Maximum number of subdivisons '//
+     &            'achieved. Splitting the interval might help.')
+               return
+            case (2)
+               call erro('Error: Round-off error detected, the '//
+     &         'requested tolerance (or default) cannot be achieved.'//
+     &         ' Try using bigger tolerances.')
+               return
+            case (3)
+               call erro('Error: Bad integrand behavior occurs at '//
+     &            'some points of the integration interval.')
+               return
+            case (4)
+               call erro('Error: Convergence problem, round-off '//
+     &            'error detected. Try using bigger tolerances.')
+               return
+            case (5)
+               call erro('Error: The integral is probably '//
+     &            'divergent, or slowly convergent.')
+               return
+c           case (6)
+            case default
+               call erro('Error: Invalid input, either '//
+     &       'absolute tolerance <= 0 or relative tolerance < 1.e-17.')
+               return
+         end select
       endif
       top=top2-rhs+1
       stk(lra)=val
