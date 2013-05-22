62a1da9294ae504491a7c0b92a4c84c2fde51456
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-05-22 09:11:33 +0200

    funptr, newfun and intppty set as obsolete.
    Change-Id: I93960679d808138ca81ee3f87e4ee098c2a8e3c2

--- scilab/modules/core/sci_gateway/c/sci_funptr.c
+++ scilab/modules/core/sci_gateway/c/sci_funptr.c
@@ -1,11 +1,11 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -16,38 +16,46 @@
 #include "Scierror.h"
 #include "Funtab.h"
 #include "stack-def.h"
+#include "sciprint.h"
+#include "warningmode.h"
 /*--------------------------------------------------------------------------*/
-int C2F(sci_funptr) (char *fname,unsigned long fname_len)
+int C2F(sci_funptr) (char *fname, unsigned long fname_len)
 {
-	CheckRhs(1,1);
-	CheckLhs(1,1);
+    if (getWarningMode())
+    {
+        sciprint(_("WARNING: Feature %s is obsolete.\n"), fname);
+        sciprint(_("WARNING: This feature will be permanently removed in Scilab %s.\n"), "6");
+    }
 
-	if ( GetType(1) == sci_strings )
-	{
-		char *VarName = NULL;
-		int m1 = 0, n1 = 0, l1 = 0;
-		int id[nsiz];
-		int funptr = 0;
-		int zero = 0;
-		int one = 1;
-		int job = SCI_HFUNCTIONS_FIND; /* Find function & returns fptr value */
+    CheckRhs(1, 1);
+    CheckLhs(1, 1);
 
-		GetRhsVar(1,STRING_DATATYPE,&m1,&n1,&l1);
-		VarName=cstk(l1);
+    if ( GetType(1) == sci_strings )
+    {
+        char *VarName = NULL;
+        int m1 = 0, n1 = 0, l1 = 0;
+        int id[nsiz];
+        int funptr = 0;
+        int zero = 0;
+        int one = 1;
+        int job = SCI_HFUNCTIONS_FIND; /* Find function & returns fptr value */
 
-		C2F(cvname)(id,VarName,&zero,(unsigned long)strlen(VarName));
-		C2F(funtab)(id,&funptr,&job,"NULL_NAME",0);
+        GetRhsVar(1, STRING_DATATYPE, &m1, &n1, &l1);
+        VarName = cstk(l1);
 
-		CreateVar(Rhs+1,MATRIX_OF_INTEGER_DATATYPE,&one,&one,&l1);
-		*istk(l1) = funptr;
+        C2F(cvname)(id, VarName, &zero, (unsigned long)strlen(VarName));
+        C2F(funtab)(id, &funptr, &job, "NULL_NAME", 0);
 
-		LhsVar(1)=Rhs+1;
-		PutLhsVar();
-	}
-	else
-	{
-		Scierror(999,_("%s: Wrong type for input argument #%d: String expected.\n"),fname,1);
-	}
-	return 0;
+        CreateVar(Rhs + 1, MATRIX_OF_INTEGER_DATATYPE, &one, &one, &l1);
+        *istk(l1) = funptr;
+
+        LhsVar(1) = Rhs + 1;
+        PutLhsVar();
+    }
+    else
+    {
+        Scierror(999, _("%s: Wrong type for input argument #%d: String expected.\n"), fname, 1);
+    }
+    return 0;
 }
 /*--------------------------------------------------------------------------*/
