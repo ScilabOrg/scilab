7ac1fe402bc90909c89260a5d9f5359e6f4f5705
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2013-06-05 13:13:43 +0200

    WIP : reforge symbol/context management
    Change-Id: I3df307fe2d30c3e65e8a3f73f5e85cd7d2926cb9

--- scilab/modules/core/sci_gateway/cpp/sci_isglobal.cpp
+++ scilab/modules/core/sci_gateway/cpp/sci_isglobal.cpp
@@ -15,6 +15,7 @@
 #include "callable.hxx"
 #include "context.hxx"
 #include "string.hxx"
+#include "bool.hxx"
 
 extern "C"
 {
@@ -29,33 +30,33 @@ Function::ReturnValue sci_isglobal(types::typed_list &in, int _iRetCount, types:
     types::typed_list::iterator inIterator;
     int iWrongType = 1;
 
-    if(in.size() != 1)
+    if (in.size() != 1)
     {
-        Scierror(77,_("%s: Wrong number of input argument(s): %d expected.\n"), "isglobal", 1);
+        Scierror(77, _("%s: Wrong number of input argument(s): %d expected.\n"), "isglobal", 1);
         return Function::Error;
     }
     else
     {
-        if(in[0]->isString() == false)
+        if (in[0]->isString() == false)
         {
-            Scierror(999,_("%s: Wrong type for input argument #%d: Single string expected.\n"), "isglobal", 1);
+            Scierror(999, _("%s: Wrong type for input argument #%d: Single string expected.\n"), "isglobal", 1);
             return Function::Error;
         }
 
         String* pS = in[0]->getAs<types::String>();
-        if(pS->isScalar() == false)
+        if (pS->isScalar() == false)
         {
-            Scierror(999,_("%s: Wrong type for input argument #%d: Single string expected.\n"), "isglobal", 1);
+            Scierror(999, _("%s: Wrong type for input argument #%d: Single string expected.\n"), "isglobal", 1);
             return Function::Error;
         }
 
-        if(symbol::Context::getInstance()->isGlobalExists(symbol::Symbol(pS->get(0))))
+        if (symbol::Context::getInstance()->isGlobalExists(symbol::Symbol(pS->get(0))))
         {
-            out.push_back(new Bool(1));
+            out.push_back(new types::Bool(1));
         }
         else
         {
-            out.push_back(new Bool(0));
+            out.push_back(new types::Bool(0));
         }
     }
     return Function::OK;
---
+++ scilab/modules/symbol/src/cpp/newScope.cpp
@@ -0,0 +1,99 @@
+/*
+*  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+*  Copyright (C) 2013 - Scilab Enterprises - Antoine ELIAS
+*
+*  This file must be used under the terms of the CeCILL.
+*  This source file is licensed as described in the file COPYING, which
+*  you should have received as part of this distribution.  The terms
+*  are also available at
+*  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
+
+#include "newScope.hxx"
+
+namespace symbol
+{
+Scope::~Scope()
+{
+    SymbolMap::iterator it = m_symbol.begin();
+    for (; it != m_symbol.end() ; it++)
+    {
+        m_vars->remove(it->first);
+    }
+}
+
+void Scope::put(const Symbol& _key, types::InternalType& _iT)
+{
+    m_symbol[_key] = 0;
+    m_vars->put(_key, _iT, m_iLevel);
+}
+
+void Scope::put(const Symbol& _key, types::InternalType& _iT, int _iLevel)
+{
+    m_symbol[_key] = 0;
+    m_vars->put(_key, _iT, _iLevel);
+}
+
+void Scopes::scope_begin()
+{
+    m_iLevel++;
+    m_scopes.push_back(new Scope(&m_vars, m_iLevel));
+    m_vars.IncreaseLevel();
+}
+
+void Scopes::scope_end()
+{
+    Scope* scope = m_scopes.back();
+    delete scope;
+    m_scopes.pop_back();
+    m_iLevel--;
+    m_vars.DecreaseLevel();
+}
+
+void Scopes::put(const Symbol& _key, types::InternalType& _iT, int _iLevel)
+{
+    //add in scope
+    m_scopes.back()->put(_key, _iT, _iLevel);
+}
+
+void Scopes::put(const Symbol& _key, types::InternalType& _iT)
+{
+    put(_key, _iT, m_iLevel);
+}
+
+void Scopes::putInPreviousScope(const Symbol& _key, types::InternalType& _iT)
+{
+    put(_key, _iT, m_iLevel - 1);
+}
+
+/* getters */
+types::InternalType* Scopes::get(const Symbol& key) const
+{
+    return m_vars.get(key);
+}
+
+types::InternalType* Scopes::getCurrentLevel(const Symbol& key) const
+{
+    return m_vars.getCurrentLevel(key);
+}
+
+types::InternalType* Scopes::getAllButCurrentLevel(const Symbol& key) const
+{
+    return m_vars.getAllButCurrentLevel(key);
+}
+
+/*remove*/
+bool Scopes::remove(const Symbol& _key)
+{
+    //m_scopes->(_key);
+    return true;
+}
+
+/*tools*/
+std::list<symbol::Symbol>& Scopes::getFunctionList(const std::wstring& _stModuleName, bool _bFromEnd)
+{
+    std::list<symbol::Symbol>* lst = new std::list<symbol::Symbol>;
+    return *lst;
+}
+}
