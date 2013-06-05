bf09c519c43979b91d5e27bf56fbe8cc91dd08c6
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2013-06-05 13:13:43 +0200

    WIP : reforge symbol/context management
    Change-Id: I3df307fe2d30c3e65e8a3f73f5e85cd7d2926cb9

---
+++ scilab/modules/symbol/includes/variables.hxx
@@ -0,0 +1,71 @@
+/*
+*  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+*  Copyright (C) 2013 - Scilab Enterprises - Antoine ELIAS
+*
+*  This file must be used under the terms of the CeCILL.
+*  This source file is licensed as described in the file COPYING, which
+*  you should have received as part of this distribution.  The terms
+*  are also available at
+*  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
+
+#ifndef __keyIABLES_HXX__
+#define __keyIABLES_HXX__
+
+#include <map>
+#include <list>
+#include "types.hxx"
+#include "symbol.hxx"
+
+namespace symbol
+{
+struct VarBox
+{
+    VarBox(int _iLevel, types::InternalType* _pIT, bool _bGlobal = false, bool _bGlobalVisible = false);
+
+    int m_iLevel;
+    types::InternalType* m_pIT;
+    bool m_bGlobal;
+    bool m_bGlobalVisible;
+};
+
+class Variables
+{
+private :
+    //current scope level
+    int m_iLevel;
+
+    typedef std::list<VarBox* > VarBoxList;
+    typedef std::map<Symbol, VarBoxList*> VarMap;
+    VarMap m_vars;
+
+public :
+    Variables() : m_iLevel(-1) {}
+
+    virtual ~Variables() {}
+
+    void put(const Symbol& _key, types::InternalType& _pIT);
+    void put(const Symbol& _key, types::InternalType& _iT, int _iLevel);
+    void putInPreviousScope(const Symbol& _key, types::InternalType& _iT);
+
+    types::InternalType* get(const Symbol& _key) const;
+    types::InternalType* getCurrentLevel(const Symbol& _key) const;
+    types::InternalType* getAllButCurrentLevel(const Symbol& _key) const;
+    types::InternalType* getInSpecificLevel(const Symbol& _key, int _iLevel) const;
+
+    std::list<symbol::Symbol>& getFunctionList(const std::wstring& _stModuleName, bool _bFromEnd) const;
+
+    bool remove(const Symbol& _key);
+
+    void IncreaseLevel()
+    {
+        m_iLevel++;
+    }
+    void DecreaseLevel()
+    {
+        m_iLevel--;
+    }
+};
+}
+#endif
