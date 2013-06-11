70df013ebe41803f1f00d9d2daeab776e11a7fef
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-06-11 17:13:40 +0200

    call_scilab module plugged.
    Change-Id: Ibb8e191cd31762344ad6ff25649cc1c9445a7ac8

--- scilab/modules/call_scilab/sci_gateway/c/sci_fromjava.c
+++ scilab/modules/call_scilab/sci_gateway/c/sci_fromjava.c
@@ -1,43 +1,47 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
- * 
+ * Copyright (C) 2013 - Scilab Enterprises - Cedric Delamarre
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
 #include "gw_core.h"
-#include "stack-c.h"
+#include "api_scilab.h"
 #include "MALLOC.h"
 #include "fromjava.h"
+#include "localization.h"
+#include "Scierror.h"
+
 /*--------------------------------------------------------------------------*/
-int sci_fromjava(char *fname,unsigned long fname_len)
+int sci_fromjava(char *fname, void* pvApiCtx)
 {
-	static int n1;
-	int *paramoutINT=(int*)MALLOC(sizeof(int)+1);
+    CheckInputArgument(pvApiCtx, 0, 0);
+    CheckOutputArgument(pvApiCtx, 1, 1);
 
-	Rhs = Max(0, Rhs);
-	CheckRhs(0,0);
-	CheckLhs(1,1);
+    if ( IsFromJava() )
+    {
+        if (createScalarBoolean(pvApiCtx, 1, TRUE))
+        {
+            Scierror(999, _("%s: Memory allocation error.\n"), fname);
+            return 1;
+        }
+    }
+    else
+    {
+        if (createScalarBoolean(pvApiCtx, 1, FALSE))
+        {
+            Scierror(999, _("%s: Memory allocation error.\n"), fname);
+            return 1;
+        }
+    }
 
-	if ( IsFromJava() )
-	{
-		*paramoutINT=(int)(TRUE);
-	}
-	else
-	{
-		*paramoutINT=(int)(FALSE);
-	}
-
-	n1=1;
-	CreateVarFromPtr(Rhs+1,MATRIX_OF_BOOLEAN_DATATYPE, &n1, &n1, &paramoutINT);
-    if (paramoutINT) {FREE(paramoutINT);paramoutINT=NULL;}
-	LhsVar(1)=Rhs+1;
-	PutLhsVar();
-
-	return 0;
+    AssignOutputVariable(pvApiCtx, 1) = 1;
+    ReturnArguments(pvApiCtx);
+    return 0;
 }
 /*--------------------------------------------------------------------------*/
