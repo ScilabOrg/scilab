fdc23925efe526a30e00f4e8019e6be91e6f9cdb
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-07-26 17:14:40 +0200

    splitURL plugged.
    test_run("fileio","splitURL",["no_check_error_output" ]);
    
    Change-Id: Ibbf580027bb69bb5f82ccc1ba79519f5d15230cf

--- scilab/modules/fileio/sci_gateway/cpp/fileio_gw.cpp
+++ scilab/modules/fileio/sci_gateway/cpp/fileio_gw.cpp
@@ -72,5 +72,6 @@ int FileioModule::Load()
     symbol::Context::getInstance()->addFunction(Function::createFunction(L"fprintfMat", &sci_fprintfMat, MODULE_NAME));
     symbol::Context::getInstance()->addFunction(Function::createFunction(L"fscanfMat", &sci_fscanfMat, MODULE_NAME));
     symbol::Context::getInstance()->addFunction(Function::createFunction(L"getURL", &sci_getURL, MODULE_NAME));
+    symbol::Context::getInstance()->addFunction(Function::createFunction(L"splitURL", &sci_splitURL, MODULE_NAME));
     return 1;
 }
