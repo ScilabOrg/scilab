383ea4c0a20e044dbd68c1b059d0cd2dafa477db
Vincent COUVERT <vincent.couvert@scilab-enterprises.com>
2013-09-16 12:48:13 +0200

    Remove dependency on GLIBC_2.15
    Change-Id: I9b1b45ad838248a1b60674068c2a4284354940c6

--- scilab/modules/gui/src/nogui/nogui.c
+++ scilab/modules/gui/src/nogui/nogui.c
@@ -15,6 +15,7 @@
 #ifndef _MSC_VER
 #include <errno.h>
 #include <stdio.h> /* fileno */
+#define __USE_FORTIFY_LEVEL 0
 #include <sys/select.h> /* fd_set */
 #include "core_math.h" /* Max */
 #include "sciprint.h"
