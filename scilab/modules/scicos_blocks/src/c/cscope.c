d07b41e03527c32eb3957a1b3998a454da2f0506
Alexandre HERISSE <alexandre.herisse@scilab-enterprises.com>
2013-09-24 16:35:43 +0200

    * Bug #11776 fixed - CMSCOPE did not take into account label&Id parameter.
    Change-Id: Id403d62edab285ae315db9e7b4c7ea6f6b96d701

--- scilab/modules/scicos_blocks/src/c/cscope.c
+++ scilab/modules/scicos_blocks/src/c/cscope.c
@@ -753,6 +753,10 @@ static char const* getFigure(scicos_block * block)
 
             setPolylinesBounds(block, i, 0);
         }
+    } else
+    {
+        // set configured parameters
+        setFigureSettings(pFigureUID, block);
     }
 
     if (sco->scope.cachedFigureUID == NULL)
