ScilabOrg/scilab

commit f3caed2765e81f5b1f241d5adcfcbd026fbcee52

Xcos blocks: fix some issues reported by Coverity #2
A common issue was the call of C2F(cvstr)(..., str, ..., strlen(str)) with
str as a fixed-size but non-initialized char[]. This is been fixed using
the sizeof C operator which return the right size.

Other minor issues hes been fixed as well.

Change-Id: Icc1faa1b6295f7951f31276e89fcf5fd69595cb3

scilab/modules/scicos_blocks/src/c/fromws_c.c
@@ -125,14 +125,8 @@ SCICOS_BLOCKS_IMPEXP void fromws_c(scicos_block *block, int flag)
     fromwork_struct *ptr = NULL;
 
     /* for path of TMPDIR/workspace */
-    char env[256];
-    char sep[2];
-#ifdef _MSC_VER
-    sep[0] = '\\';
-#else
-    sep[0] = '/';
-#endif
-    sep[1] = '\0';
+    char filePrefix[] = "TMPDIR/Workspace/";
+    char* env;
 
     /*retrieve dimensions of output port*/
     my       = GetOutPortRows(block, 1); /* number of rows of Outputs*/
@@ -143,15 +137,13 @@ SCICOS_BLOCKS_IMPEXP void fromws_c(scicos_block *block, int flag)
     if (flag == 4)
     {
         /* convert scilab code of the variable name to C string */
-        C2F(cvstr)(&(Fnlength), &(FName), str, (j = 1, &j), (unsigned long)strlen(str));
+        C2F(cvstr)(&(Fnlength), &(FName), str, (j = 1, &j), sizeof(str));
         str[Fnlength] = '\0';
 
         /* retrieve path of TMPDIR/workspace */
-        strcpy(env, getenv("TMPDIR"));
-        strcat(env, sep);
-        strcat(env, "Workspace");
-        strcat(env, sep);
-        strcat(env, str);
+        env = scicos_malloc(sizeof(filePrefix) + Fnlength);
+        memcpy(env, filePrefix, sizeof(filePrefix));
+        memcpy(env + (sizeof(filePrefix) - 1), str, Fnlength + 1);
 
         /* open tmp file */
         /* "r" : read */
@@ -159,12 +151,13 @@ SCICOS_BLOCKS_IMPEXP void fromws_c(scicos_block *block, int flag)
         status = "rb";
 
         filename = expandPathVariable(env);
+        scicos_free(env);
         if (filename)
         {
             C2F(mopen)(&fd, filename, status, &swap, &res, &ierr);
-            FREE(filename);
-            filename = NULL;
         }
+        FREE(filename);
+        filename = NULL;
 
         if (ierr != 0)
         {
@@ -1543,6 +1536,7 @@ static int Ishm(int *fd, int *Ytype, int *nPoints, int *my, int *ny, int *YsubTy
             (ptr_i[35] != 1))
     {
         Coserror(_("Invalid variable type : error in hypermat scilab coding.\n"));
+        scicos_free(ptr_i);
         return 0;
     }
 

scilab/modules/scicos_blocks/src/c/canimxy.c
@@ -371,7 +371,6 @@ static BOOL pushData(scicos_block * block, int row)
     coordinates = sco->internal.coordinates[row];
 
     return setGraphicObjectProperty(pPolylineUID, __GO_DATA_MODEL_COORDINATES__, coordinates, jni_double_vector, sco->internal.maxNumberOfPoints);
-    return result;
 }
 
 /*****************************************************************************

scilab/modules/scicos_blocks/src/c/evaluate_expr.c
@@ -110,6 +110,13 @@ SCICOS_BLOCKS_IMPEXP void evaluate_expr(scicos_block *block, int flag)
                     break;
                 case 5:
                     count = count + 1;
+                    /* invalid script : call a function without lhs */
+                    if (bottom <= 0)
+                    {
+                        set_block_error(-2);
+                        return;
+                    }
+
                     switch (block->ipar[count])
                     {
                         case 1:
@@ -624,7 +631,7 @@ SCICOS_BLOCKS_IMPEXP void evaluate_expr(scicos_block *block, int flag)
                             {
                                 if (stack[bottom] > 0)
                                 {
-                                    stack[bottom] = stack[bottom];
+                                    /* stack[bottom] = stack[bottom]; */
                                 }
                                 else
                                 {

scilab/modules/scicos_blocks/src/c/readau.c
@@ -133,7 +133,7 @@ ipar[10:9+lfil] = character codes for file name
             {
                 /*     read a new buffer */
                 m = ipar[6] * ipar[7];
-                F2C(cvstr)(&three, &(ipar[2]), type, &job, (unsigned long)strlen(type));
+                F2C(cvstr)(&three, &(ipar[2]), type, &job, sizeof(type));
                 for (i = 2; i >= 0; i--)
                     if (type[i] != ' ')
                     {
@@ -171,7 +171,7 @@ ipar[10:9+lfil] = character codes for file name
     }
     else if (*flag == 4)
     {
-        F2C(cvstr)(&(ipar[1]), &(ipar[10]), str, &job, (unsigned long)strlen(str));
+        F2C(cvstr)(&(ipar[1]), &(ipar[10]), str, &job, sizeof(str));
         str[ipar[1]] = '\0';
         wcfopen(fd, str, "rb");
         if (!fd )
@@ -184,7 +184,7 @@ ipar[10:9+lfil] = character codes for file name
         /* skip first records */
         if (ipar[9] > 1)
         {
-            F2C(cvstr)(&three, &(ipar[2]), type, &job, (unsigned long)strlen(type));
+            F2C(cvstr)(&three, &(ipar[2]), type, &job, sizeof(type));
             for (i = 2; i >= 0; i--)
                 if (type[i] != ' ')
                 {
@@ -204,7 +204,7 @@ ipar[10:9+lfil] = character codes for file name
         }
         /* read first buffer */
         m = ipar[6] * ipar[7];
-        F2C(cvstr)(&three, &(ipar[2]), type, &job, (unsigned long)strlen(type));
+        F2C(cvstr)(&three, &(ipar[2]), type, &job, sizeof(type));
         for (i = 2; i >= 0; i--)
             if (type[i] != ' ')
             {

scilab/modules/scicos_blocks/src/c/readc.c
@@ -94,7 +94,7 @@ ipar[10+lfil:9+lfil++ny+ievt] = reading mask
             {
                 /*     read a new buffer */
                 m = ipar[6] * ipar[7];
-                F2C(cvstr)(&three, &(ipar[2]), type, &job, (unsigned long)strlen(type));
+                F2C(cvstr)(&three, &(ipar[2]), type, &job, sizeof(type));
                 for (i = 2; i >= 0; i--)
                     if (type[i] != ' ')
                     {
@@ -158,7 +158,7 @@ ipar[10+lfil:9+lfil++ny+ievt] = reading mask
     }
     else if (*flag == 4)
     {
-        F2C(cvstr)(&(ipar[1]), &(ipar[10]), str, &job, (unsigned long)strlen(str));
+        F2C(cvstr)(&(ipar[1]), &(ipar[10]), str, &job, sizeof(str));
         str[ipar[1]] = '\0';
         wcfopen(fd, str, "rb");
         if (!fd )
@@ -171,7 +171,7 @@ ipar[10+lfil:9+lfil++ny+ievt] = reading mask
         /* skip first records */
         if (ipar[9] > 1)
         {
-            F2C(cvstr)(&three, &(ipar[2]), type, &job, (unsigned long)strlen(type));
+            F2C(cvstr)(&three, &(ipar[2]), type, &job, sizeof(type));
             for (i = 2; i >= 0; i--)
                 if (type[i] != ' ')
                 {
@@ -191,7 +191,7 @@ ipar[10+lfil:9+lfil++ny+ievt] = reading mask
         }
         /* read first buffer */
         m = ipar[6] * ipar[7];
-        F2C(cvstr)(&three, &(ipar[2]), type, &job, (unsigned long)strlen(type));
+        F2C(cvstr)(&three, &(ipar[2]), type, &job, sizeof(type));
         for (i = 2; i >= 0; i--)
             if (type[i] != ' ')
             {

scilab/modules/scicos_blocks/src/c/writec.c
@@ -81,7 +81,7 @@ ipar[7:6+lfil] = character codes for file name
         }
         else  /* buffer is full write it to the file */
         {
-            F2C(cvstr)(&three, &(ipar[2]), type, &job, (unsigned long)strlen(type));
+            F2C(cvstr)(&three, &(ipar[2]), type, &job, sizeof(type));
             for (i = 2; i >= 0; i--)
                 if (type[i] != ' ')
                 {
@@ -99,7 +99,7 @@ ipar[7:6+lfil] = character codes for file name
     }
     else if (*flag == 4)
     {
-        F2C(cvstr)(&(ipar[1]), &(ipar[7]), str, &job, (unsigned long)strlen(str));
+        F2C(cvstr)(&(ipar[1]), &(ipar[7]), str, &job, sizeof(str));
         str[ipar[1]] = '\0';
         wcfopen(fd, str, "wb");
         if (!fd )
@@ -120,7 +120,7 @@ ipar[7:6+lfil] = character codes for file name
         k    = (int) z[1];
         if (k >= 1) /* flush rest of buffer */
         {
-            F2C(cvstr)(&three, &(ipar[2]), type, &job, (unsigned long)strlen(type));
+            F2C(cvstr)(&three, &(ipar[2]), type, &job, sizeof(type));
             for (i = 2; i >= 0; i--)
                 if (type[i] != ' ')
                 {

commit 13601194be16b2a1ea2e36f3a0df3bbd3969eb13

Xcos blocks: fix Coverrity regression after f3caed27
Change-Id: I1c2da3f341a5fa6946312fa802db8f75ced949d8

scilab/modules/scicos_blocks/src/c/fromws_c.c
@@ -142,6 +142,11 @@ SCICOS_BLOCKS_IMPEXP void fromws_c(scicos_block *block, int flag)
 
         /* retrieve path of TMPDIR/workspace */
         env = scicos_malloc(sizeof(filePrefix) + Fnlength);
+        if (env == NULL)
+        {
+            set_block_error(-16);
+            return;
+        }
         memcpy(env, filePrefix, sizeof(filePrefix));
         memcpy(env + (sizeof(filePrefix) - 1), str, Fnlength + 1);
 