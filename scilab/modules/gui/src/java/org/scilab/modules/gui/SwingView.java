7ca80911aa3e03d98eb4fa6a208389049e1341b8
Bruno JOFRET <bruno.jofret@scilab-enterprises.com>
2013-10-16 19:57:36 +0200

    Increase Figure creation speed.
    Change-Id: I004f0f5be5e699993b41bea2e82508ab7e33a9b2

--- scilab/modules/gui/src/java/org/scilab/modules/gui/SwingView.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/SwingView.java
@@ -377,6 +377,8 @@ public final class SwingView implements GraphicView {
                 SwingScilabWindow window = new SwingScilabWindow();
 
                 window.setTitle(figureTitle);
+                Integer[] figureSize = figure.getSize();
+                window.setDims(new Size(figureSize[0], figureSize[1]));
                 /* TOOLBAR */
                 ToolBar toolBar = ToolBarBuilder.buildToolBar(SwingScilabTab.GRAPHICS_TOOLBAR_DESCRIPTOR, figureId);
                 /* INFOBAR */
@@ -402,7 +404,7 @@ public final class SwingView implements GraphicView {
                 DockingManager.dock(tab, window.getDockingPort());
                 ActiveDockableTracker.requestDockableActivation(tab);
 
-                window.setVisible(true);
+                //window.setVisible(true);
                 tab.setVisible(true);
                 tab.setName(figureTitle);
 
@@ -621,11 +623,21 @@ public final class SwingView implements GraphicView {
 
     @Override
     public void updateObject(final Integer id, final int property) {
+        final TypedObject registeredObject = allObjects.get(id);
+
+        if (property == __GO_VALID__ && ((Boolean) GraphicController.getController().getProperty(id, __GO_VALID__))) {
+            if (registeredObject.getValue() instanceof SwingScilabTab){
+                ((SwingScilabTab) registeredObject.getValue()).getParentWindow().setVisible(true);
+                ((SwingScilabTab) registeredObject.getValue()).setVisible(true);
+                Integer[] figureSize = (Integer[]) GraphicController.getController().getProperty(id, __GO_SIZE__);
+                ((SwingScilabTab) registeredObject.getValue()).getParentWindow().setDims(new Size(figureSize[0], figureSize[1]));                
+            }
+        }
+
         if (property == __GO_IMMEDIATE_DRAWING__) {
             return;
         }
 
-        final TypedObject registeredObject = allObjects.get(id);
         if (registeredObject == null && property != __GO_STYLE__) {
             return;
         }
