d238382778fb026c04086fa4235cd5941feb79a3
Calixte DENIZET <calixte.denizet@scilab-enterprises.com>
2013-10-21 12:34:15 +0200

    Bug 13003 fixed: EOJ: add a converter to handle string to enum
    Change-Id: I7a5a753f75e85753977a60fa97f2358f0e3b8804

--- scilab/modules/external_objects_java/src/java/org/scilab/modules/external_objects_java/FunctionArguments.java
+++ scilab/modules/external_objects_java/src/java/org/scilab/modules/external_objects_java/FunctionArguments.java
@@ -87,6 +87,19 @@ public final class FunctionArguments {
                 return from.isArray() && to.isAssignableFrom(ArrayList.class);
             }
         });
+
+        // Converter to convert a double[] to a ArrayList<Double> (or an other primitive type)
+        registerConverter(new Converter() {
+            @Override
+            public Object convert(Object original, Class<?> to) {
+                return Enum.valueOf((Class) to, (String) original);
+            }
+
+            @Override
+            public boolean canConvert(Class<?> from, Class<?> to) {
+                return String.class.isAssignableFrom(from) && to.isEnum();
+            }
+        });
     }
 
     /**
