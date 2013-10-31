353d1068ba04c3f933ca54f9166632a2cd89ea6d
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-10-31 17:40:22 +0100

    optim module rewritten in cpp.
    Change-Id: If1d2b006c1e96afaf0c4883166d580a32ab5916c

---
+++ scilab/modules/optimization/includes/optimizationfunctions.hxx
@@ -0,0 +1,112 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2013 - DIGITEO - Cedric DELAMARRE
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+#ifndef __OPTIMIZATIONS_HXX__
+#define __OPTIMIZATIONS_HXX__
+
+#include <map>
+#include "dynlib_optimization.h"
+#include "string.hxx"
+#include "callable.hxx"
+
+extern "C"
+{
+#include "Thread_Wrapper.h"
+}
+
+extern "C"
+{
+    // optim
+    void C2F(genros)(int* ind, int* n, double* x, double* f, double* g, int* ti, float* tr, double* td);
+    void C2F(topt2)(int* ind, int* n, double* x, double* f, double* g, int* ti, float* tr, double* td);
+    void C2F(icsemc)(int* ind, int* n, double* x, double* f, double* g, int* ti, float* tr, double* td);
+    void C2F(mcsec)(int* ind, int* n, double* x, double* f, double* g, int* ti, float* tr, double* td);
+
+    // fsolve
+    void C2F(fsol1)(int* n, double* x, double* v, int* iflag);
+    void C2F(fsolj1)(int* n, double* x, double* v, int* iflag);
+
+}
+
+class OPTIMIZATION_IMPEXP OptimizationFunctions
+{
+
+public :
+
+    OptimizationFunctions(std::wstring callerName);
+    ~OptimizationFunctions();
+
+    void setXRows(int);
+    void setXCols(int);
+    int getXRows();
+    int getXCols();
+
+    // optim
+    void setOptimCostfFunction(types::Callable*);
+    bool setOptimCostfFunction(types::String*);
+    void setCostfArgs(types::InternalType*);
+    void execCostf(int* ind, int* n, double* x, double* f, double* g, int* ti, float* tr, double* td);
+
+    // fsolve
+    void setFsolveFctFunction(types::Callable*);
+    bool setFsolveFctFunction(types::String*);
+    void setFsolveFctArgs(types::InternalType*);
+    void execFsolveFct(int* n, double* x, double* v, int* iflag);
+
+    void setFsolveJacFunction(types::Callable*);
+    bool setFsolveJacFunction(types::String*);
+    void setFsolveJacArgs(types::InternalType*);
+    void execFsolveJac(int* n, double* x, double* v, double* jac, int* ldjac, int* iflag);
+
+private :
+
+    std::map<std::wstring, void*> m_staticFunctionMap;
+    std::wstring m_wstrCaller;
+    int m_iXRows;
+    int m_iXCols;
+
+    // optim
+    types::Callable* m_pCallOptimCostfFunction;
+    types::String* m_pStringOptimCostfFunctionDyn;
+    types::String* m_pStringOptimCostfFunctionStatic;
+    std::vector<types::InternalType*> m_OptimArgs;
+    void callCostfMacro(int* ind, int* n, double* x, double* f, double* g, int* ti, float* tr, double* td);
+
+    // fsolve
+    types::Callable* m_pCallFsolveFctFunction;
+    types::String* m_pStringFsolveFctFunctionDyn;
+    types::String* m_pStringFsolveFctFunctionStatic;
+    std::vector<types::InternalType*> m_fsolveFctArgs;
+    void callFsolveFctMacro(int* n, double* x, double* v, int* iflag);
+
+    types::Callable* m_pCallFsolveJacFunction;
+    types::String* m_pStringFsolveJacFunctionDyn;
+    types::String* m_pStringFsolveJacFunctionStatic;
+    std::vector<types::InternalType*> m_fsolveJacArgs;
+    void callFsolveJacMacro(int* n, double* x, double* v, double* jac, int* ldjac, int* iflag);
+
+};
+
+class OPTIMIZATION_IMPEXP Optimization
+{
+    // differential equation functions
+private :
+    static std::map<__threadId, OptimizationFunctions*> m_mapOptimizationFunctions;
+
+public :
+    static void addOptimizationFunctions(OptimizationFunctions* _opFunction);
+    static void removeOptimizationFunctions();
+    static OptimizationFunctions* getOptimizationFunctions();
+
+};
+#endif /* !__OPTIMIZATIONS_HXX__ */
+
---
+++ scilab/modules/optimization/sci_gateway/cpp/sci_lsqrsolve.cpp
@@ -0,0 +1,32 @@
+/*
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2013 - Scilab Enterprises - Cedric DELAMARRE
+*
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at
+* http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
+/*--------------------------------------------------------------------------*/
+
+#include "optimization_gw.hxx"
+#include "function.hxx"
+#include "double.hxx"
+#include "string.hxx"
+
+extern "C"
+{
+#include "localization.h"
+#include "Scierror.h"
+}
+/*--------------------------------------------------------------------------*/
+
+types::Function::ReturnValue sci_sqrsolve(types::typed_list &in, int _iRetCount, types::typed_list &out)
+{
+    printf("sqrsolve\n");
+    out.push_back(types::Double::Empty());
+    return types::Function::OK;
+}
+
