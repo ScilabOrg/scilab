e9eeeb09a9c47211ec094e7eeb6bf51968bb1349
Bruno JOFRET <bruno.jofret@scilab-enterprises.com>
2014-02-06 14:24:58 +0100

    DrawerVisitor more selective on jobs
    Change-Id: I337b1361ef04f6df4d4ef353f443b397f98cd940

--- scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/DrawerVisitor.java
+++ scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/DrawerVisitor.java
@@ -906,6 +906,10 @@ public class DrawerVisitor implements Visitor, Drawer, GraphicView {
         /*
          * Check if property is CHILDREN and if there is a new child I should care about
          */
+        if (id != figure.getIdentifier() && openGLChildren.get(id) == null) {
+            return;
+        }
+        
         if (property == GraphicObjectProperties.__GO_CHILDREN__) {
             if (id != figure.getIdentifier()) {
                 /* Ignore children that are not mine */
@@ -1078,6 +1082,11 @@ public class DrawerVisitor implements Visitor, Drawer, GraphicView {
 
     @Override
     public void deleteObject(Integer id) {
+        
+        if (openGLChildren.get(id) == null) {
+            return; // Not of my managed openGL children
+        }
+        
         openGLChildren.remove(id);
 
         if (isImmediateDrawing(id)) {
