ScilabOrg/scilab

commit 65ea2594be544746ba8998f934df6b38d4a263fc

Bug 13271 fixed: plot2d with logscale and %nan value freezed scilab
Change-Id: Ie8f8fdd99db920727265355a4380dc458f2fd13e

scilab/modules/graphics/src/c/BasicAlgos.c
@@ -24,17 +24,24 @@
 /*------------------------------------------------------------------------*/
 double sciFindStPosMin(const double x[], int n)
 {
-    double min = 0.;
+    double min = -1.0;
     int i = 0;
 
     if (n <= 0)
     {
-        return -1.0;
+        return min;
     }
 
-    min = x[0];
+    for (i = 0; i < n; i++)
+    {
+        if (x[i] > 0)
+        {
+            min = x[i];
+            break;
+        }
+    }
 
-    for (i = 1 ; i < n ; i++)
+    for (; i < n ; i++)
     {
         if (x[i] > 0.0 && x[i] < min)
         {

scilab/modules/graphics/tests/nonreg_tests/bug_13271.dia.ref
@@ -1,0 +1,15 @@
+// =============================================================================
+// Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+// Copyright (C) 2014 - Scilab Enterprises - Calixte DENIZET
+//
+//  This file is distributed under the same license as the Scilab package.
+// =============================================================================
+// <-- TEST WITH GRAPHIC -->
+// <-- Non-regression test for bug 13213 -->
+//
+// <-- Bugzilla URL -->
+// http://bugzilla.scilab.org/13213
+//
+// <-- Short Description -->
+// plot2d with logscale and %nan value freezed scilab
+plot2d("nl", [1:2], [%nan;1]);

scilab/modules/graphics/tests/nonreg_tests/bug_13271.tst
@@ -1,0 +1,18 @@
+// =============================================================================
+// Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+// Copyright (C) 2014 - Scilab Enterprises - Calixte DENIZET
+//
+//  This file is distributed under the same license as the Scilab package.
+// =============================================================================
+
+// <-- TEST WITH GRAPHIC -->
+
+// <-- Non-regression test for bug 13213 -->
+//
+// <-- Bugzilla URL -->
+// http://bugzilla.scilab.org/13213
+//
+// <-- Short Description -->
+// plot2d with logscale and %nan value freezed scilab
+
+plot2d("nl", [1:2], [%nan;1]);

commit 81712646851fea171ec258204026adbbd60384a2

Graphics: fix test 12399 (bug introduced in commit 65ea2594be544746ba8998f934df6b38d4a263fc)
Change-Id: If9bf657f59b711ec3c38811a259de65babf2fefe

scilab/modules/graphics/src/c/BasicAlgos.c
@@ -21,11 +21,15 @@
 #include "MALLOC.h"
 #include "core_math.h"
 #include "freeArrayOfString.h"
+
+#define spINSIDE_SPARSE
+#include "../../sparse/includes/spConfig.h"
 /*------------------------------------------------------------------------*/
 double sciFindStPosMin(const double x[], int n)
 {
     double min = -1.0;
     int i = 0;
+    char hasNeg = 0;
 
     if (n <= 0)
     {
@@ -39,6 +43,16 @@ double sciFindStPosMin(const double x[], int n)
             min = x[i];
             break;
         }
+        else if (!hasNeg && x[i] <= 0)
+        {
+            hasNeg = 1;
+        }
+    }
+
+    if (i == n && !hasNeg)
+    {
+        // we have only NaN
+        return NAN;
     }
 
     for (; i < n ; i++)

scilab/modules/graphics/src/c/Plo2dn.c
@@ -583,7 +583,7 @@ void compute_data_bounds2(int cflag, char dataflag, char *logflags, double *x, d
     }
 
     /* back to default values for  x=[] and y = [] */
-    if (drect[2] == LARGEST_REAL || drect[3] == -LARGEST_REAL)
+    if (drect[2] == LARGEST_REAL || drect[3] == -LARGEST_REAL || isnan(drect[2]) || isnan(drect[3]))
     {
         if (logflags[1] != 'l')
         {
@@ -597,7 +597,7 @@ void compute_data_bounds2(int cflag, char dataflag, char *logflags, double *x, d
         drect[3] = 10.0;
     }
 
-    if (drect[0] == LARGEST_REAL || drect[1] == -LARGEST_REAL)
+    if (drect[0] == LARGEST_REAL || drect[1] == -LARGEST_REAL || isnan(drect[0]) || isnan(drect[1]))
     {
         if (logflags[0] != 'l')
         {