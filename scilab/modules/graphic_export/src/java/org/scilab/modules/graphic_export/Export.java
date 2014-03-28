ScilabOrg/scilab

commit 172e2aced9d156445da3d0a3af8350d9d64d099a

EMF export: --without-emf can be used to disable this feature.
Change-Id: I0c643fbdaa74ced7634e6d5976cfcfc9daeffc70

scilab/modules/graphic_export/src/java/org/scilab/modules/graphic_export/Export.java
@@ -30,6 +30,9 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 import java.text.AttributedCharacterIterator;
@@ -48,7 +51,6 @@ import org.apache.xmlgraphics.java2d.ps.EPSDocumentGraphics2D;
 import org.apache.xmlgraphics.java2d.ps.PSDocumentGraphics2D;
 import org.apache.xmlgraphics.ps.DSCConstants;
 import org.apache.xmlgraphics.ps.PSGenerator;
-import org.freehep.graphicsio.emf.EMFGraphics2D;
 import org.scilab.forge.scirenderer.Canvas;
 import org.scilab.forge.scirenderer.implementation.g2d.G2DCanvas;
 import org.scilab.forge.scirenderer.implementation.g2d.G2DCanvasFactory;
@@ -1061,10 +1063,38 @@ public class Export {
     private static class EMFExporter extends Exporter {
 
         private OutputStream out;
-        private EMFGraphics2D g2d;
+        private Class<Graphics2D> g2dClass;
+        private Constructor<Graphics2D> g2dCtor;
+        private Graphics2D g2d;
         private ByteArrayOutputStream buffer;
 
-        public EMFExporter() { }
+        public EMFExporter() {
+            final Class<Graphics2D> g2dClass;
+            try {
+                g2dClass = (Class<Graphics2D>) Class.forName("org.freehep.graphicsio.emf.EMFGraphics2D");
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException("This Scilab build does not provide EMF support");
+            }
+
+            final Constructor[] ctors = g2dClass.getDeclaredConstructors();
+            Constructor ctor = null;
+            for (int i = 0; i < ctors.length; i++) {
+                ctor = ctors[i];
+                final Type[] args = ctor.getGenericParameterTypes();
+                if (args.length != 2) {
+                    continue;
+                }
+                if (args[0] != OutputStream.class) {
+                    continue;
+                }
+                if (args[1] != Dimension.class) {
+                    continue;
+                }
+
+                g2dCtor = ctor;
+                break;
+            }
+        }
 
         @Override
         public Graphics2D getGraphics2D(int width, int height, File file, final ExportParams params) {
@@ -1077,16 +1107,23 @@ public class Export {
                     out = new BufferedOutputStream(new FileOutputStream(file));
                 }
                 if (params.orientation == ExportParams.LANDSCAPE) {
-                    g2d = new EMFGraphics2D(out, new Dimension(height, width));
-                    g2d.startExport();
+                    g2d = g2dCtor.newInstance(out, new Dimension(height, width));
+                    g2dClass.getMethod("startExport").invoke(g2d);
                     AffineTransform transf = AffineTransform.getRotateInstance(Math.PI / 2);
                     transf.preConcatenate(AffineTransform.getTranslateInstance(height, 0));
                     g2d.setTransform(transf);
                 } else {
-                    g2d = new EMFGraphics2D(out, new Dimension(width, height));
-                    g2d.startExport();
+                    g2d = g2dCtor.newInstance(out, new Dimension(width, height));
+                    g2dClass.getMethod("startExport").invoke(g2d);
                 }
-            } catch (IOException e) { }
+            } catch (IOException e) {
+            } catch (IllegalAccessException e) {
+            } catch (IllegalArgumentException e) {
+            } catch (InvocationTargetException e) {
+            } catch (NoSuchMethodException e) {
+            } catch (SecurityException e) {
+            } catch (InstantiationException e) {
+            }
 
             return g2d;
         }
@@ -1094,8 +1131,15 @@ public class Export {
         @Override
         public void write() throws IOException {
             if (g2d != null) {
-                g2d.endExport();
-                g2d.closeStream();
+                try {
+                    g2dClass.getMethod("endExport").invoke(g2d);
+                    g2dClass.getMethod("closeStream").invoke(g2d);
+                } catch (IllegalAccessException e) {
+                } catch (IllegalArgumentException e) {
+                } catch (InvocationTargetException e) {
+                } catch (NoSuchMethodException e) {
+                } catch (SecurityException e) {
+                }
             }
             if (buffer != null && file != null) {
                 FileOutputStream fos = new FileOutputStream(file);

scilab/configure.ac
@@ -985,30 +985,34 @@ interface for JOGL2 - or libGL (OpenGL library) are installed and if the version
                 FOP=$PACKAGE_JAR_FILE
                 AC_SUBST(FOP)
 
-		# Freehep Graphics2D
-                AC_JAVA_CHECK_PACKAGE([freehep-graphics2d],[org.freehep.graphics2d.VectorGraphics],[Freehep Graphics2D])
-                FREEHEP_GRAPHICS2D=$PACKAGE_JAR_FILE
-                AC_SUBST(FREEHEP_GRAPHICS2D)
+                AC_ARG_WITH(emf, [AS_HELP_STRING([--without-emf-export],
+                    [disable support for EMF (Windows Metafile) export])])
+                if test "x$with_emf" != xno; then
+                    # Freehep Graphics2D
+                    AC_JAVA_CHECK_PACKAGE([freehep-graphics2d],[org.freehep.graphics2d.VectorGraphics],[Freehep Graphics2D])
+                    FREEHEP_GRAPHICS2D=$PACKAGE_JAR_FILE
+                    AC_SUBST(FREEHEP_GRAPHICS2D)
 
-		# Freehep GraphicsIO EMF
-                AC_JAVA_CHECK_PACKAGE([freehep-graphicsio-emf],[org.freehep.graphicsio.emf.EMFGraphics2D],[Freehep GraphicsIO EMF])
-                FREEHEP_GRAPHICSIO_EMF=$PACKAGE_JAR_FILE
-                AC_SUBST(FREEHEP_GRAPHICSIO_EMF)
+                    # Freehep GraphicsIO EMF
+                    AC_JAVA_CHECK_PACKAGE([freehep-graphicsio-emf],[org.freehep.graphicsio.emf.EMFGraphics2D],[Freehep GraphicsIO EMF])
+                    FREEHEP_GRAPHICSIO_EMF=$PACKAGE_JAR_FILE
+                    AC_SUBST(FREEHEP_GRAPHICSIO_EMF)
 
-		# Freehep GraphicsIO
-                AC_JAVA_CHECK_PACKAGE([freehep-graphicsio],[org.freehep.graphicsio.VectorGraphicsIO],[Freehep GraphicsIO])
-                FREEHEP_GRAPHICSIO=$PACKAGE_JAR_FILE
-                AC_SUBST(FREEHEP_GRAPHICSIO)
+                    # Freehep GraphicsIO
+                    AC_JAVA_CHECK_PACKAGE([freehep-graphicsio],[org.freehep.graphicsio.VectorGraphicsIO],[Freehep GraphicsIO])
+                    FREEHEP_GRAPHICSIO=$PACKAGE_JAR_FILE
+                    AC_SUBST(FREEHEP_GRAPHICSIO)
 
-		# Freehep IO
-                AC_JAVA_CHECK_PACKAGE([freehep-io],[org.freehep.util.io.XMLSequence],[Freehep IO])
-                FREEHEP_IO=$PACKAGE_JAR_FILE
-                AC_SUBST(FREEHEP_IO)
+                    # Freehep IO
+                    AC_JAVA_CHECK_PACKAGE([freehep-io],[org.freehep.util.io.XMLSequence],[Freehep IO])
+                    FREEHEP_IO=$PACKAGE_JAR_FILE
+                    AC_SUBST(FREEHEP_IO)
 
-		# Freehep Util
-                AC_JAVA_CHECK_PACKAGE([freehep-util],[org.freehep.util.StringUtilities],[Freehep Util])
-                FREEHEP_UTIL=$PACKAGE_JAR_FILE
-                AC_SUBST(FREEHEP_UTIL)
+                    # Freehep Util
+                    AC_JAVA_CHECK_PACKAGE([freehep-util],[org.freehep.util.StringUtilities],[Freehep Util])
+                    FREEHEP_UTIL=$PACKAGE_JAR_FILE
+                    AC_SUBST(FREEHEP_UTIL)
+                fi
 
                 # xml.apache.org SVG Library (under mandriva for example)
                 AC_JAVA_CHECK_PACKAGE([batik-all],[org.apache.batik.parser.Parser],[Apache SVG Library],"yes")

scilab/modules/javasci/Makefile.in
@@ -976,10 +976,10 @@ maintainer-clean-generic:
 	@echo "This command is intended for maintainers to use"
 	@echo "it deletes files that may require special tools to rebuild."
 	-test -z "$(BUILT_SOURCES)" || rm -f $(BUILT_SOURCES)
-@JAVASCI_FALSE@install-html-local:
 @JAVASCI_FALSE@distclean-local:
-@JAVASCI_FALSE@install-data-local:
 @JAVASCI_FALSE@clean-local:
+@JAVASCI_FALSE@install-data-local:
+@JAVASCI_FALSE@install-html-local:
 clean: clean-am
 
 clean-am: clean-generic clean-libtool clean-local \

commit c56924a061137c1f827db32ffdc6349a72a21847

Export: emf export broken by commit 172e2aced9d156445da3d0a3af8350d9d64d099a
Change-Id: I0495561104614e571042deefdcff21d7757b578c

scilab/modules/graphic_export/src/java/org/scilab/modules/graphic_export/Export.java
@@ -1069,7 +1069,6 @@ public class Export {
         private ByteArrayOutputStream buffer;
 
         public EMFExporter() {
-            final Class<Graphics2D> g2dClass;
             try {
                 g2dClass = (Class<Graphics2D>) Class.forName("org.freehep.graphicsio.emf.EMFGraphics2D");
             } catch (ClassNotFoundException e) {