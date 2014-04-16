ScilabOrg/scilab

commit 9b514623e1d642e6c2d27da3b54e5090efa72253

Fix Coverity issues introduced by https://codereview.scilab.org/#/c/13430/ & https://codereview.scilab.org/#/c/13421/
** CID 1154247:  Resource leak  (RESOURCE_LEAK)
/modules/string/sci_gateway/c/sci_convstr.c: 117 in sci_convstr()

** CID 1154246:  Unchecked return value  (CHECKED_RETURN)
/modules/xml/src/c/XMLMlistsManagement.c: 92 in createXMLObjectAtPos()

Change-Id: I049b5e2d17f1a4b6ed04208a82e182f46e1ea18e

scilab/modules/string/sci_gateway/c/sci_convstr.c
@@ -113,6 +113,7 @@ int sci_convstr(char *fname, unsigned long fname_len)
     /* To input the string matrix */
     if (getAllocatedMatrixOfWideString(pvApiCtx, piAddr1, &iRows1, &iCols1, &pstInput))
     {
+        freeAllocatedMatrixOfWideString(iRows1, iCols1, pstInput);
         Scierror(999, _("%s: Wrong type for input argument #%d: Matrix of strings expected.\n"), fname, 1);
         return 0;
     }

scilab/modules/xml/src/c/XMLMlistsManagement.c
@@ -89,7 +89,6 @@ int createXMLObjectAtPos(int type, int pos, int id, void *pvApiCtx)
         printError(&err, 0);
         return 0;
     }
-    getMListType(mlistaddr, pvApiCtx);
     return 1;
 }
 

commit bd8f0469e9e406322294caf4ea390677ba08a95e

* Bug #13291 fixed - xmltojar([],[],'ja_JP') might SIGSEGV
To reproduce: "make doc-ja_JP" on a build using libasan
9b514623e1 introduced the issue, fix coverity the right way :

** CID 1154247:  Resource leak  (RESOURCE_LEAK)
    /modules/string/sci_gateway/c/sci_convstr.c: 117 in sci_convstr()

Change-Id: Ia6829628974e24f85eaa4f53af918e9ab3c82f1c

scilab/modules/api_scilab/src/cpp/api_string.cpp
@@ -662,7 +662,8 @@ int getAllocatedMatrixOfWideString(void* _pvCtx, int* _piAddress, int* _piRows, 
     }
 
     int* piLen = (int*)MALLOC(sizeof(int) **_piRows **_piCols);
-    *_pwstData = (wchar_t**)MALLOC(sizeof(wchar_t*) **_piRows **_piCols);
+    wchar_t** pwstData = *_pwstData;
+    pwstData = (wchar_t**)MALLOC(sizeof(wchar_t*) **_piRows **_piCols);
 
     sciErr = getMatrixOfWideString(_pvCtx, _piAddress, _piRows, _piCols, piLen, NULL);
     if (sciErr.iErr)
@@ -673,16 +674,21 @@ int getAllocatedMatrixOfWideString(void* _pvCtx, int* _piAddress, int* _piRows, 
             FREE(piLen);
             piLen = NULL;
         }
+        if (pwstData)
+        {
+            FREE(pwstData);
+            pwstData = NULL;
+        }
         printError(&sciErr, 0);
         return sciErr.iErr;
     }
 
     for (int i = 0 ; i < *_piRows **_piCols ; i++)
     {
-        (*_pwstData)[i] = (wchar_t*)MALLOC(sizeof(wchar_t) * (piLen[i] + 1));//+1 for null termination
+        pwstData[i] = (wchar_t*)MALLOC(sizeof(wchar_t) * (piLen[i] + 1));//+1 for null termination
     }
 
-    sciErr = getMatrixOfWideString(_pvCtx, _piAddress, _piRows, _piCols, piLen, *_pwstData);
+    sciErr = getMatrixOfWideString(_pvCtx, _piAddress, _piRows, _piCols, piLen, pwstData);
 
     if (piLen)
     {

scilab/modules/string/sci_gateway/c/sci_convstr.c
@@ -113,7 +113,6 @@ int sci_convstr(char *fname, unsigned long fname_len)
     /* To input the string matrix */
     if (getAllocatedMatrixOfWideString(pvApiCtx, piAddr1, &iRows1, &iCols1, &pstInput))
     {
-        freeAllocatedMatrixOfWideString(iRows1, iCols1, pstInput);
         Scierror(999, _("%s: Wrong type for input argument #%d: Matrix of strings expected.\n"), fname, 1);
         return 0;
     }