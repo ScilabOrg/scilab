7f2aada5041e17b0d820e27140aaaf784953f05d
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2014-04-17 12:44:18 +0200

    toolbox skeleton updated.
    Change-Id: I7a09833f10bdfc32fe7a684953bd37c17d5e21f4

--- scilab/contrib/toolbox_skeleton/sci_gateway/cpp/sci_cpp_find.cxx
+++ scilab/contrib/toolbox_skeleton/sci_gateway/cpp/sci_cpp_find.cxx
@@ -4,89 +4,89 @@
 /* ==================================================================== */
 #include <string>
 /* ==================================================================== */
-extern "C" 
+extern "C"
 {
-/* ==================================================================== */	
+/* ==================================================================== */
   #include "api_scilab.h"
   #include "Scierror.h"
   #include "MALLOC.h"
   #include <localization.h>
 
 /* ==================================================================== */
-  int sci_cpp_find(char *fname) 
+  int sci_cpp_find(char *fname, void* pvApiCtx)
   {
     SciErr sciErr;
-    
+
     int m1 = 0, n1 = 0;
     int *piAddressVarOne = NULL;
     char *pStVarOne = NULL;
     int lenStVarOne = 0;
     int iType1 = 0;
-    
+
     int m2 = 0, n2 = 0;
     int *piAddressVarTwo = NULL;
     char *pStVarTwo = NULL;
     int lenStVarTwo = 0;
     int iType2 = 0;
-    
+
     /* Check the number of input argument */
-    CheckInputArgument(pvApiCtx, 2,2); 
-    
+    CheckInputArgument(pvApiCtx, 2,2);
+
     /* Check the number of output argument */
     CheckOutputArgument(pvApiCtx, 1,1);
-    
+
     /* get Address of inputs */
     sciErr = getVarAddressFromPosition(pvApiCtx, 1, &piAddressVarOne);
     if(sciErr.iErr)
     {
       printError(&sciErr, 0);
       return 0;
-    }     
+    }
     sciErr = getVarAddressFromPosition(pvApiCtx, 2, &piAddressVarTwo);
     if(sciErr.iErr)
     {
       printError(&sciErr, 0);
       return 0;
-    } 
-    
+    }
+
     /* checks types */
     sciErr = getVarType(pvApiCtx, piAddressVarOne, &iType1);
     if(sciErr.iErr)
     {
       printError(&sciErr, 0);
       return 0;
-    } 
-    
+    }
+
     if ( iType1 != sci_strings )
     {
       Scierror(999,_("%s: Wrong type for input argument #%d: A string expected.\n"),fname,1);
       return 0;
     }
-  
+
     sciErr = getVarType(pvApiCtx, piAddressVarTwo, &iType2);
     if(sciErr.iErr)
     {
       printError(&sciErr, 0);
       return 0;
-    } 
-    
+    }
+
     if ( iType2 != sci_strings )
     {
       Scierror(999,_("%s: Wrong type for input argument #%d: A string expected.\n"),fname,2);
       return 0;
     }
-    
+
     /* get strings */
-    
+
     sciErr = getMatrixOfString(pvApiCtx, piAddressVarOne,&m1,&n1,&lenStVarOne,&pStVarOne);
     if(sciErr.iErr)
     {
       printError(&sciErr, 0);
       return 0;
-    } 
-    
+    }
+
     /* check size */
-    if ( (m1 != n1) && (n1 != 1) ) 
+    if ( (m1 != n1) && (n1 != 1) )
     {
       Scierror(999,_("%s: Wrong size for input argument #%d: A string expected.\n"),fname,1);
       return 0;
@@ -99,16 +99,16 @@ extern "C"
     {
       printError(&sciErr, 0);
       return 0;
-    } 
-    
+    }
+
     sciErr = getMatrixOfString(pvApiCtx, piAddressVarTwo,&m2,&n2,&lenStVarTwo,&pStVarTwo);
     if(sciErr.iErr)
     {
       printError(&sciErr, 0);
       return 0;
-    } 
-    
-    if ( (m2 != n2) && (n2 != 1) ) 
+    }
+
+    if ( (m2 != n2) && (n2 != 1) )
     {
       Scierror(999,_("%s: Wrong size for input argument #%d: A string expected.\n"),fname,2);
       return 0;
@@ -121,16 +121,16 @@ extern "C"
     {
       printError(&sciErr, 0);
       return 0;
-    } 
-    
-    
+    }
+
+
     std::string myMessage (pStVarOne);
     if (pStVarOne)
       {
-	FREE(pStVarOne); 
+	FREE(pStVarOne);
 	pStVarOne = NULL;
       }
-    	
+
     std::string search(pStVarTwo);
     if (pStVarTwo)
       {
@@ -140,26 +140,26 @@ extern "C"
 
     /* Where we will store the position */
     double dOut = 0.0;
-    if (myMessage.find(search) != std::string::npos) 
+    if (myMessage.find(search) != std::string::npos)
     {
       /* The actual operation */
-      dOut = myMessage.find(search); 
-    } 
-    else 
+      dOut = myMessage.find(search);
+    }
+    else
     {
       dOut = -1; /* Substring not found */
     }
-    
+
     /* create result on stack */
     int m_out = 1;
     int n_out = 1;
-    
+
     createMatrixOfDouble(pvApiCtx, nbInputArgument(pvApiCtx) + 1, m_out, n_out, &dOut);
-    
-    AssignOutputVariable(pvApiCtx, 1) = nbInputArgument(pvApiCtx) + 1; 
+
+    AssignOutputVariable(pvApiCtx, 1) = nbInputArgument(pvApiCtx) + 1;
 
     return 0;
 	}
-/* ==================================================================== */	
+/* ==================================================================== */
 } /* extern "C" */
 /* ==================================================================== */
