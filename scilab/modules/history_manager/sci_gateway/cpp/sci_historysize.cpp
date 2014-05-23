100bf4a89a2a056241dca61ca53681724d122eb5
Anais AUBERT <anais.aubert@scilab-enterprises.com>
2014-05-23 16:48:30 +0200

    fixed historysize with 1 parameter
    Change-Id: I8d74170e549eeb90bf44e6f691aefde22338c826

--- scilab/modules/history_manager/sci_gateway/cpp/sci_historysize.cpp
+++ scilab/modules/history_manager/sci_gateway/cpp/sci_historysize.cpp
@@ -1,6 +1,7 @@
 /*
 * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
 * Copyright (C) 2011 - DIGITEO - Cedric DELAMARRE
+* Copyright (C) 2014 - Scilab Enterprises - Anais AUBERT
 *
 * This file must be used under the terms of the CeCILL.
 * This source file is licensed as described in the file COPYING, which
@@ -15,6 +16,7 @@
 #include "function.hxx"
 #include "HistoryManager.hxx"
 #include "double.hxx"
+#include "string.hxx"
 
 extern "C"
 {
@@ -25,14 +27,69 @@ extern "C"
 
 types::Function::ReturnValue sci_historysize(types::typed_list &in, int _iRetCount, types::typed_list &out)
 {
+    types::String* pStr = NULL;
+    types::Double* pDo = NULL;
+    int ret  = 0;
+
     if (in.size() == 0)
     {
         int size = HistoryManager::getInstance()->getNumberOfLines() - 1;
         out.push_back(new types::Double((double)size));
     }
+    else if (in.size() == 1)
+    {
+        if (in[0]->isString() == true)
+        {
+            if(in[0]->getAs<types::String>()->isScalar() == true)
+            {
+                pStr = in[0]->getAs<types::String>();
+                if (wcscmp(pStr->get(0), L"max") == 0)
+                {
+                    ret = HistoryManager::getInstance()->getNumberOfLinesMax();
+                    out.push_back(new types::Double((double)ret));
+                    
+                }
+                else
+                {
+                    Scierror(999, _("%s: Wrong value for input argument #%d: \"%s\" expected.\n"), "historysize", 1, "max");
+                    return types::Function::Error;
+                }    
+            }
+            else
+            {
+                Scierror(999, _("%s: Wrong size for input argument #%d.\n"), "historysize", 1);
+                return types::Function::Error;
+            }
+            
+        }
+        else if (in[0]->isDouble() == true)
+        {
+            if(in[0]->getAs<types::Double>()->isScalar() == true)
+            {
+                pDo = in[0]->getAs<types::Double>();
+                
+                if (!HistoryManager::getInstance()->setNumberOfLinesMax(pDo->get(0)))
+                {
+                    Scierror(999, _("%s: Wrong value for input argument #%d.\n"), "historysize", 1);
+                    return types::Function::Error;
+                }
+                out.push_back(pDo);
+            }
+            else
+            {
+                Scierror(999, _("%s: Wrong size for input argument #%d.\n"), "historysize", 1);
+                return types::Function::Error;
+            }
+        }
+        else
+        {
+            Scierror(999, _("%s: Wrong type for input argument #%d.\n"), "historysize", 1);
+            return types::Function::Error;
+        }
+    }
     else
     {
-        Scierror(77, _("%s: Wrong number of input argument(s): %d expected.\n"), "historysize", 0);
+        Scierror(77, _("%s: Wrong number of input argument(s): %d to %d expected.\n"), "historymanager", 0, 1);
         return types::Function::Error;
     }
     return types::Function::OK;
