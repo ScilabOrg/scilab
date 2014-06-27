954598336d79a2161ed0dd79a1d30f775f7ff986
Anais AUBERT <anais.aubert@scilab-enterprises.com>
2014-06-27 12:00:20 +0200

    triu(hypermat) displays dimension error
    Change-Id: Ib2f79c1ac009da9d758121a622c2ee753f1fc7d2

--- scilab/modules/elementary_functions/sci_gateway/cpp/sci_triu.cpp
+++ scilab/modules/elementary_functions/sci_gateway/cpp/sci_triu.cpp
@@ -1,6 +1,7 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2012 - Scilab Enterprises - Cedric DELAMARRE
+ * Copyright (C) 2014 - Scilab Enterprises - Anais AUBERT
  *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -40,6 +41,11 @@ types::Function::ReturnValue sci_triu(types::typed_list &in, int _iRetCount, typ
         return types::Function::Error;
     }
 
+    if (in[0]->getAs<types::GenericType>()->getDims() > 2)
+    {
+        Scierror(999, _("%s: Wrong number of dimensions for input argument #%d: %d expected.\n"), "triu", 1, 2);
+        return types::Function::Error;
+    }
     if (in.size() == 2)
     {
         if (in[1]->isDouble() == false)
