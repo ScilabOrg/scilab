6aa187115f0fdd58d037844c380528c8a0d32e2d
Clément DAVID <clement.david@scilab-enterprises.com>
2014-06-30 17:28:53 +0200

    YaSp Scicos: link using the old API
    This is a first link, some of these gateways should be updated to
    API Scilab. All the used fortran API has been stubbed and will abort()
    Scilab.
    
    Change-Id: Ic98b61a8441f8c75ede75d0a424618e37e20bbbc

---
+++ scilab/modules/scicos/src/c/scilab5_needed_stubs.c
@@ -0,0 +1,121 @@
+#include <stdlib.h>
+#include "machine.h"
+
+void F2C(syncexec)()
+{
+    abort();
+}
+void F2C(GetCommand)()
+{
+    abort();
+}
+void F2C(ismenu)()
+{
+    abort();
+}
+void F2C(ierode)()
+{
+    abort();
+}
+void F2C(errgst)()
+{
+    abort();
+}
+void F2C(check_dims)()
+{
+    abort();
+}
+void F2C(check_scalar)()
+{
+    abort();
+}
+void F2C(createvarfromptr)()
+{
+    abort();
+}
+void F2C(namstr)()
+{
+    abort();
+}
+void F2C(iop)()
+{
+    abort();
+}
+void F2C(createlistcvarfromptr)()
+{
+    abort();
+}
+void F2C(GetData)()
+{
+    abort();
+}
+void F2C(createlistvarfromptr)()
+{
+    abort();
+}
+void F2C(xscion)()
+{
+    abort();
+}
+void F2C(getrhsvar)()
+{
+    abort();
+}
+void F2C(vartype)()
+{
+    abort();
+}
+void F2C(checklhs)()
+{
+    abort();
+}
+void F2C(callFunctionFromGateway)()
+{
+    abort();
+}
+void F2C(str2sci)()
+{
+    abort();
+}
+void F2C(cha1)()
+{
+    abort();
+}
+void F2C(stack)()
+{
+    abort();
+}
+void F2C(com)()
+{
+    abort();
+}
+void F2C(createdata)()
+{
+    abort();
+}
+void F2C(listentry)()
+{
+    abort();
+}
+void F2C(putlhsvar)()
+{
+    abort();
+}
+void F2C(intersci)()
+{
+    abort();
+}
+void F2C(checkrhs)()
+{
+    abort();
+}
+void F2C(createvar)()
+{
+    abort();
+}
+void F2C(vstk)()
+{
+    abort();
+}
+
+
