e6fcf9915b1fd13743e9972335294a5d0396e77e
Sylvain GENIN <sylvain.genin@scilab-enterprises.com>
2014-07-24 15:21:28 +0200

    fix [a;b],[a,b] for the boolean
    Change-Id: I1a81be8638f55b42293d0d339d6499b8f3f6bc37

--- scilab/modules/ast/src/cpp/ast/run_MatrixExp.cpp
+++ scilab/modules/ast/src/cpp/ast/run_MatrixExp.cpp
@@ -81,9 +81,10 @@ void RunVisitorT<T>::visitprivate(const MatrixExp &e)
 
                 GenericType* pGTResult = poRow->getAs<GenericType>();
 
-                if (pGT->isList() || pGTResult->isList() ||
+                if ((pGT->isList() || pGTResult->isList() ||
                         pGT->isBool() || pGTResult->isBool() ||
                         pGT->isInt()  || pGTResult->isInt())
+                        && (pGTResult->getType() != pGT->getType()))
                 {
                     poRow = callOverloadMatrixExp(L"c", pGTResult, pGT);
                     continue;
@@ -148,9 +149,9 @@ void RunVisitorT<T>::visitprivate(const MatrixExp &e)
             //check dimension
             GenericType* pGTResult = poResult->getAs<GenericType>();
 
-            if (pGT->isList() || pGTResult->isList() ||
+            if ((pGT->isList() || pGTResult->isList() ||
                     pGT->isBool() || pGTResult->isBool() ||
-                    pGT->isInt()  || pGTResult->isInt())
+                    pGT->isInt()  || pGTResult->isInt()) && (pGT->getType() != pGTResult->getType()))
             {
                 poResult = callOverloadMatrixExp(L"f", pGTResult, pGT);
                 continue;
