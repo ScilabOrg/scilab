fc46233125c0023b629ef91ca733c0a44b79cc26
Paul Bignier <paul.bignier@scilab-enterprises.com>
2014-08-21 12:53:44 +0200

    Xcos MVC: implement graphics / model
    Read these fields from MLists (and return them as MLists).
    
    Change-Id: I1327f01707efb8822d0bc5f685bb3ad38c7e1d19

--- scilab/modules/scicos/src/cpp/view_scilab/BaseAdapter.hxx
+++ scilab/modules/scicos/src/cpp/view_scilab/BaseAdapter.hxx
@@ -17,6 +17,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <iostream>
 
 #include "user.hxx"
 #include "internal.hxx"
--- scilab/modules/scicos/src/cpp/view_scilab/BlockAdapter.cpp
+++ scilab/modules/scicos/src/cpp/view_scilab/BlockAdapter.cpp
@@ -11,9 +11,11 @@
  */
 
 #include <string>
+#include <vector>
 
 #include "internal.hxx"
 #include "list.hxx"
+#include "mlist.hxx"
 #include "string.hxx"
 #include "types.hxx"
 #include "user.hxx"
@@ -36,19 +38,138 @@ namespace view_scilab
 namespace
 {
 
+static const wchar_t* graphicsStr = L"graphics";
+static const std::wstring orig (L"orig");
+static const std::wstring sz (L"sz");
+static const std::wstring flip (L"flip");
+static const std::wstring theta (L"theta");
+static const std::wstring exprs (L"exprs");
+static const std::wstring pin (L"pin");
+static const std::wstring pout (L"pout");
+static const std::wstring pein (L"pein");
+static const std::wstring peout (L"peout");
+static const std::wstring gr_i (L"gr_i");
+static const std::wstring id (L"id");
+static const std::wstring in_implicit (L"in_implicit");
+static const std::wstring out_implicit (L"out_implicit");
+static const std::wstring in_style (L"in_style");
+static const std::wstring out_style (L"out_style");
+static const std::wstring in_label (L"in_label");
+static const std::wstring out_label (L"out_label");
+static const std::wstring style (L"style");
+
+static const wchar_t* modelStr = L"model";
+static const std::wstring sim (L"sim");
+static const std::wstring in (L"in");
+static const std::wstring in2 (L"in2");
+static const std::wstring intyp (L"intyp");
+static const std::wstring out (L"out");
+static const std::wstring out2 (L"out2");
+static const std::wstring outtyp (L"outtyp");
+static const std::wstring evtin (L"evtin");
+static const std::wstring evtout (L"evtout");
+static const std::wstring state (L"state");
+static const std::wstring dstate (L"dstate");
+static const std::wstring odstate (L"odstate");
+static const std::wstring rpar (L"rpar");
+static const std::wstring ipar (L"ipar");
+static const std::wstring opar (L"opar");
+static const std::wstring blocktype (L"blocktype");
+static const std::wstring firing (L"firing");
+static const std::wstring dep_ut (L"dep_ut");
+static const std::wstring label (L"label");
+static const std::wstring nzcross (L"nzcross");
+static const std::wstring nmode (L"nmode");
+static const std::wstring equations (L"equations");
+static const std::wstring uid (L"uid");
+
 struct graphics
 {
     static types::InternalType* get(const BlockAdapter& adaptor, const Controller& controller)
     {
-        return new GraphicsAdapter(adaptor.getAdaptee());
+        GraphicsAdapter adaptee = GraphicsAdapter(adaptor.getAdaptee());
+
+        types::MList* o = new types::MList();
+        types::String* Graphics = new types::String(1, 1);
+        Graphics->set(0, graphicsStr);
+        o->set(0, Graphics);
+
+        types::InternalType* currentField;
+        std::vector<std::wstring> graphicsFields (18);
+        graphicsFields[0] = orig;
+        graphicsFields[1] = sz;
+        graphicsFields[2] = flip;
+        graphicsFields[3] = theta;
+        graphicsFields[4] = exprs;
+        graphicsFields[5] = pin;
+        graphicsFields[6] = pout;
+        graphicsFields[7] = pein;
+        graphicsFields[8] = peout;
+        graphicsFields[9] = gr_i;
+        graphicsFields[10] = id;
+        graphicsFields[11] = in_implicit;
+        graphicsFields[12] = out_implicit;
+        graphicsFields[13] = in_style;
+        graphicsFields[14] = out_style;
+        graphicsFields[15] = in_label;
+        graphicsFields[16] = out_label;
+        graphicsFields[17] = style;
+
+        for (int i = 0; i < 18; ++i)
+        {
+            //currentField = adaptee.getProperty(graphicsFields[i], controller);
+            o->set(i + 1, currentField);
+        }
+
+        return o;
     }
 
     static bool set(BlockAdapter& adaptor, types::InternalType* v, Controller& controller)
     {
-        if (v->getType() == types::InternalType::ScilabUserType
+        if (v->getType() == types::InternalType::ScilabMList
                 && v->getShortTypeStr() == GraphicsAdapter::getSharedTypeStr())
         {
-            GraphicsAdapter* graphics = v->getAs<GraphicsAdapter>();
+            GraphicsAdapter* graphics = new GraphicsAdapter(adaptor.getAdaptee());
+
+            types::MList* current = v->getAs<types::MList>();
+            if (current->getSize() != 19)
+            {
+                return false;
+            }
+
+            types::InternalType* currentField;
+            std::vector<std::wstring> graphicsFields (18);
+            graphicsFields[0] = orig;
+            graphicsFields[1] = sz;
+            graphicsFields[2] = flip;
+            graphicsFields[3] = theta;
+            graphicsFields[4] = exprs;
+            graphicsFields[5] = pin;
+            graphicsFields[6] = pout;
+            graphicsFields[7] = pein;
+            graphicsFields[8] = peout;
+            graphicsFields[9] = gr_i;
+            graphicsFields[10] = id;
+            graphicsFields[11] = in_implicit;
+            graphicsFields[12] = out_implicit;
+            graphicsFields[13] = in_style;
+            graphicsFields[14] = out_style;
+            graphicsFields[15] = in_label;
+            graphicsFields[16] = out_label;
+            graphicsFields[17] = style;
+
+            for (int i = 0; i < 18; ++i)
+            {
+                if ((currentField = current->getField(graphicsFields[i])) == NULL)
+                {
+                    return false;
+                }
+                if (!graphics->setProperty(graphicsFields[i], currentField, controller))
+                {
+                    return false;
+                }
+            }
+
             adaptor.setAdaptee(graphics->getAdaptee());
             return true;
         }
@@ -60,18 +181,102 @@ struct model
 {
     static types::InternalType* get(const BlockAdapter& adaptor, const Controller& controller)
     {
-        return new ModelAdapter(adaptor.getAdaptee());
+        GraphicsAdapter adaptee = GraphicsAdapter(adaptor.getAdaptee());
+
+        types::MList* o = new types::MList();
+        types::String* Graphics = new types::String(1, 1);
+        Graphics->set(0, modelStr);
+        o->set(0, Graphics);
+
+        types::InternalType* currentField;
+        std::vector<std::wstring> modelFields (23);
+        modelFields[0] = sim;
+        modelFields[1] = in;
+        modelFields[2] = in2;
+        modelFields[3] = intyp;
+        modelFields[4] = out;
+        modelFields[5] = out2;
+        modelFields[6] = outtyp;
+        modelFields[7] = evtin;
+        modelFields[8] = evtout;
+        modelFields[9] = state;
+        modelFields[10] = dstate;
+        modelFields[11] = odstate;
+        modelFields[12] = rpar;
+        modelFields[13] = ipar;
+        modelFields[14] = opar;
+        modelFields[15] = blocktype;
+        modelFields[16] = firing;
+        modelFields[17] = dep_ut;
+        modelFields[18] = label;
+        modelFields[19] = nzcross;
+        modelFields[20] = nmode;
+        modelFields[21] = equations;
+        modelFields[22] = uid;
+
+        for (int i = 0; i < 23; ++i)
+        {
+            //currentField = adaptee.getProperty(modelFields[i], controller);
+            o->set(i + 1, currentField);
+        }
+
+        return o;
     }
 
     static bool set(BlockAdapter& adaptor, types::InternalType* v, Controller& controller)
     {
-        if (v->getType() == types::InternalType::ScilabUserType
+        if (v->getType() == types::InternalType::ScilabMList
                 && v->getShortTypeStr() == ModelAdapter::getSharedTypeStr())
         {
-            ModelAdapter* model = v->getAs<ModelAdapter>();
+            ModelAdapter* model = new ModelAdapter(adaptor.getAdaptee());
+
+            types::MList* current = v->getAs<types::MList>();
+            if (current->getSize() != 24)
+            {
+                return false;
+            }
+
+            types::InternalType* currentElement;
+            std::vector<std::wstring> modelFields (23);
+            modelFields[0] = sim;
+            modelFields[1] = in;
+            modelFields[2] = in2;
+            modelFields[3] = intyp;
+            modelFields[4] = out;
+            modelFields[5] = out2;
+            modelFields[6] = outtyp;
+            modelFields[7] = evtin;
+            modelFields[8] = evtout;
+            modelFields[9] = state;
+            modelFields[10] = dstate;
+            modelFields[11] = odstate;
+            modelFields[12] = rpar;
+            modelFields[13] = ipar;
+            modelFields[14] = opar;
+            modelFields[15] = blocktype;
+            modelFields[16] = firing;
+            modelFields[17] = dep_ut;
+            modelFields[18] = label;
+            modelFields[19] = nzcross;
+            modelFields[20] = nmode;
+            modelFields[21] = equations;
+            modelFields[22] = uid;
+
+            for (int i = 0; i < 23; ++i)
+            {
+                if ((currentElement = current->getField(modelFields[i])) == NULL)
+                {
+                    return false;
+                }
+                if (!model->setProperty(modelFields[i], currentElement, controller))
+                {
+                    return false;
+                }
+            }
             adaptor.setAdaptee(model->getAdaptee());
             return true;
         }
+
         return false;
     }
 };
