02bf539b9ef11c9fe6475ccf96676d00952e0ca7
Vladislav TRUBKIN <uladzislau.trubkin@scilab-enterprises.com>
2014-10-06 16:17:09 +0200

    try to fix sci_load
    Change-Id: Ic0ca3e3dcd86167ab752e3a82b9c2e0b9f0881ab

--- scilab/modules/io/sci_gateway/cpp/sci_load.cpp
+++ scilab/modules/io/sci_gateway/cpp/sci_load.cpp
@@ -11,7 +11,10 @@
 */
 
 #include <string.h>
+#include "api_scilab.hxx"
 #include "function.hxx"
+#include "overload.hxx"
+#include "execvisitor.hxx"
 #include "io_gw.hxx"
 #include "string.hxx"
 #include "library.hxx"
@@ -21,6 +24,7 @@ extern "C"
 {
 #include "sci_malloc.h"
 #include "expandPathVariable.h"
+#include "h5_fileManagement.h"
 }
 
 using namespace types;
@@ -28,12 +32,6 @@ using namespace types;
 /*--------------------------------------------------------------------------*/
 Function::ReturnValue sci_load(types::typed_list &in, int _iRetCount, types::typed_list &out)
 {
-    int iXMLFileLen = 0;
-    if (in.size() != 1)
-    {
-        return Function::Error;
-    }
-
     InternalType* pIT = in[0];
 
     if (pIT->isString() == false)
@@ -48,15 +46,57 @@ Function::ReturnValue sci_load(types::typed_list &in, int _iRetCount, types::typ
         return Function::Error;
     }
 
-    wchar_t* pstPath = pS->get(0);
-    wchar_t* pwstPath = expandPathVariableW(pstPath);
-    Library* lib = loadlib(pwstPath);
-    FREE(pwstPath);
+    char* pstPath = wide_string_to_UTF8(pS->get(0));
 
-    if (lib == NULL)
+    // get file extension
+    std::string fileExt(pstPath);
+    int lastPos = fileExt.find_last_of("/\\") + 1;
+    fileExt = fileExt.substr(lastPos);
+    lastPos = fileExt.find_last_of(".") + 1;
+    fileExt = fileExt.substr(lastPos);
+
+    if (!fileExt.compare("lib") || !fileExt.compare("xml"))
     {
+        wchar_t* pwstPathLib = expandPathVariableW(to_wide_string(pstPath));
+        Library* lib = loadlib(pwstPathLib);
+        FREE(pwstPathLib);
+
+        if (lib == NULL)
+        {
+            return Function::Error;
+        }
+    }
+    else if (!fileExt.compare("sod") || !fileExt.compare("scg") || !fileExt.compare("dat") || !fileExt.compare("hdf5"))
+    {
+        //call overload
+        std::wstring wstFuncName = L"%_sodload";
+        return Overload::call(wstFuncName, in, _iRetCount, out, new ast::ExecVisitor());
+    }
+    else
+    {
+        Scierror(999, _("%s: Wrong file extension.\n"), "load");
+        FREE(pstPath);
         return Function::Error;
     }
+    FREE(pstPath);
+
+    /*if (isHDF5File(pstPath))
+    {
+        //call overload
+        std::wstring wstFuncName = L"%_sodload";
+        return Overload::call(wstFuncName, in, _iRetCount, out, new ast::ExecVisitor());
+    }
+    else
+    {
+        wchar_t* pwstPathLib = expandPathVariableW(to_wide_string(pstPath));
+        Library* lib = loadlib(pwstPathLib);
+        FREE(pwstPathLib);
+
+        if (lib == NULL)
+        {
+            return Function::Error;
+        }
+    }*/
 
     return Function::OK;
 }
