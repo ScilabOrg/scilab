fdadaf39436fe5cbb33624f4282dc25269ce4014
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2014-10-20 10:52:51 +0200

    fix mem leak in insert of empty indexes
    a = [1 2;3 4]
    a(a == a) = []
    
    a.b(2).name = "NAME"
    
    st.a = "toto";
    s.a = [];
    s.a(1) = st;
    
    clear st;
    st.a(2).b = 10;
    st.a(5).b = 10;
    
    Change-Id: Iff6d9ec7c640d0d078241b46cfd4e8bd76ef0b1f

--- scilab/modules/ast/src/cpp/ast/visitor_common.cpp
+++ scilab/modules/ast/src/cpp/ast/visitor_common.cpp
@@ -785,6 +785,7 @@ types::InternalType* evaluateFields(const ast::Exp* _pExp, std::list<ExpHistory*
                                         pFirstField->isCellExp(),
                                         pIT));
 
+    std::cout << "--------------- evaluate" << std::endl;
     //*** evaluate fields ***//
     while (iterFields != fields.end())
     {
@@ -855,7 +856,7 @@ types::InternalType* evaluateFields(const ast::Exp* _pExp, std::list<ExpHistory*
                     workFields.push_back(pEHChield);
                 }
 
-                delete pLOut;
+                pLOut->killMe();
             }
         }
         else if (pITCurrent->isTList() || pITCurrent->isMList())
@@ -1256,6 +1257,8 @@ types::InternalType* evaluateFields(const ast::Exp* _pExp, std::list<ExpHistory*
         }
     }
 
+    std::cout << "--------------- insert" << std::endl;
+
     //*** insert what we have to assign ***//
     while (workFields.empty() == false)
     {
@@ -1310,6 +1313,10 @@ types::InternalType* evaluateFields(const ast::Exp* _pExp, std::list<ExpHistory*
                 {
                     pStruct->get(pEH->getWhereReinsert())->set(pEH->getExpAsString(), _pAssignValue);
                 }
+
+                // insert _pAssignValue in parent, delete PEh and removes it from list<ExpHistory*>
+                delete pEH;
+                evalFields.pop_back();
             }
             else if (pParent->isTList() || pParent->isMList())
             {
@@ -1336,6 +1343,11 @@ types::InternalType* evaluateFields(const ast::Exp* _pExp, std::list<ExpHistory*
                 {
                     pTL->set(pEH->getExpAsString(), _pAssignValue);
                 }
+
+                // set _pAssignValue in parent, so kill the current if needed
+                // insert _pAssignValue in parent, delete PEh and removes it from list<ExpHistory*>
+                delete pEH;
+                evalFields.pop_back();
             }
             else
             {
@@ -1345,6 +1357,7 @@ types::InternalType* evaluateFields(const ast::Exp* _pExp, std::list<ExpHistory*
         }
     }
 
+    std::cout << "--------------- update" << std::endl;
     //*** update fields ***//
     while (evalFields.empty() == false)
     {
@@ -1429,9 +1442,16 @@ types::InternalType* evaluateFields(const ast::Exp* _pExp, std::list<ExpHistory*
             }
         }
 
+        if (pEH->getCurrent())
+        {
+            pEH->getCurrent()->killMe();
+        }
+
         evalFields.pop_back();
         delete pEH;
     }
+
+    std::cout << "--------------- end" << std::endl;
 
     if (!evalFields.empty())
     {
@@ -1455,6 +1475,7 @@ InternalType* insertionCall(const ast::Exp& e, typed_list* _pArgs, InternalType*
         //_pInsert = new Double(-1, -1, &pdbl);
         //pdbl[0] = 1;
         pIL = Double::Identity(-1, -1);
+        _pInsert->killMe();
         _pInsert = pIL;
     }
     else if (_pInsert->isImplicitList())
@@ -1462,6 +1483,7 @@ InternalType* insertionCall(const ast::Exp& e, typed_list* _pArgs, InternalType*
         pIL = _pInsert->getAs<ImplicitList>()->extractFullMatrix();
         if (pIL && pIL->isDeletable())
         {
+            _pInsert->killMe();
             _pInsert = pIL;
         }
     }
@@ -1676,7 +1698,7 @@ InternalType* insertionCall(const ast::Exp& e, typed_list* _pArgs, InternalType*
                     // overload
                     types::Double* pEmpty = types::Double::Empty();
                     pOut = callOverload(e, L"i", _pArgs, _pInsert, pEmpty);
-                    delete pEmpty;
+                    pEmpty->killMe();
                     break;
                 }
             }
@@ -1798,7 +1820,7 @@ InternalType* insertionCall(const ast::Exp& e, typed_list* _pArgs, InternalType*
             }
 
             pRet = pDest->insert(_pArgs, pP);
-            delete pP;
+            pP->killMe();
         }
         else if (_pVar->isPoly() && _pInsert->isPoly())
         {
@@ -1902,13 +1924,19 @@ InternalType* insertionCall(const ast::Exp& e, typed_list* _pArgs, InternalType*
                                     // set elements in the new position
                                     pStructInsert->get(i)->set(pwcsField, pLExtract->get(i));
                                 }
+
+                                pLExtract->killMe();
                             }
                         }
+
+                        pStrFieldsName->killMe();
                     }
 
                     // insert elements in following pArgs
                     pRet = pStruct->insert(_pArgs, pStructInsert);
                     pStructRet = pRet->getAs<Struct>();
+
+                    pStructInsert->killMe();
 
                     // insert fields of pStructInsert in pRet
                     for (int i = 0; i < pStrInsertFieldsName->getSize(); i++)
@@ -1918,6 +1946,8 @@ InternalType* insertionCall(const ast::Exp& e, typed_list* _pArgs, InternalType*
                             pStructRet->addField(pStrInsertFieldsName->get(i));
                         }
                     }
+
+                    pStrInsertFieldsName->killMe();
                 }
                 else
                 {
