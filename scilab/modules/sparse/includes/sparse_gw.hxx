c1f7040a7d36d51c975e45e58e9cec2e23262d46
Anais AUBERT <anais.aubert@scilab-enterprises.com>
2014-10-29 10:01:50 +0100

    luget
    a=rand(5,2)*rand(2,5);A=sparse(a);
    [hand,rk]=lufact(A);[P,L,U,Q]=luget(hand);
    full(L), P*L*U*Q-A
    
    Change-Id: Ifb223340fb736de6e6866f69d977dc6e48a0bf3a

--- scilab/modules/sparse/includes/sparse_gw.hxx
+++ scilab/modules/sparse/includes/sparse_gw.hxx
@@ -43,5 +43,7 @@ CPP_GATEWAY_PROTOTYPE(sci_spones);
 CPP_GATEWAY_PROTOTYPE(sci_full);
 CPP_GATEWAY_PROTOTYPE(sci_ordmmd);
 CPP_GATEWAY_PROTOTYPE(sci_lufact);
+CPP_GATEWAY_PROTOTYPE(sci_luget);
+
 
 #endif /* !__SPARSE_GW_HXX__ */
