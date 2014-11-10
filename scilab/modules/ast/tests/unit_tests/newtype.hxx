9b5a970796606d2054e47afd14f0b75e87580ec7
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2014-11-10 10:45:09 +0100

    UserType print.
    Change-Id: I4a0a3d3e79d54336133952cc1a1b289d1a533aac

---
+++ scilab/modules/ast/tests/unit_tests/newtype.hxx
@@ -0,0 +1,47 @@
+/*
+*  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+*  Copyright (C) 2014 - Scilab Enterprises - Cedric Delamarre
+*
+*  This file must be used under the terms of the CeCILL.
+*  This source file is licensed as described in the file COPYING, which
+*  you should have received as part of this distribution.  The terms
+*  are also available at
+*  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
+
+#include "user.hxx"
+
+class NewType : public types::UserType
+{
+public:
+    NewType() {}
+    ~NewType() {}
+
+    bool                    hasToString()
+    {
+        return true;
+    }
+
+    bool                    toString(std::wostringstream& ostr)
+    {
+        ostr << "This is my type.";
+        return true;
+    }
+
+    std::wstring            getTypeStr()
+    {
+        return L"NewType";
+    }
+
+    std::wstring            getShortTypeStr()
+    {
+        return L"nt";
+    }
+
+    types::InternalType*    clone()
+    {
+        return new NewType();
+    }
+
+};
