7f226561aae7e2da12c40283dca52e1f88ea0e3f
Sylvain GENIN <sylvain.genin@scilab-enterprises.com>
2014-12-05 15:16:51 +0100

    fix bug 9277 :  managed the slash when he got an odd monbre
    Change-Id: Ia3948b29caf422ea02a3c016107d1edef5f401db

--- scilab/modules/output_stream/src/cpp/scilab_sprintf.cpp
+++ scilab/modules/output_stream/src/cpp/scilab_sprintf.cpp
@@ -374,7 +374,7 @@ static wchar_t* replaceAndCountLines(const wchar_t* _pwstInput, int* _piLines, i
         {
             if (iInputLen == i + 1)
             {
-                pwstFirstOutput[iPos++] = L'\\';
+                //pwstFirstOutput[iPos++] = L'\\';
                 continue;
             }
 
@@ -402,9 +402,11 @@ static wchar_t* replaceAndCountLines(const wchar_t* _pwstInput, int* _piLines, i
                     pwstFirstOutput[iPos++] = L'\t';
                     i++;
                     break;
-                default:
+                case L'\\':
                     pwstFirstOutput[iPos++] = L'\\';
                     i++;
+                    break;
+                default:
                     break;
             }
         }
