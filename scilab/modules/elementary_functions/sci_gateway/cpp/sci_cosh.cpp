4bcc95db361ab8fe15a8f4daf758eccc6882cfd8
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2014-12-16 14:58:53 +0100

    improve elementary functions gateways and call libc/libc++ native functions when is possible
    Change-Id: I45314f0581bda58c4f31c04cb05bbeba458cbb46

--- scilab/modules/elementary_functions/sci_gateway/cpp/sci_cosh.cpp
+++ scilab/modules/elementary_functions/sci_gateway/cpp/sci_cosh.cpp
@@ -1,14 +1,14 @@
 /*
- * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) 2012 - DIGITEO - Cedric DELAMARRE
- *
- * This file must be used under the terms of the CeCILL.
- * This source file is licensed as described in the file COPYING, which
- * you should have received as part of this distribution.  The terms
- * are also available at
- * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
- *
- */
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2012 - DIGITEO - Cedric DELAMARRE
+*
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at
+* http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
 /*--------------------------------------------------------------------------*/
 #include "elem_func_gw.hxx"
 #include "function.hxx"
@@ -20,29 +20,29 @@ extern "C"
 {
 #include "Scierror.h"
 #include "localization.h"
-#include "sin.h"
-#include "dynlib_elementary_functions_gw.h"
+#include "elem_common.h"
+#include "cos.h"
 }
 
 /*
-clear a;nb = 2500;a = rand(nb, nb);tic();sin(a);toc
-clear a;nb = 2500;a = rand(nb, nb); a = a + a *%i;tic();sin(a);toc
+clear a;nb = 2500;a = rand(nb, nb);tic();cosh(a);toc
+clear a;nb = 2500;a = rand(nb, nb); a = a + a *%i;tic();cosh(a);toc
 */
 /*--------------------------------------------------------------------------*/
-types::Function::ReturnValue sci_sin(types::typed_list &in, int _iRetCount, types::typed_list &out)
+types::Function::ReturnValue sci_cosh(types::typed_list &in, int _iRetCount, types::typed_list &out)
 {
-    types::Double* pDblIn   = NULL;
-    types::Double* pDblOut  = NULL;
+    types::Double* pDblIn = NULL;
+    types::Double* pDblOut = NULL;
 
     if (in.size() != 1)
     {
-        Scierror(77, _("%s: Wrong number of input argument(s): %d expected.\n"), "sin", 1);
+        Scierror(77, _("%s: Wrong number of input argument(s): %d expected.\n"), "cosh", 1);
         return types::Function::Error;
     }
 
     if (_iRetCount > 1)
     {
-        Scierror(78, _("%s: Wrong number of output argument(s): %d expected.\n"), "sin", 1);
+        Scierror(78, _("%s: Wrong number of output argument(s): %d expected.\n"), "cosh", 1);
         return types::Function::Error;
     }
 
@@ -54,21 +54,25 @@ types::Function::ReturnValue sci_sin(types::typed_list &in, int _iRetCount, type
         double* pInR = pDblIn->get();
         double* pOutR = pDblOut->get();
         int size = pDblIn->getSize();
-
         if (pDblIn->isComplex())
         {
             double* pInI = pDblIn->getImg();
             double* pOutI = pDblOut->getImg();
+
             for (int i = 0; i < size; i++)
             {
-                zsins(pInR[i], pInI[i], &pOutR[i], &pOutI[i]);
+                //zcoss(-pInI[i], pInR[i], &pOutR[i], &pOutI[i]);
+                std::complex<double> c(pInR[i], pInI[i]);
+                std::complex<double> d = std::cosh(c);
+                pOutR[i] = d.real();
+                pOutI[i] = d.imag();
             }
         }
         else
         {
             for (int i = 0; i < size; i++)
             {
-                pOutR[i] = dsins(pInR[i]);
+                pOutR[i] = std::cosh(pInR[i]);
             }
         }
 
@@ -77,7 +81,7 @@ types::Function::ReturnValue sci_sin(types::typed_list &in, int _iRetCount, type
     else if (in[0]->isSparse())
     {
         types::Sparse* pSparseIn = in[0]->getAs<types::Sparse>();
-        types::Sparse* pSparseOut = new types::Sparse(pSparseIn->getRows(), pSparseIn->getCols(), pSparseIn->isComplex());
+        types::Double* pDblOut = new types::Double(pSparseIn->getRows(), pSparseIn->getCols(), pSparseIn->isComplex());
 
         int const nonZeros = static_cast<int>(pSparseIn->nonZeros());
         int* pRows = new int[nonZeros * 2];
@@ -88,36 +92,39 @@ types::Function::ReturnValue sci_sin(types::typed_list &in, int _iRetCount, type
         double* pNonZeroI = new double[nonZeros];
         pSparseIn->outputValues(pNonZeroR, pNonZeroI);
 
+        int iSize = pSparseIn->getSize();
+        int iOne = 1;
+        double dOne = 1;
+        int iZero = 0;
+
+        C2F(dcopy)(&iSize, &dOne, &iZero, pDblOut->get(), &iOne);
+
         if (pSparseIn->isComplex())
         {
-            for (int i = 0 ; i < nonZeros ; i++)
+            for (int i = 0; i < nonZeros; i++)
             {
-                std::complex<double> complex;
-                double dblReal = complex.real();
-                double dblImg = complex.imag();
-                zsins(pNonZeroR[i], pNonZeroI[i], &dblReal, &dblImg);
-                pSparseOut->set(pRows[i] - 1, pCols[i] - 1, complex, false);
+                int iPos = (pCols[i] - 1) * pSparseIn->getRows() + (pRows[i] - 1);
+                zcoss(-pNonZeroI[i], pNonZeroR[i], pDblOut->get() + iPos, pDblOut->getImg() + iPos);
             }
         }
         else
         {
-            for (int i = 0 ; i < nonZeros ; i++)
+            for (int i = 0; i < nonZeros; i++)
             {
-                pSparseOut->set(pRows[i] - 1, pCols[i] - 1, dsins(pNonZeroR[i]), false);
+                int iPos = (pCols[i] - 1) * pSparseIn->getRows() + (pRows[i] - 1);
+                pDblOut->set(iPos, std::cosh(pNonZeroR[i]));
             }
         }
-
-        pSparseOut->finalize();
 
         delete[] pRows;
         delete[] pNonZeroR;
         delete[] pNonZeroI;
 
-        out.push_back(pSparseOut);
+        out.push_back(pDblOut);
     }
     else
     {
-        std::wstring wstFuncName = L"%"  + in[0]->getShortTypeStr() + L"_sin";
+        std::wstring wstFuncName = L"%" + in[0]->getShortTypeStr() + L"_cosh";
         return Overload::call(wstFuncName, in, _iRetCount, out, new ast::ExecVisitor());
     }
 
