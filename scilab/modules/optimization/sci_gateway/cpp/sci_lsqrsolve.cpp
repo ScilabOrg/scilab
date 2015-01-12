cb5581d75c5e94af888a683a81368bd3a20549ee
Anais AUBERT <anais.aubert@scilab-enterprises.com>
2015-01-12 10:22:48 +0100

    fix lsqrsolve
    test_run("optimization","bug_3348",["no_check_error_output" ]);
    
    Change-Id: Ie5cb3a25a4966883c08c41fd255bf081f09db675

--- scilab/modules/optimization/sci_gateway/cpp/sci_lsqrsolve.cpp
+++ scilab/modules/optimization/sci_gateway/cpp/sci_lsqrsolve.cpp
@@ -281,6 +281,12 @@ types::Function::ReturnValue sci_lsqrsolve(types::typed_list &in, int _iRetCount
             iMaxfev = pDblStop->get(3);
             dEpsfcn = pDblStop->get(4);
             dFactor = pDblStop->get(5);
+
+            if (dFtol < 0 || dXtol < 0 || dGtol < 0 || iMaxfev <= 0 || dFactor <= 0)
+            {
+                Scierror(999, _("%s: Wrong value for input argument #%d.\n"), "lsqrsolve", iPos + 1, 1, 6);
+                return types::Function::Error;
+            }
         }
         else
         {
