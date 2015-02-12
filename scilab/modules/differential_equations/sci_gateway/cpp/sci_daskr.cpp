12c10977bb0844b0f3595974abe947a01ea7aa6d
Sylvain GENIN <sylvain.genin@scilab-enterprises.com>
2015-02-12 09:31:03 +0100

    correction equation in sci_daskr
    Change-Id: Ie4bd0feadfb44f3a646123866d6430588efbf8c2

--- scilab/modules/differential_equations/sci_gateway/cpp/sci_daskr.cpp
+++ scilab/modules/differential_equations/sci_gateway/cpp/sci_daskr.cpp
@@ -839,7 +839,7 @@ types::Function::ReturnValue sci_daskr(types::typed_list &in, int _iRetCount, ty
         // LENWP is the length ot the rwork segment containing
         // the matrix elements of the preconditioner P
 
-        LENWP = 7 * pDblX0->getRows();
+        LENWP = pDblX0->getRows() * pDblX0->getRows();
         rworksize += (maxord + 5) * pDblX0->getRows() + 3 * ng
                      + (maxl + 3 + min(1, maxl - kmp)) * pDblX0->getRows()
                      + (maxl + 3) * maxl + 1 + LENWP;
@@ -862,7 +862,7 @@ types::Function::ReturnValue sci_daskr(types::typed_list &in, int _iRetCount, ty
         // LENIWP is the length ot the iwork segment containing
         // the matrix indexes of the preconditioner P
         // (compressed sparse row format)
-        LENIWP = 25 * pDblX0->getRows() + 1;
+        LENIWP = 2 * pDblX0->getRows() * pDblX0->getRows();
         iworksize += LENIWP;
     }
 
