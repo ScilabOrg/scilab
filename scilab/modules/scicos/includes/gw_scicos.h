764a379c2320aa7b436d1120e07d36c491109955
Paul Bignier <paul.bignier@scilab-enterprises.com>
2015-02-23 11:02:40 +0100

    Xcos gateways: rewrite sci_pointer_xproperty to C++
    Change-Id: I5dacf2f5bd1ad0c26f8e395acc415effbbb8ab69

--- scilab/modules/scicos/includes/gw_scicos.h
+++ scilab/modules/scicos/includes/gw_scicos.h
@@ -41,7 +41,6 @@ SCICOS_IMPEXP int sci_ftree3(char *fname, void* pvApiCtx);
 SCICOS_IMPEXP int sci_ftree4(char *fname, void* pvApiCtx);
 
 SCICOS_IMPEXP int sci_diffobjs(char *fname, void* pvApiCtx);
-SCICOS_IMPEXP int sci_pointer_xproperty(char *fname, void* pvApiCtx);
 SCICOS_IMPEXP int sci_phase_simulation(char *fname, void* pvApiCtx);
 SCICOS_IMPEXP int sci_set_xproperty(char *fname, void* pvApiCtx);
 SCICOS_IMPEXP int sci_set_blockerror(char *fname, void* pvApiCtx);
