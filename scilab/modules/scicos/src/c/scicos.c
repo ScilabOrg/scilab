09a685e6182640a5c936bd5b7cf9874ab4f7eb52
Paul Bignier <paul.bignier@scilab-enterprises.com>
2015-03-06 10:53:21 +0100

    Xcos : rewrite sciblk2 to C++
    Change-Id: If3d8f877e36a5d07ae24b1f8b0f3f48ebed89c9d

--- scilab/modules/scicos/src/c/scicos.c
+++ scilab/modules/scicos/src/c/scicos.c
@@ -3883,7 +3883,8 @@ void callf(double *t, scicos_block *block, scicos_flag *flag)
                         block->evout, &block->nevout, block->rpar, &block->nrpar,
                         block->ipar, &block->nipar, (double **)block->inptr,
                         block->insz, &block->nin,
-                        (double **)block->outptr, block->outsz, &block->nout);
+                        (double **)block->outptr, block->outsz, &block->nout,
+                        block->scsptr);
             }
             /* with zero crossing */
             else
@@ -3895,6 +3896,7 @@ void callf(double *t, scicos_block *block, scicos_flag *flag)
                          block->ipar, &block->nipar, (double **)block->inptr,
                          block->insz, &block->nin,
                          (double **)block->outptr, block->outsz, &block->nout,
+                         block->scsptr,
                          block->g, &block->ng);
             }
 
