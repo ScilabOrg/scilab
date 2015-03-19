7af2d7d8facc95844ce94d0ec68010128712a2e0
Paul Bignier <paul.bignier@scilab-enterprises.com>
2015-03-19 10:26:07 +0100

    Xcos gateways: rewrite data2sig and sig2data to C++
    Change-Id: I405fe5e3e00eb928bb338c78daed9e0b69ef2994

--- scilab/modules/scicos/includes/gw_scicos.h
+++ scilab/modules/scicos/includes/gw_scicos.h
@@ -43,8 +43,6 @@ SCICOS_IMPEXP int sci_getscicosvars(char *fname, void* pvApiCtx);
 SCICOS_IMPEXP int sci_curblockc(char *fname, void* pvApiCtx);
 
 /* it was in a separate gateway */
-SCICOS_IMPEXP int sci_data2sig(char *fname, void* pvApiCtx);
-SCICOS_IMPEXP int sci_sig2data(char *fname, void* pvApiCtx);
 SCICOS_IMPEXP int sci_model2blk(char *fname, void* pvApiCtx);
 SCICOS_IMPEXP int sci_callblk(char *fname, void* pvApiCtx);
 
