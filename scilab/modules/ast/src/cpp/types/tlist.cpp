2ba5e58249330bcaef9d182836a9db938b0449f2
Sylvain GENIN <sylvain.genin@scilab-enterprises.com>
2015-05-27 08:44:09 +0200

    read data in the tlist whit polynom $
     A=[1 2;3 4];
     test=tlist(["test","chp1","chp2"], A, A);
     test($+1-1)== A
     test($+1-2)== A
     test($+1-3)== ["test","chp1","chp2"]
     test($+1-4)
     test($+2-1)
    
    Change-Id: I23a389d0885638cee622d87ce77a31705baa32a4

--- scilab/modules/ast/src/cpp/types/tlist.cpp
+++ scilab/modules/ast/src/cpp/types/tlist.cpp
@@ -129,13 +129,34 @@ bool TList::invoke(typed_list & in, optional_list & /*opt*/, int _iRetCount, typ
 
             delete pList;
         }
+        else if (arg->isPoly() && (wcscmp(arg->getAs<Polynom>()->getVariableName().c_str(), L"$") == 0))
+        {
+            Polynom* pPoly = arg->getAs<Polynom>();
+            SinglePoly* pSinglePoly = pPoly->get(0);
+
+            int iMaxDim = 0;
+
+
+            double dblParse = -1 * pSinglePoly->get(0);
+
+            if (pSinglePoly->getRank() < 2 && dblParse >= 0 && dblParse < getSize())
+            {
+
+                out.push_back(get(getSize() - 1 - dblParse));
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
 
         if (out.empty() == false)
         {
             return true;
         }
     }
-
     Callable::ReturnValue ret;
     // Overload of extraction need
     // the tlist from where we extract
