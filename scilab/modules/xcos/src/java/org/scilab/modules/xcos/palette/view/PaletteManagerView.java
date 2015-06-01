370bfe4458cb316664ed417eb9f3a669682aa3c6
Marcos CARDINOT <mcardinot@gmail.com>
2015-06-01 16:22:11 -0300

    * Bug #12453 fixed - left panel of the Palette Browser resizes when enabling/disabling any palette
    How to reproduce:
    - open the palette browser
    - go to 'Palettes' in the tree panel (left side)
    - disable any palette
    -> resizing
    
    Change-Id: I9341f639ee7940026983b2b8b3164603edbc049e

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/view/PaletteManagerView.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/view/PaletteManagerView.java
@@ -214,6 +214,8 @@ public class PaletteManagerView extends SwingScilabDockablePanel implements Simp
                     .getLastPathComponent());
             t.setSelectionPath(p);
         }
+        // bug #12453
+        t.expandPath(p);
     }
 
     /**
