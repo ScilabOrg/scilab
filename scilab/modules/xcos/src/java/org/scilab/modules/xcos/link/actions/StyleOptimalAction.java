3c9a1125136f5ffff67757a7a62fd780b6f33428
Chenfeng Zhu <zhuchenf@gmail.com>
2015-06-19 11:53:01 +0200

    dummy
    Change-Id: I1637dc4316e13a7a2b8b0a3422287e3098d98962

---
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/link/actions/StyleOptimalAction.java
@@ -0,0 +1,25 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2015 - Chenfeng ZHU
+ */
+package org.scilab.modules.xcos.link.actions;
+
+import java.awt.event.ActionEvent;
+
+import org.scilab.modules.graph.ScilabGraph;
+
+/**
+ * Implement the set link optimal action. (Empty)
+ */
+@SuppressWarnings(value = { "serial" })
+public class StyleOptimalAction extends StyleAction {
+
+    public StyleOptimalAction(ScilabGraph scilabGraph) {
+        super(scilabGraph);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+}
