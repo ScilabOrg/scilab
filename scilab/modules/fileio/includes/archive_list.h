858f7b069d14a9d3c0a407f01f79c040cfc8c9c7
Kartik Gupta <kartikgupta0909@gmail.com>
2015-06-20 19:45:53 +0530

    archiveList working with hand written wrappers
    Change-Id: Id0142821070e83f7dd6629fa71435d9b5cbd81ff

---
+++ scilab/modules/fileio/includes/archive_list.h
@@ -0,0 +1,30 @@
+/*
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2009 - DIGITEO - Allan CORNET
+*
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at
+* http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
+*
+*/
+/*--------------------------------------------------------------------------*/
+#ifndef __ARCHIVE_LIST_H__
+#define __ARCHIVE_LIST_H__
+
+#include "dynlib_fileio.h"
+
+/**
+* Creates an absolute or full path name for the specified relative path name.
+* @param[out] absPath Pointer to a buffer containing the absolute or full path name, or NULL.
+* @param[in] relPath Relative path name.
+* @param[in] maxLength Maximum length of the absolute path name buffer (absPath).
+* @return returns a pointer to a buffer containing the absolute path name (absPath).
+*/
+
+FILEIO_IMPEXP char** archive_list(char *filename,int *size,int *error);
+
+
+#endif
+/*--------------------------------------------------------------------------*/
---
+++ scilab/modules/fileio/src/c/archive_list.c
@@ -0,0 +1,54 @@
+#include<archive.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<archive_entry.h>
+#include<string.h>
+#include "MALLOC.h"
+
+char** archive_list(char *filename, int *size, int *error)
+{
+struct archive *a;
+struct archive_entry *entry;
+int r;
+a = archive_read_new();
+archive_read_support_filter_all(a);
+archive_read_support_format_all(a);
+r = archive_read_open_filename(a, filename, 10240);    //Opening the archive to read the headers
+char **file_list;
+
+
+if (r != ARCHIVE_OK)			//If the archive could not be opened
+{
+    *error = 1;
+    return file_list;
+}
+*size = 0;
+
+
+while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {   //Iterating over the archive header to get filenames
+  *size = *size + 1;
+  if((*size) == 1)
+  {
+	  file_list = (char**)MALLOC(sizeof(char*)*(*size));
+  }
+  else
+  {
+	  file_list = (char**)REALLOC(file_list,sizeof(char*)*(*size));
+  }
+  file_list[(*size)-1] = (char*)MALLOC(sizeof(char)*1024);
+  file_list[(*size)-1] = NULL;
+  file_list[(*size)-1] = strdup(archive_entry_pathname(entry));
+  archive_read_data_skip(a);
+}
+
+
+r = archive_read_free(a); 
+if (r != ARCHIVE_OK)			 //If the archive struct could not be freed properly
+{
+  *error = 2;
+  return file_list;
+}
+
+
+return file_list;
+}
