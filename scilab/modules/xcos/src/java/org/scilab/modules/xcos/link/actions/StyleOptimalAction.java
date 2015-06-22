fe7418c03d35b3b9a6aeca9a69c6c41b3f2ae299
Chenfeng Zhu <zhuchenf@gmail.com>
2015-06-22 04:11:32 +0200

    Optimal_Link_Style
    One-click to choose an "optimal" path for the link.
    Choose the links and click the button to find an optimal path for the links and change the links style.
    
    1. get the current orientation of port instead of the default orientation.
    2. get a route to avoid blocks and intersections of lines with less turning.
    
    Change-Id: I26ef9c34c5cc1796016a2f38fe9a94b1908153e4

---
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/link/actions/StyleOptimalAction.java
@@ -0,0 +1,92 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2015 - Chenfeng ZHU
+ */
+package org.scilab.modules.xcos.link.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import org.scilab.modules.graph.ScilabComponent;
+import org.scilab.modules.graph.ScilabGraph;
+import org.scilab.modules.gui.menuitem.MenuItem;
+import org.scilab.modules.xcos.graph.XcosDiagram;
+import org.scilab.modules.xcos.link.BasicLink;
+import org.scilab.modules.xcos.utils.XcosMessages;
+import org.scilab.modules.xcos.utils.XcosRoute;
+
+import com.mxgraph.util.mxConstants;
+
+/**
+ * Implement the set link optimal action.
+ */
+@SuppressWarnings(value = { "serial" })
+public class StyleOptimalAction extends StyleAction {
+    /** Name of the action */
+    public static final String NAME = XcosMessages.LINK_STYLE_OPTIMAL;
+    /** Icon name of the action */
+    public static final String SMALL_ICON = "";
+    /** Mnemonic key of the action */
+    public static final int MNEMONIC_KEY = KeyEvent.VK_O;
+
+    public StyleOptimalAction(ScilabGraph scilabGraph) {
+        super(scilabGraph);
+    }
+
+    /**
+     * @param scilabGraph
+     * @return menu item
+     */
+    public static MenuItem createMenu(ScilabGraph scilabGraph) {
+        return createMenu(scilabGraph, StyleOptimalAction.class);
+    }
+
+    /**
+     * @param e
+     * @see org.scilab.modules.xcos.link.actions.StyleAction#actionPerformed(java.awt.event.ActionEvent)
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        final XcosDiagram graph = (XcosDiagram) getGraph(e);
+
+        // action disabled when the cell is edited
+        final ScilabComponent comp = ((ScilabComponent) graph.getAsComponent());
+        if (comp.isEditing()) {
+            return;
+        }
+
+        final Object[] links = getLinks();
+
+        graph.getModel().beginUpdate();
+        try {
+            // graph.setCellStyle(null, links);
+            double scale = graph.getView().getScale();
+            graph.getView().setScale(1.0);
+            graph.setCellStyles(mxConstants.STYLE_NOEDGESTYLE, "1", links);
+            reset(graph, links);
+            this.updateLinkOptimal(graph, links);
+            graph.getView().setScale(scale);
+            // reset(graph, links);
+        } finally {
+            graph.getModel().endUpdate();
+        }
+    }
+
+    /**
+     * Update the style of links one by one.
+     *
+     * @param graph
+     * @param links
+     */
+    public void updateLinkOptimal(XcosDiagram graph, Object[] links) {
+        Object[] allChildCells = graph.getChildCells(graph.getDefaultParent());
+        XcosRoute route = new XcosRoute();
+        for (Object o : links) {
+            if (o instanceof BasicLink) {
+                BasicLink link = (BasicLink) o;
+                route.updateRoute(link, allChildCells, graph);
+            }
+        }
+    }
+
+}
