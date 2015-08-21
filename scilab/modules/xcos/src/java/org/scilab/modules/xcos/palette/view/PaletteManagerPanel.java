26f4e2edd164ff14ff11432b9cb80afc1320fcc9
Marcos CARDINOT <mcardinot@gmail.com>
2015-08-21 17:45:01 +0100

    Palette browser: allowing multiple selection by holding down the ctrl key
    This patch introduces a new feature to the palette browser.
    Now it is able to select multiple blocks by holding down the ctrl key.
    
    test cases:
    A)
    - open the palette browser
    - hold down the ctrl key
    - left click on some blocks
    - drag the blocks to a diagram
    
    B)
    - open the palette browser
    - hold down the ctrl key
    - left click on some blocks
    - double click any block to load all of them on the most recent diagram
    
    C)
    - open the palette browser
    - hold down the ctrl key
    - use the arrow keys to move the selection (hightlight)
    - press SPACE key to select a block
    - press ENTER to load the selected blocks on the most recent diagram
    
    Change-Id: Iba03c2ad71cf7e894bea2804834a56ab2135881f

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/view/PaletteManagerPanel.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/view/PaletteManagerPanel.java
@@ -132,7 +132,7 @@ public class PaletteManagerPanel extends JSplitPane {
      */
     public void addRecentltyUsedBlock(PaletteBlock block) {
         PaletteNode currentNode = (PaletteNode) tree.getLastSelectedPathComponent();
-        if (currentNode.getName().equals(RECENTLY_USED_BLOCKS)) {
+        if (currentNode != null && currentNode.getName().equals(RECENTLY_USED_BLOCKS)) {
             return;
         }
 
@@ -173,6 +173,7 @@ public class PaletteManagerPanel extends JSplitPane {
      * Zoom
      * @param newSize new paletteblocksize enum
      */
+    // CSOFF: CyclomaticComplexity
     private void zoom(PaletteBlockSize newSize) {
         if (newSize == null || newSize == currentSize) {
             return;
