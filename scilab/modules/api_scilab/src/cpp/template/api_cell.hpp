e1daddd41dd27bd113d2337edb4270b04dca8c28
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2015-11-03 23:55:30 +0100

    WIP: new C API for Scilab gateways (once again ...)
    2 modes:
      - "safe" version, good for development phase
      - "unsafe" version, remove checks in API for better performance and nice
        crashes
    
    Standard calls use "safe" version, to use "unsafe" version
    define __API_SCILAB_UNSAFE__ before include of "api_scilab.h"
    
    Done:
      - common functions ( unsafe & safe )
      - double ( unsafe & safe )
      - string ( unsafe & safe )
      - int ( unsafe & safe )
      - struct ( unsafe & safe )
      - cell ( unsafe & safe )
      - polynomial ( unsafe & safe )
      - boolean ( unsafe & safe )
      - (_/t/m)list ( unsafe & safe )
      - graphic handle ( unsafe & safe )
      - update dynamic_link gateway builder to link the new interface
      - add somes tests in api_scilab
    
    Not done yet:
      - sparse ( safe and unsafe )
      - deal with usertype
      - lot of tests in both mode
    
    Change-Id: I15b9912842326b41353bd002b8876cf7246a4884

---
+++ scilab/modules/api_scilab/src/cpp/template/api_cell.hpp
@@ -0,0 +1,107 @@
+/*
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2015 - Scilab Enterprises - Antoine ELIAS
+*
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at
+* http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
+*/
+
+#include "gatewaystruct.hxx"
+#include "cell.hxx"
+
+extern "C"
+{
+#include "api_scilab.h"
+}
+
+scilabVar API_PROTO(createCell)(scilabEnv env, int dim, const int* dims)
+{
+#ifdef __API_SCILAB_SAFE__
+    if (dims == nullptr)
+    {
+        scilab_setInternalError(env, L"createCell", _W("dims array cannot be NULL"));
+        return nullptr;
+    }
+
+    for (int i = 0; i < dim; ++i)
+    {
+        if (dims[i] < 0)
+        {
+            scilab_setInternalError(env, L"createCell", _W("dimensions cannot be negative"));
+            return nullptr;
+        }
+    }
+#endif
+    types::Cell* c = new types::Cell(dim, dims);
+#ifdef __API_SCILAB_SAFE__
+    if (c == nullptr)
+    {
+        scilab_setInternalError(env, L"createCell", _W("memory allocation error"));
+        return nullptr;
+    }
+#endif
+    return (scilabVar)c;
+}
+
+scilabVar API_PROTO(createCell2d)(scilabEnv env, int row, int col)
+{
+    int dims[2] = {row, col};
+#ifdef __API_SCILAB_SAFE__
+    for (int i = 0; i < 2; ++i)
+    {
+        if (dims[i] < 0)
+        {
+            scilab_setInternalError(env, L"createCell2d", _W("dimensions cannot be negative"));
+            return nullptr;
+        }
+    }
+#endif
+    types::Cell* c = new types::Cell(2, dims);
+#ifdef __API_SCILAB_SAFE__
+    if (c == nullptr)
+    {
+        scilab_setInternalError(env, L"createCell2d", _W("memory allocation error"));
+        return nullptr;
+    }
+#endif
+    return (scilabVar)c;
+}
+
+api_return API_PROTO(getCellArray)(scilabEnv env, scilabVar var, const scilabVar** vals)
+{
+    types::Cell* c = (types::Cell*)var;
+#ifdef __API_SCILAB_SAFE__
+    if (c->isCell() == false)
+    {
+        scilab_setInternalError(env, L"getCellArray", _W("var must be a cell variable"));
+        return API_ERROR;
+    }
+#endif
+    
+    *vals = (scilabVar*)c->get();
+    return API_OK;
+}
+
+api_return API_PROTO(setCellArray)(scilabEnv env, scilabVar var, const scilabVar* vals)
+{
+    types::Cell* c = (types::Cell*)var;
+#ifdef __API_SCILAB_SAFE__
+    if (c->isCell() == false)
+    {
+        scilab_setInternalError(env, L"setCellArray", _W("var must be a cell variable"));
+        return API_ERROR;
+    }
+#endif
+    bool bset = c->set((types::InternalType**)vals);
+#ifdef __API_SCILAB_SAFE__
+    if (bset == false)
+    {
+        scilab_setInternalError(env, L"setBooleanArray", _W("unable to set data"));
+        return API_ERROR;
+    }
+#endif
+    return bset ? API_OK : API_ERROR;
+}
