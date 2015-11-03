76c5862d056294b954c0f9e8f9da6b4762985456
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2015-11-03 23:55:30 +0100

    WIP: new C API for Scilab gateways (once again ...)
    2 modes:
      - "safe" version, good for development phase
      - "unsafe" version, remove checks in API for better performance and nice
        crashes
    
    Standard calls use "safe" version, to use "unsafe" version
    define __API_SCILAB_UNSAFE__ before include of "api_scilab.h"
    
    Done:
      - common functions ( unsafe )
      - double ( unsafe )
      - string ( unsafe )
      - int ( unsafe )
      - struct ( unsafe )
      - cell ( unsafe )
      - polynomial ( unsafe )
      - boolean ( unsafe )
      - (_/t/m)list ( unsafe )
      - graphic handle ( unsafe )
    
    Not done yet:
      - all types in "safe" versions
      - sparse ( safe and unsafe )
      - deal with usertype
      - lot of tests in both mode
    
    Change-Id: I15b9912842326b41353bd002b8876cf7246a4884

---
+++ scilab/modules/api_scilab/src/cpp/template/api_common.hpp
@@ -0,0 +1,308 @@
+/*
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2015 - Scilab Enterprises - Antoine ELIAS
+*
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at
+* http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
+*/
+
+#include "gatewaystruct.hxx"
+#include "double.hxx"
+#include "context.hxx"
+#include "list.hxx"
+
+extern "C"
+{
+#include "api_scilab.h"
+}
+
+/* Scilab 6 API*/
+
+//var
+scilabVar API_PROTO(getVar)(wchar_t* name)
+{
+    return (scilabVar)symbol::Context::getInstance()->get(symbol::Symbol(name));
+}
+
+//type
+int API_PROTO(getType)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    switch (it->getType())
+    {
+        case types::InternalType::ScilabDouble:
+            return sci_matrix;
+            break;
+        case types::InternalType::ScilabPolynom:
+            return sci_poly;
+            break;
+        case types::InternalType::ScilabBool:
+            return sci_boolean;
+            break;
+        case types::InternalType::ScilabSparse:
+            return sci_sparse;
+            break;
+        case types::InternalType::ScilabSparseBool:
+            return sci_boolean_sparse;
+            break;
+            //case types::InternalType::RealMatlabSparse :
+            //    return sci_matlab_sparse;
+            //    break;
+        case types::InternalType::ScilabInt8:
+        case types::InternalType::ScilabUInt8:
+        case types::InternalType::ScilabInt16:
+        case types::InternalType::ScilabUInt16:
+        case types::InternalType::ScilabInt32:
+        case types::InternalType::ScilabUInt32:
+        case types::InternalType::ScilabInt64:
+        case types::InternalType::ScilabUInt64:
+            return sci_ints;
+            break;
+        case types::InternalType::ScilabHandle:
+            return sci_handles;
+            break;
+        case types::InternalType::ScilabString:
+            return sci_strings;
+            break;
+        case types::InternalType::ScilabMacroFile:
+            return sci_u_function;
+            break;
+        case types::InternalType::ScilabMacro:
+            return sci_c_function;
+            break;
+        case types::InternalType::ScilabList:
+            return sci_list;
+            break;
+        case types::InternalType::ScilabCell:
+            return sci_cell;
+            break;
+        case types::InternalType::ScilabTList:
+            return sci_tlist;
+            break;
+        case types::InternalType::ScilabMList:
+            return sci_mlist;
+            break;
+        case types::InternalType::ScilabStruct:
+            // Scilab < 6 compatibility... Struct have type 17;
+            return sci_struct;
+            break;
+        case types::InternalType::ScilabUserType:
+            return sci_pointer;
+            break;
+        case types::InternalType::ScilabColon:
+        case types::InternalType::ScilabImplicitList:
+            return sci_implicit_poly;
+            break;
+        case types::InternalType::ScilabFunction:
+            return sci_intrinsic_function;
+            break;
+        case types::InternalType::ScilabLibrary:
+            return sci_lib;
+            break;
+        default:
+            return 0;
+    }
+
+}
+
+int API_PROTO(isDouble)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    return it->isDouble() ? 1 : 0;
+}
+
+int API_PROTO(isString)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    return it->isString() ? 1 : 0;
+}
+
+int API_PROTO(isBoolean)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    return it->isBool() ? 1 : 0;
+}
+
+int API_PROTO(isCell)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    return it->isCell() ? 1 : 0;
+}
+
+int API_PROTO(isInt)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    return it->isInt() ? 1 : 0;
+}
+
+int API_PROTO(isList)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    return it->isList() ? 1 : 0;
+}
+
+int API_PROTO(isTList)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    return it->isTList() ? 1 : 0;
+}
+
+int API_PROTO(isMList)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    return it->isMList() ? 1 : 0;
+}
+
+int API_PROTO(isHandle)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    return it->isHandle() ? 1 : 0;
+}
+
+//complex
+int API_PROTO(isComplex)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    if (it->isGenericType())
+    {
+        types::GenericType* gt = static_cast<types::GenericType*>(it);
+        return gt->isComplex() ? 1 : 0;
+    }
+
+    return 0;
+}
+
+//dimension
+int API_PROTO(getDim)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    if (it->isArrayOf())
+    {
+        types::GenericType* gt = static_cast<types::GenericType*>(it);
+        return gt->getDims();
+    }
+
+    int type = it->getType();
+    switch (type)
+    {
+        case types::InternalType::ScilabList:
+        case types::InternalType::ScilabMList:
+        case types::InternalType::ScilabTList:
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int API_PROTO(getDimArray)(scilabEnv env, scilabVar var, int** dims)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    if (it->isArrayOf())
+    {
+        types::GenericType* gt = static_cast<types::GenericType*>(it);
+        *dims = gt->getDimsArray();
+        return gt->getSize();
+    }
+
+
+    int type = it->getType();
+    switch (type)
+    {
+        case types::InternalType::ScilabList:
+        case types::InternalType::ScilabMList:
+        case types::InternalType::ScilabTList:
+        {
+            types::List* l = it->getAs<types::List>();
+            return l->getSize();
+        }
+    }
+
+    return 0;
+}
+
+int API_PROTO(getSize)(scilabEnv env, scilabVar var)
+{
+    types::InternalType* it = (types::InternalType*)var;
+    if (it->isArrayOf())
+    {
+        types::GenericType* gt = static_cast<types::GenericType*>(it);
+        return gt->getSize();
+    }
+
+    int type = it->getType();
+    switch (type)
+    {
+        case types::InternalType::ScilabList:
+        case types::InternalType::ScilabMList:
+        case types::InternalType::ScilabTList:
+        {
+            types::List* l = it->getAs<types::List>();
+            return l->getSize();
+        }
+    }
+
+    return 0;
+}
+
+int API_PROTO(isScalar)(scilabEnv env, scilabVar var)
+{
+    return scilab_getSize(env, var) == 1 ? 1 : 0;
+}
+
+int API_PROTO(isVector)(scilabEnv env, scilabVar var)
+{
+    int* dims = nullptr;
+    int dim = scilab_getDimArray(env, var, &dims);
+
+    for (int i = 0; i < dim; ++i)
+    {
+        int ret = scilab_isNVector(env, var, i);
+        if (ret)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int API_PROTO(isNVector)(scilabEnv env, scilabVar var, int n)
+{
+    int* dims = nullptr;
+    int dim = scilab_getDimArray(env, var, &dims);
+
+    for (int i = 0; i < dim; ++i)
+    {
+        if (i == n)
+        {
+            continue;
+        }
+
+        if (dims[i] != 1)
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int API_PROTO(isRowVector)(scilabEnv env, scilabVar var)
+{
+    return scilab_isNVector(env, var, 0);
+}
+
+int API_PROTO(isColVector)(scilabEnv env, scilabVar var)
+{
+    return scilab_isNVector(env, var, 1);
+}
+
+int API_PROTO(isHypermat)(scilabEnv env, scilabVar var)
+{
+    return scilab_getDim(env, var) > 2 ? 1 : 0;
+}
---
+++ scilab/modules/functions/sci_gateway/c/sci_api_test.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#define __API_SCILAB_UNSAFE__
+#include "api_scilab.h"
+#include "gw_functions.h"
+#include "string.h"
+
+/*old*/
+int sci_api_old_empty(char* fname, void* pvApiCtx)
+{
+    int rhs = nbInputArgument(pvApiCtx);
+    createScalarDouble(pvApiCtx, rhs + 1, 1);
+    LhsVar(1) = rhs + 1;
+    PutLhsVar();
+    return 0;
+}
+
+int sci_api_old_work(char *fname, void* pvApiCtx)
+{
+    int rhs = nbInputArgument(pvApiCtx);
+    double ret = 0;
+    for (int i = 0; i < rhs; ++i)
+    {
+        int* addr = NULL;
+        getVarAddressFromPosition(pvApiCtx, i + 1, &addr);
+        if (addr)
+        {
+            int type;
+            getVarType(pvApiCtx, addr, &type);
+            switch (type)
+            {
+                case sci_matrix:
+                {
+                    double val;
+                    getScalarDouble(pvApiCtx, addr, &val);
+                    ret += val;
+                    break;
+                }
+                case sci_strings:
+                {
+                    wchar_t* val;
+                    getAllocatedSingleWideString(pvApiCtx, addr, &val);
+                    ret += wcslen(val);
+                    freeAllocatedSingleWideString(val);
+                    break;
+                }
+
+                case sci_mlist:
+                {
+                    break;
+                }
+
+                default:
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+
+    createScalarDouble(pvApiCtx, rhs + 1, ret);
+    LhsVar(1) = rhs + 1;
+    PutLhsVar();
+    return 0;
+}
+
+int sci_api_empty(scilabEnv env, int nin, scilabVar* in, scilabOpt* opt, int nout, scilabVar* out)
+{
+    out[0] = scilab_createDouble(env, 1);
+    if (out[0] == NULL)
+    {
+        printf("check works !\n");
+    }
+    return 0;
+}
+
+double computework(scilabEnv env, scilabVar item)
+{
+    double ret = 0;
+    switch (scilab_getType(env, item))
+    {
+        case sci_matrix:
+        {
+            double* val;
+            scilab_getDoubleArray(env, item, &val);
+            ret = val[0];
+            break;
+        }
+
+        case sci_strings:
+        {
+            wchar_t** wc = NULL;
+            scilab_getStringArray(env, item, &wc);
+            ret = (int)wcslen(wc[0]);
+            break;
+        }
+        case sci_cell:
+        {
+            int j = 0;
+            scilabVar* items;
+            int size = scilab_getSize(env, item);
+
+            scilab_getCellArray(env, item, &items);
+            for (j = 0; j < size; ++j)
+            {
+                void* itemaddr = items[j];
+                ret += computework(env, itemaddr);
+            }
+
+            break;
+        }
+        case sci_struct:
+        {
+            int i = 0;
+            wchar_t** fields = NULL;
+            int count = scilab_getFields(env, item, &fields);
+            for (i = 0; i < count; ++i)
+            {
+                scilabVar f = scilab_getStructData2d(env, item, fields[i], 0, 0);
+                ret += computework(env, f);
+            }
+
+            break;
+        }
+    }
+
+    return ret;
+}
+
+int sci_api_work(scilabEnv env, int nin, scilabVar* in, scilabOpt* opt, int nout, scilabVar* out)
+{
+    int i = 0;
+    double ret = 0;
+    for (i = 0; i < nin; ++i)
+    {
+        ret += computework(env, in[i]);
+    }
+
+    out[0] = scilab_createDouble(env, ret);
+    return 0;
+}
