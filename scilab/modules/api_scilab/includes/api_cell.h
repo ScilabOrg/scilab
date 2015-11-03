4244db30be0f3f3bf0ba11ed7af8cd210d48a5b2
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2015-11-03 23:55:30 +0100

    WIP: new C API for Scilab gateways (once again ...)
    2 modes:
      - "safe" version, good for development phase
      - "unsafe" version, remove checks in API for better performance and nice
        crashes
    
    Standard calls use "safe" version, to use "unsafe" version
    define __API_SCILAB_UNSAFE__ before include of "api_scilab.h"
    
    Done:
      - common, double, string, int, struct, cell, polynomial,
        boolean, (_/t/m)list, graphic handle.
      - update dynamic_link gateway builder to link the new interface
      - add somes tests in api_scilab
      - add some help pages ( double, int, string, handle, list, boolean, poly
        )
    
    Not done yet:
      - sparse.
      - deal with overload.
      - deal with usertype.
      - lot of tests in both mode.
      - help pages ( struct, cell, sparse, main page, scilabEnv, scilabVar,
        scilabStatus )
    
    Change-Id: I15b9912842326b41353bd002b8876cf7246a4884

---
+++ scilab/modules/api_scilab/includes/api_cell.h
@@ -0,0 +1,36 @@
+/*
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2015 - Scilab Enteprises - Antoine ELIAS
+*
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at
+* http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
+*/
+
+#ifndef __CELL_API__
+#define __CELL_API__
+
+#if !defined(__INTERNAL_API_SCILAB__)
+#error Do not include api_cell.h. Include api_scilab.h instead.
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define scilab_createCell       API_PROTO(createCell)
+#define scilab_createCell2d     API_PROTO(createCell2d)
+#define scilab_getCellArray     API_PROTO(getCellArray)
+#define scilab_setCellArray     API_PROTO(setCellArray)
+
+    scilabVar API_PROTO(createCell)(scilabEnv env, int dim, const int* dims);
+    scilabVar API_PROTO(createCell2d)(scilabEnv env, int row, int col);
+
+    scilabStatus API_PROTO(getCellArray)(scilabEnv env, scilabVar var, const scilabVar** vals);
+    scilabStatus API_PROTO(setCellArray)(scilabEnv env, scilabVar var, const scilabVar* vals);
+#ifdef __cplusplus
+}
+#endif
+#endif /* __CELL_API__ */
