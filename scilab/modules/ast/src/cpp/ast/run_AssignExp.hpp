baf2c08c76e0327199f9aa62677bd88e4fb82b63
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2015-11-18 15:29:27 +0100

    update mode management
    Change-Id: Ice12edf6b2f4a7152d84ae017bd738fce1396f94

--- scilab/modules/ast/src/cpp/ast/run_AssignExp.hpp
+++ scilab/modules/ast/src/cpp/ast/run_AssignExp.hpp
@@ -113,7 +113,7 @@ void RunVisitorT<T>::visitprivate(const AssignExp  &e)
                 }
             }
 
-            if (e.isVerbose() && ConfigVariable::isPromptShow())
+            if (e.isVerbose() && ConfigVariable::isPrintOutput())
             {
                 std::wstring wstrName = pVar->getSymbol().getName();
                 std::wostringstream ostr;
@@ -195,7 +195,7 @@ void RunVisitorT<T>::visitprivate(const AssignExp  &e)
 
             if (pOut != NULL)
             {
-                if (e.isVerbose() && ConfigVariable::isPromptShow())
+                if (e.isVerbose() && ConfigVariable::isPrintOutput())
                 {
                     std::wostringstream ostr;
                     ostr << L" " << *getStructNameFromExp(pCell) << L"  = " << std::endl;
@@ -357,7 +357,7 @@ void RunVisitorT<T>::visitprivate(const AssignExp  &e)
                 }
             }
 
-            if (e.isVerbose() && ConfigVariable::isPromptShow())
+            if (e.isVerbose() && ConfigVariable::isPrintOutput())
             {
                 std::wostringstream ostr;
                 ostr << L" " << *getStructNameFromExp(&pCall->getName()) << L"  = " << std::endl;
@@ -497,7 +497,7 @@ void RunVisitorT<T>::visitprivate(const AssignExp  &e)
                 delete i;
             }
 
-            if (e.isVerbose() && ConfigVariable::isPromptShow())
+            if (e.isVerbose() && ConfigVariable::isPrintOutput())
             {
                 const std::wstring *pstName = getStructNameFromExp(pField);
 
